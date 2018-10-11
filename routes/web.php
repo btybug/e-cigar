d449e05c36336ebcfc38a9b651a516b41677f52e
gev2092 <gev2092harutyunyan@gmail.com>
2018-10-11 13:25:12 +0400

    up

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/blog', 'Frontend/BlogController@index')->name('blog');
+Route::get('/forum', 'Frontend/ForumController@index')->name('forum');
+Route::get('/shop', 'ShopController@index')->name('shop');
