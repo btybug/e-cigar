99b128fbdaf6ea087fa87491756ffc930d7d820b
Hovo <hovoaep@gmail.com>
2018-10-11 17:59:19 +0400

    add checkob tree

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -37,8 +37,57 @@
 
     </div>
     <div class="col-md-9">
-
+    <div id="treeview_json"></div>
     </div>
 @stop
 @section('js')
+<script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.js"></script>
+<script>
+    var tree = [
+  {
+    text: "Parent 1",
+    nodes: [
+      {
+        text: "Child 1",
+        nodes: [
+          {
+            text: "Grandchild 1"
+          },
+          {
+            text: "Grandchild 2"
+          }
+        ]
+      },
+      {
+        text: "Child 2"
+      }
+    ]
+  },
+  {
+    text: "Parent 2"
+  },
+  {
+    text: "Parent 3"
+  },
+  {
+    text: "Parent 4"
+  },
+  {
+    text: "Parent 5"
+  }
+]
+    $('#treeview_json').treeview({
+        data: tree,
+        showCheckbox: true,
+        onNodeChecked: function(event, node) {
+            console.log(1)
+            // $('#checkable-output').prepend('<p>' + node.text + ' was checked</p>');
+        },
+        onNodeUnchecked: function (event, node) {
+            console.log(111)
+            // $('#checkable-output').prepend('<p>' + node.text + ' was unchecked</p>');
+        }
+});
+
+</script>
 @stop
