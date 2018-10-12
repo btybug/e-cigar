b57cdb04108215a290077a95428a0d2c911e8fac
terakyan <edo.terakyan@gmail.com>
2018-10-12 11:53:33 +0400

    up

--- resources/views/frontend/my_account/index.blade.php
+++ resources/views/frontend/my_account/index.blade.php
@@ -14,44 +14,36 @@
 
                     <ul class="nav nav-tabs" id="myTab" role="tablist">
                         <li class="nav-item">
-                            <a class="nav-link active" data-toggle="tab" href="{!! route('my_account_profile') !!}" role="tab" aria-controls="home">Profile</a>
+                            <a class="nav-link active" href="{!! route('my_account_profile') !!}" >Profile</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="{!! route('my_account_password') !!}" role="tab" aria-controls="profile">Password</a>
+                            <a class="nav-link" href="{!! route('my_account_password') !!}" >Password</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="{!! route('my_account_logs') !!}" role="tab" aria-controls="messages">Logs</a>
+                            <a class="nav-link" href="{!! route('my_account_logs') !!}">Logs</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="{!! route('my_account_favourites') !!}" role="tab" aria-controls="settings">Favourites</a>
+                            <a class="nav-link"  href="{!! route('my_account_favourites') !!}">Favourites</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="{!! route('my_account_orders') !!}" role="tab" aria-controls="orders">My orders</a>
+                            <a class="nav-link" href="{!! route('my_account_orders') !!}" >My orders</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="{!! route('shop_my_cart') !!}" role="tab" aria-controls="card">My Card</a>
+                            <a class="nav-link"  href="{!! route('shop_my_cart') !!}">My Card</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="{!! route('my_account_address') !!}" role="tab" aria-controls="address-book">Address book
+                            <a class="nav-link" href="{!! route('my_account_address') !!}" >Address book
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="tab" href="{!! route('my_account_verification') !!}" role="tab" aria-controls="address-book">Verification
+                            <a class="nav-link" href="{!! route('my_account_verification') !!}" >Verification
                             </a>
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="col-md-8">
-                <div class="tab-content">
-                    <div class="tab-pane active" id="home" role="tabpanel">..1..</div>
-                    <div class="tab-pane" id="profile" role="tabpanel">..2..</div>
-                    <div class="tab-pane" id="messages" role="tabpanel">..3..</div>
-                    <div class="tab-pane" id="settings" role="tabpanel">..4..</div>
-                    <div class="tab-pane" id="orders" role="tabpanel">..5..</div>
-                    <div class="tab-pane" id="card" role="tabpanel">..6..</div>
-                    <div class="tab-pane" id="address-book" role="tabpanel">..7..</div>
-                </div>
+
             </div>
         </div>
 
