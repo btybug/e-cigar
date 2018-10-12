753ae301ac77e83e0732e7194c5e1cb47910ec02
sahakavatar <hakobyan.sahak88@gmail.com>
2018-10-12 11:08:40 +0400

    lang page

--- resources/views/admin/settings/languages.blade.php
+++ resources/views/admin/settings/languages.blade.php
@@ -1,26 +1,113 @@
 @extends('layouts.admin')
 @section('content')
-   languages
+    <section class="settings_lang">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title">Listing All Languages </h3>
+                        <div class="box-tools pull-right">
+                            <a href="{!! route('admin_settings_languages_new') !!}" type="button" class="btn btn-block btn-primary">Add</a>
+                        </div>
+                    </div>
+
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-xs-12">
+                            </div>
+                        </div>
+
+                        <div class="row default-div hidden">
+                            <div class="col-xs-12">
+                                <div class="alert alert-success alert-dismissible" role="alert">
+                                    Default language has been changed successfully!
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>Default</th>
+                                        <th>Language</th>
+                                        <th>Icon</th>
+                                        <th>Code</th>
+                                        <th>Action</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <label>
+                                                <input type="radio" name="languages_id" value="1"  class="default_language"  >
+                                            </label>
+                                        </td>
+                                        <!--<td>1</td>-->
+                                        <td>English</td>
+                                        <td><img src="http://demo0.laravelcommerce.com//resources/assets/images/language_flags/1486556365.503984030_english.jpg" width="25px" alt=""></td>
+                                        <td>en</td>
+                                        <td>
+                                            <a data-toggle="tooltip" data-placement="bottom" title=" English" href="#" class="badge bg-light-blue"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+
+
+                                            <a data-toggle="tooltip" data-placement="bottom" title=" English" id="deleteLanguageId" languages_id ="1" class="badge bg-red"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label>
+                                                <input type="radio" name="languages_id" value="6"  class="default_language"  >
+                                            </label>
+                                        </td>
+                                        <!--<td>6</td>-->
+                                        <td>Indonesia</td>
+                                        <td><img src="http://demo0.laravelcommerce.com//" width="25px" alt=""></td>
+                                        <td>in</td>
+                                        <td>
+                                            <a data-toggle="tooltip" data-placement="bottom" title=" Indonesia" href="#" class="badge bg-light-blue"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+
+
+                                            <a data-toggle="tooltip" data-placement="bottom" title=" Indonesia" id="deleteLanguageId" languages_id ="6" class="badge bg-red"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label>
+                                                <input type="radio" name="languages_id" value="8"  class="default_language"  checked  >
+                                            </label>
+                                        </td>
+                                        <!--<td>8</td>-->
+                                        <td>Español</td>
+                                        <td><img src="http://demo0.laravelcommerce.com//" width="25px" alt=""></td>
+                                        <td>es</td>
+                                        <td>
+                                            <a data-toggle="tooltip" data-placement="bottom" title=" Español" href="#" class="badge bg-light-blue"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <div class="col-xs-12 text-right">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+        </div>
+    </section>
 @stop
 @section('css')
-    <link rel="stylesheet" href="{{asset('public/admin_theme/flagstrap/css/flags.css')}}">
+    <link rel="stylesheet" href="{{asset('public/css/custom.css?v='.rand(111,999))}}">
 @stop
 @section('js')
-    <script src="{{asset('public/admin_theme/flagstrap/js/jquery.flagstrap.js')}}"></script>
-    <script>
-        $(document).ready(function () {
-            $('#flag-select').flagStrap({
-                countries: {
-                    "AM": "Armenia",
-                    "GB": "United Kingdom",
-                    "RU": "Russia"
-                },
-                buttonSize: "btn-sm",
-                buttonType: "btn-info",
-                labelMargin: "10px",
-                scrollable: false,
-                scrollableHeight: "350px"
-            });
-        });
-    </script>
+
 @stop
--- resources/views/admin/settings/new_languages.blade.php
+++ resources/views/admin/settings/new_languages.blade.php
@@ -1,26 +1,123 @@
 @extends('layouts.admin')
 @section('content')
-   add languages
+    <section class="setting_add_lang">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title">Add Language</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-xs-12">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="box box-info">
+                                    <!-- form start -->
+                                    <div class="box-body">
+
+                                        <form method="POST"
+                                              action=""
+                                              accept-charset="UTF-8" class="form-horizontal form-validate"
+                                              enctype="multipart/form-data"><input name="_token" type="hidden"
+                                                                                   value="eWIORi0IImLFJ1jm0g72IoUOHA3RgBpLlk4ZFl4j">
+
+                                            <div class="form-group">
+                                                <label for="name" class="col-sm-2 col-md-3 control-label">Name
+
+                                                </label>
+                                                <div class="col-sm-10 col-md-4">
+                                                    <input class="form-control field-validate" id="name" name="name"
+                                                           type="text" value="">
+                                                    <span class="help-block">Language name such as &quot;English&quot;</span>
+                                                    <span class="help-block hidden">This field is required.</span>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name" class="col-sm-2 col-md-3 control-label">Code</label>
+                                                <div class="col-sm-10 col-md-4">
+                                                    <input class="form-control field-validate" id="code" name="code"
+                                                           type="text" value="">
+                                                    <span class="help-block">Language code such as &quot;en&quot; for english.</span>
+                                                    <span class="help-block hidden">This field is required.</span>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name"
+                                                       class="col-sm-2 col-md-3 control-label">Direction</label>
+                                                <div class="col-sm-10 col-md-4">
+                                                    <select class="form-control field-validate" id="direction"
+                                                            name="direction">
+                                                        <option value="rtl">Right To Left</option>
+                                                        <option value="ltr">Left To Right</option>
+                                                    </select>
+                                                    <span class="help-block">Language Direction</span>
+                                                    <span class="help-block hidden">This field is required.</span>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name" class="col-sm-2 col-md-3 control-label">Icon</label>
+                                                <div class="col-sm-10 col-md-4">
+                                                    <input id="newImage" name="newImage" type="file">
+                                                    <span class="help-block">Language Flag</span>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name"
+                                                       class="col-sm-2 col-md-3 control-label">Directory</label>
+                                                <div class="col-sm-10 col-md-4">
+                                                    <input class="form-control" id="directory" name="directory"
+                                                           type="text" value="">
+                                                    <span class="help-block">
+								Language Directory such as &quot;english&quot;</span>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="name"
+                                                       class="col-sm-2 col-md-3 control-label">Default</label>
+                                                <div class="col-sm-10 col-md-4">
+                                                    <select class="form-control field-validate" id="is_default"
+                                                            name="is_default">
+                                                        <option value="1">Yes</option>
+                                                        <option value="0" selected>No</option>
+                                                    </select>
+                                                    <span class="help-block">Set default language.</span>
+                                                    <span class="help-block hidden">This field is required.</span>
+                                                </div>
+                                            </div>
+
+                                            <!-- /.box-body -->
+                                            <div class="box-footer text-right">
+                                                <div class="col-sm-offset-2 col-md-offset-3 col-sm-10 col-md-4">
+                                                    <button type="submit" class="btn btn-primary">Add Language</button>
+                                                    <a href="#"
+                                                       type="button" class="btn btn-default">Back</a>
+                                                </div>
+                                            </div>
+                                            <!-- /.box-footer -->
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @stop
 @section('css')
-    <link rel="stylesheet" href="{{asset('public/admin_theme/flagstrap/css/flags.css')}}">
+    <link rel="stylesheet" href="{{asset('public/css/custom.css?v='.rand(111,999))}}">
 @stop
 @section('js')
-    <script src="{{asset('public/admin_theme/flagstrap/js/jquery.flagstrap.js')}}"></script>
-    <script>
-        $(document).ready(function () {
-            $('#flag-select').flagStrap({
-                countries: {
-                    "AM": "Armenia",
-                    "GB": "United Kingdom",
-                    "RU": "Russia"
-                },
-                buttonSize: "btn-sm",
-                buttonType: "btn-info",
-                labelMargin: "10px",
-                scrollable: false,
-                scrollableHeight: "350px"
-            });
-        });
-    </script>
+
 @stop
