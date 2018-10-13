f18789dfc61827495b0dd29b7ff58c3820e8d6b4
Sahakavatar <hakobyan.sahak88@gmail.com>
2018-10-13 21:50:03 +0400

    update

--- resources/views/frontend/my_account/address.blade.php
+++ resources/views/frontend/my_account/address.blade.php
@@ -12,38 +12,38 @@
                     <div class="panel panel-default">
                         <div class="panel-body">
                         <form class="form-horizontal">
-                            <div class="form-group row">
+                            <div class="form-group">
                                 <label for="text" class="control-label col-xs-4">Company name</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">1st Line address</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">2nd line address</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">City</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">Country</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">Post Code</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
                             </div>
-                            <div class="form-group row row">
+                            <div class="form-group row">
                                 <div class="col-xs-offset-4 col-xs-8">
                                     <button name="submit" type="submit" class="btn btn-primary">Submit</button>
                                 </div>
@@ -55,36 +55,36 @@
             </div>
             <div class="col-md-6">
                 <div class="container">
-                    <h2>Billing Address</h2>
+                    <h2>Default Shipping</h2>
                     <div class="panel panel-default">
                         <div class="panel-body">
                         <form class="form-horizontal">
-                            <div class="form-group row">
+                            <div class="form-group">
                                 <label for="text" class="control-label col-xs-4">Company name</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">1st Line address</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">2nd line address</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">City</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">Country</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
                                 </div>
-                            </div><div class="form-group row">
+                            </div><div class="form-group">
                                 <label for="text" class="control-label col-xs-4">Post Code</label>
                                 <div class="col-xs-8">
                                     <input id="text" name="text" type="text" class="form-control">
@@ -116,7 +116,44 @@
                                 </div>
                             </div>
                             <div class="col-md-9">
-                                preview
+                                <form class="form-horizontal">
+                                    <div class="form-group">
+                                        <label for="text" class="control-label col-xs-4">Company name</label>
+                                        <div class="col-xs-8">
+                                            <input id="text" name="text" type="text" class="form-control">
+                                        </div>
+                                    </div><div class="form-group">
+                                        <label for="text" class="control-label col-xs-4">1st Line address</label>
+                                        <div class="col-xs-8">
+                                            <input id="text" name="text" type="text" class="form-control">
+                                        </div>
+                                    </div><div class="form-group">
+                                        <label for="text" class="control-label col-xs-4">2nd line address</label>
+                                        <div class="col-xs-8">
+                                            <input id="text" name="text" type="text" class="form-control">
+                                        </div>
+                                    </div><div class="form-group">
+                                        <label for="text" class="control-label col-xs-4">City</label>
+                                        <div class="col-xs-8">
+                                            <input id="text" name="text" type="text" class="form-control">
+                                        </div>
+                                    </div><div class="form-group">
+                                        <label for="text" class="control-label col-xs-4">Country</label>
+                                        <div class="col-xs-8">
+                                            <input id="text" name="text" type="text" class="form-control">
+                                        </div>
+                                    </div><div class="form-group">
+                                        <label for="text" class="control-label col-xs-4">Post Code</label>
+                                        <div class="col-xs-8">
+                                            <input id="text" name="text" type="text" class="form-control">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-xs-offset-4 col-xs-8">
+                                            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
