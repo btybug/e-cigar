65ccf23d972ed5ba9d8f18d94e195db2713c3104
Sahakavatar <hakobyan.sahak88@gmail.com>
2018-10-15 09:54:39 +0400

    addresses table

---
+++ database/migrations/2018_10_13_180109_create_addresses_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('company')->nullable();
+            $table->string('type',20)->nullable();
+            $table->string('first_line_address');
+            $table->string('second_line_address');
+            $table->string('city',50);
+            $table->string('country',50);
+            $table->string('post_code',20);
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
