96c43b108f4bb9edda79ecb64dfa06bc6354c706
Hovo <hovoaep@gmail.com>
2018-10-15 11:40:52 +0400

    add tags in invetory

--- resources/views/admin/inventory/tags/index.blade.php
+++ resources/views/admin/inventory/tags/index.blade.php
@@ -9,7 +9,7 @@
             <div class="col-md-6 "><a class="btn btn-primary pull-right" href="#">Add new</a></div>
         </div>
         <div class="col-xs-12">
-            <table id="attributes-table" class="table table-style table-bordered" cellspacing="0" width="100%">
+            <!-- <table id="attributes-table" class="table table-style table-bordered" cellspacing="0" width="100%">
                 <thead>
                 <tr>
                     <th>#</th>
@@ -20,12 +20,25 @@
                     <th>Actions</th>
                 </tr>
                 </thead>
-            </table>
+            </table> -->
+            <div>
+            <form >
+            <div class="col-md-10">
+                <input type="text" id="add-new-tags" class="form-control " placeholder="Add new tags">
+                </div>
+                    <button type="submit" class="btn btn-submit ">Add new Tag</button>
+                </div>
+            </form>
+            <div>
+                <div class="tags">
+                
+                </div>
+            </div>
         </div>
     </div>
 @stop
 @section('js')
-    <script>
+    <!-- <script>
         $(function () {
             {{--$('#attributes-table').DataTable({--}}
                 {{--ajax:  "{!! route('datatable_all_attributes') !!}",--}}
@@ -43,5 +56,52 @@
             {{--});--}}
         });
 
+    </script> -->
+    <script>
+    const makeHtml = (data) => {
+           return `<div class="single-tags col-md-2" style=" margin: 5px;">
+                    <div class="single-tags-text">
+                        <p>${data}</p>
+                    </div>
+                    <div class="remove-tag">
+                        <i class="fa fa-times"></i>
+                    </div>
+                </div>`
+        }
+        const  makeAllHtml  =  (data) => {
+            $(".tags").empty()
+            data.forEach(item => {
+                $(".tags").append( makeHtml(item))
+             })
+        }
+        const firstData = JSON.parse(localStorage.getItem('tags'))
+        if (firstData) {
+            makeAllHtml(firstData)
+        }
+        
+        $("form").submit(function(e){
+            e.preventDefault()
+            let value = $("#add-new-tags").val()
+            let allData = JSON.parse(localStorage.getItem('tags'));
+            if (allData === null) {
+                let arr = []
+                localStorage.setItem("tags" , JSON.stringify([value]));
+                makeAllHtml(arr)
+                return
+            }
+            allData.push(value)
+            makeAllHtml(allData)
+            localStorage.setItem("tags" , JSON.stringify(allData));
+
+        })
+
+        $("body").on("click", ".remove-tag", function(){
+            let value = $(this).closest(".single-tags").find(".single-tags-text").text().trim()
+            let allData = JSON.parse(localStorage.getItem('tags'));
+            let index = allData.indexOf(value)
+            allData.splice( index, 1 );
+            makeAllHtml(allData)
+            localStorage.setItem("tags" , JSON.stringify(allData));
+        })
     </script>
 @stop
