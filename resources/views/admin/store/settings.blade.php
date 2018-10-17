c1f4c27e2643c55fa2f254746ef01fcae8585f0e
Hovo <hovoaep@gmail.com>
2018-10-17 14:38:22 +0400

    fix shiping tables js

--- resources/views/admin/store/settings.blade.php
+++ resources/views/admin/store/settings.blade.php
@@ -132,11 +132,12 @@
                     </tbody>
                 </table>
                 <table class="table table-responsive table--store-settings">
-                    <tbody>
-                    <tr class="bg-my-light-blue">
+                <tr class="bg-my-light-blue">
                         <td>Shipping Zone - <span>Armenia</span></td>
                         <td colspan="5">Tax Rate - <span>ArmeniaVaT20</span></td>
                     </tr>
+                    <tbody>
+                    
                     <tr class="bg-my-light-pink">
                         <th>Order Amount</th>
                         <th>Courier</th>
@@ -191,22 +192,24 @@
                             <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
                         </td>
                     </tr>
-                    <tfoot>
-                    
-                    <tr>
+                    <tr class="add-new-ship-filed-container">
                         <td colspan="6" class="text-right">
                             <button type="button" class="btn btn-primary add-new-ship-filed"><i class="fa fa-plus-circle"></i></button>
                         </td>
                     </tr>
+                    </tbody>
+
+                    <tfoot>
+                    
+                 
                     <tr>
-                        <td colspan="5" class="text-center table--store-settings_add-options add-new-ship-filed">
+                        <td colspan="5" class="text-center table--store-settings_add-options add-new-order-filed">
                             <span><i class="fa fa-plus"></i></span> Add more option
                         </td>
                     </tr>
 
 
                     </tfoot>
-                    </tbody>
                 </table>
             </div>
         </div>
@@ -235,81 +238,83 @@ $("body").on("click", ".add-new-option", function () {
       <button type="button" data-table-id=${id} class="btn btn-primary delete-all-option"><i class="fa fa-trash"></i></button>
    </td>
 </tr>`
-let html2 = `<table class="table table-responsive table--store-settings container-for-table-remove" >
-   <tbody>
-      <tr class="bg-my-light-blue">
-         <td>Shipping Zone - <span>Armenia</span></td>
-         <td colspan="3">Tax Rate - <span>ArmeniaVaT20</span></td>
-         <td colspan="2" class="text-right"><button type="button" data-table-id="${id}" class="btn btn-primary delete-all-option"><i class="fa fa-trash"></i></button></span></td>
-      </tr>
-      <tr class="bg-my-light-pink">
-         <th>Order Amount</th>
-         <th>Courier</th>
-         <th>Cost</th>
-         <th colspan="3">Time</th>
-      </tr>
-      <tr>
-         <td class="table--store-settings_vert-top">
-            <input type="number" min="1" max="5" class="form-control" style="display: inline-block; width: auto">
-            <span>To</span>
-            <input type="number" min="1" max="50" class="form-control" style="display: inline-block; width: auto">
-         </td>
-         <td>
-            <select id="PosType" class="form-control">
-               <option selected="">Normal Post</option>
-               <option>...</option>
-            </select>
-         </td>
-         <td>
-            <span class="form-control">
-            5
-            </span>
-         </td>
-         <td>
-            <span class="form-control">
-            3 days
-            </span>
-         </td>
-         <td colspan="2" class="text-right">
-            <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
-         </td>
-      </tr>
-      <tr>
-         <td></td>
-         <td>
-            <select id="dhl" class="form-control">
-               <option selected="">DHL</option>
-               <option>...</option>
-            </select>
-         </td>
-         <td>
-            <span class="form-control">
-            5
-            </span>
-         </td>
-         <td>
-            <span class="form-control">
-            1 day
-            </span>
-         </td>
-         <td colspan="2" class="text-right">
-            <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
-         </td>
-      </tr>
-   </tbody>
-   <tfoot>
-      <tr>
-         <td colspan="6" class="text-right">
-            <button type="button" class="btn btn-primary add-new-ship-filed"><i class="fa fa-plus-circle"></i></button>
-         </td>
-      </tr>
-      <tr>
-         <td colspan="5" class="text-center table--store-settings_add-options add-new-ship-filed">
-            <span><i class="fa fa-plus"></i></span> Add more option
-         </td>
-      </tr>
-   </tfoot>
-</table>`
+let html2 = `
+<table class="table table-responsive table--store-settings container-for-table-remove">
+                <tr class="bg-my-light-blue">
+                <td>Shipping Zone - <span>Armenia</span></td>
+                <td colspan="3">Tax Rate - <span>ArmeniaVaT20</span></td>
+                <td colspan="2" class="text-right"><button type="button" data-table-id="${id}" class="btn btn-primary delete-all-option"><i class="fa fa-trash"></i></button></span></td>
+                    </tr>
+                    <tbody>
+                    
+                    <tr class="bg-my-light-pink">
+                        <th>Order Amount</th>
+                        <th>Courier</th>
+                        <th>Cost</th>
+                        <th colspan="3">Time</th>
+                    </tr>
+                    <tr>
+                        <td class="table--store-settings_vert-top">
+                            <input type="number" min="1" max="5" class="form-control" style="display: inline-block; width: auto">
+                            <span>To</span>
+                            <input type="number" min="1" max="50" class="form-control" style="display: inline-block; width: auto">
+                        </td>
+                        <td>
+                            <select id="PosType" class="form-control">
+                                <option selected>Normal Post</option>
+                                <option>...</option>
+                            </select>
+                        </td>
+                        <td>
+                            <span class="form-control">
+                                5
+                            </span>
+                        </td>
+                        <td>
+                            <span class="form-control">
+                                3 days
+                            </span>
+                        </td>
+                        <td colspan="2" class="text-right">
+                            <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <select id="dhl" class="form-control">
+                                <option selected>DHL</option>
+                                <option>...</option>
+                            </select>
+                        </td>
+                        <td>
+                            <span class="form-control">
+                                5
+                            </span>
+                        </td>
+                        <td>
+                            <span class="form-control">
+                                1 day
+                            </span>
+                        </td>
+                        <td colspan="2" class="text-right">
+                            <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
+                        </td>
+                    </tr>
+                    <tr class="add-new-ship-filed-container">
+                        <td colspan="6" class="text-right">
+                            <button type="button" class="btn btn-primary add-new-ship-filed"><i class="fa fa-plus-circle"></i></button>
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td colspan="5" class="text-center table--store-settings_add-options add-new-order-filed">
+                            <span><i class="fa fa-plus"></i></span> Add more option
+                        </td>
+                    </tr>
+                    </tfoot>
+                </table>`
 $(".all-options").append(html)
 $("#shipping").append(html2)
 })
@@ -341,13 +346,79 @@ let html = `<tr>
       <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
    </td>
 </tr>`
-$(this).closest("table").find("tbody").append(html)
+$(this).closest("tbody").find(".add-new-ship-filed-container").before(html)
 })
 
 $("body").on("click", ".delete-all-option", function (e) {
     console.log()
     let id =  $(this).attr("data-table-id")
     $("body").find(`[data-table-id="${id}"]`).closest(".container-for-table-remove").remove()
+})
+$("body").on("click", ".add-new-order-filed", function (e) {
+    // console.log(e)
+  let html = `  <tbody>
+   
+   <tr class="bg-my-light-pink">
+      <th>Order Amount</th>
+      <th>Courier</th>
+      <th>Cost</th>
+      <th colspan="3">Time</th>
+   </tr>
+   <tr>
+      <td class="table--store-settings_vert-top">
+         <input type="number" min="1" max="5" class="form-control" style="display: inline-block; width: auto">
+         <span>To</span>
+         <input type="number" min="1" max="50" class="form-control" style="display: inline-block; width: auto">
+      </td>
+      <td>
+         <select id="PosType" class="form-control">
+            <option selected="">Normal Post</option>
+            <option>...</option>
+         </select>
+      </td>
+      <td>
+         <span class="form-control">
+         5
+         </span>
+      </td>
+      <td>
+         <span class="form-control">
+         3 days
+         </span>
+      </td>
+      <td colspan="2" class="text-right">
+         <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
+      </td>
+   </tr>
+   <tr>
+      <td></td>
+      <td>
+         <select id="dhl" class="form-control">
+            <option selected="">DHL</option>
+            <option>...</option>
+         </select>
+      </td>
+      <td>
+         <span class="form-control">
+         5
+         </span>
+      </td>
+      <td>
+         <span class="form-control">
+         1 day
+         </span>
+      </td>
+      <td colspan="2" class="text-right">
+         <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
+      </td>
+   </tr>
+   <tr class="add-new-ship-filed-container">
+      <td colspan="6" class="text-right">
+         <button type="button" class="btn btn-primary add-new-ship-filed"><i class="fa fa-plus-circle"></i></button>
+      </td>
+   </tr>
+</tbody>`
+$(this).closest("table").append(html)
 })
 
 </script>
