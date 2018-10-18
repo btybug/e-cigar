54ed59432cbacae48e423eb7602bb39e1f042a1d
Nune <hakobyan.sahak88@gmail.com>
2018-10-18 11:33:38 +0400

    admin order version1

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -151,6 +151,104 @@
                         </div>
                     </div>
                 </div>
+                <div class="tab-pane tabe-pane--edit-order fade" id="editOrder" role="tabpanel" aria-labelledby="editOrder-tab">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item active">
+                            <a class="nav-link" id="customerDtls-tab" data-toggle="tab" href="#customerDtls" role="tab" aria-controls="customerDtls" aria-selected="true" aria-expanded="true">1. Customer Details</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="products-tab" data-toggle="tab" href="#products" role="tab" aria-controls="products" aria-selected="false">2. Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="paymentDtls-tab" data-toggle="tab" href="#paymentDtls" role="tab" aria-controls="paymentDtls" aria-selected="false">3. Payment Details</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="shippingDtls-tab" data-toggle="tab" href="#shippingDtls" role="tab" aria-controls="shippingDtls" aria-selected="false">4. Shipping Details</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="totals-tab" data-toggle="tab" href="#totals" role="tab" aria-controls="totals" aria-selected="false">5. Totals</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content tab-content-store-settings">
+                        <div class="tab-pane fade active in" id="customerDtls" role="tabpanel" aria-labelledby="customerDtls-tab">
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="input-store">Store</label>
+                                <div class="col-sm-10">
+                                    <select name="store_id" id="input-store" class="form-control">
+                                        <option value="0" selected="selected">Default</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group has-error">
+                                <label class="col-sm-2 control-label" for="input-currency">Currency</label>
+                                <div class="col-sm-10">
+                                    <select name="currency" id="input-currency" class="form-control">
+                                        <option value="EUR">Euro</option>
+                                        <option value="GBP">Pound Sterling</option>
+                                        <option value="USD" selected="selected">US Dollar</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="input-customer">Customer</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="customer" value="" placeholder="Customer" id="input-customer" class="form-control" autocomplete="off"><ul class="dropdown-menu"></ul>
+                                    <input type="hidden" name="customer_id" value="0">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label" for="input-customer-group">Customer Group</label>
+                                <div class="col-sm-10">
+                                    <select name="customer_group_id" id="input-customer-group" class="form-control">
+                                        <option value="1" selected="selected">Default</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group required">
+                                <label class="col-sm-2 control-label" for="input-firstname">First Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="firstname" value="sddc" id="input-firstname" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group required">
+                                <label class="col-sm-2 control-label" for="input-lastname">Last Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="lastname" value="sdvdsvc" id="input-lastname" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group required">
+                                <label class="col-sm-2 control-label" for="input-email">E-Mail</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="email" value="focusbenj@gmail.com" id="input-email" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group required">
+                                <label class="col-sm-2 control-label" for="input-telephone">Telephone</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="telephone" value="08069386824" id="input-telephone" class="form-control">
+                                </div>
+                            </div>
+                            <div class="text-right">
+                                <button type="button" id="button-customer" data-loading-text="Loading..." class="btn btn-primary"><i class="fa fa-arrow-right"></i> Continue</button>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="products" role="tabpanel" aria-labelledby="products-tab">
+                            Edit products fields
+                        </div>
+                        <div class="tab-pane fade" id="paymentDtls" role="tabpanel" aria-labelledby="paymentDtls-tab">
+                            Edit Payment Details fields
+                        </div>
+                        <div class="tab-pane fade" id="shippingDtls" role="tabpanel" aria-labelledby="shippingDtls-tab">
+                            Edit Shipping Details fields
+                        </div>
+                        <div class="tab-pane fade" id="totals" role="tabpanel" aria-labelledby="totals-tab">
+                            Edit Totals fields
+                        </div>
+                    </div>
+
+
+                </div>
+
             </div>
 
 
@@ -173,50 +271,58 @@
                 <option>...</option>
             </select>
         </div>
-        <div class="col-md-2" style="border-left: 2px solid #ddd; height: 868px; overflow: auto">
+        <div class="col-md-2">
             {{--<h3 style="color: red">"Here comes order notes column"</h3>--}}
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title text-center">Order Notes</h3>
                 </div>
                 <div class="order-notes">
-                    <div>
-                        <div class="status">
-                            <p>Order status changed from processing to Processing.</p>
+                    <div style="border-left: 2px solid #ddd; height: 500px; overflow: auto">
+                        <div class="status-outer">
+                            <div class="status">
+                                <p>Order status changed from processing to Processing.</p>
+                            </div>
+                            <div class="added d-flex">
+                                <span>added on October 15 2018 at 5:33 am by Abokamal</span>
+                                <button class="btn btn-primary">
+                                    <i>Delete Note</i>
+                                </button>
+                            </div>
                         </div>
-                        <div class="added d-flex">
-                            <span>added on October 15 2018 at 5:33 am by Abokamal</span>
-                            <button class="btn btn-primary">
-                                <i>Delete Note</i>
-                            </button>
+                        <div class="status-outer">
+                            <div class="status">
+                                <p>Order status changed from On Hold to Processing.</p>
+                            </div>
+                            <div class="added d-flex">
+                                <span>added on October 15 2018 at 5:33 am by Abokamal</span>
+                                <button class="btn btn-primary">
+                                    <i>Delete Note</i>
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="status">
-                        <p>Order status changed from On Hold to Processing.</p>
-                    </div>
-                    <div class="added d-flex">
-                        <span>added on October 15 2018 at 5:33 am by Abokamal</span>
-                        <button class="btn btn-primary">
-                            <i>Delete Note</i>
-                        </button>
-                    </div>
-                    <div class="status1">
-                        <p>Awaiting BACS payment Order status changed from Pending Payment to On Hold.</p>
-                    </div>
-                    <div class="added d-flex">
-                        <span>added on October 15 2018 at 5:33 am by Haseeb</span>
-                        <button class="btn btn-primary">
-                            <i>Delete Note</i>
-                        </button>
-                    </div>
-                    <div class="status1">
-                        <p>Barcode generated successfully: <em>http://ukprintplus.co.uk/? ws barcode=10530</em></p>
-                    </div>
-                    <div class="added d-flex" style=" border-bottom: 1px solid gainsboro; padding-bottom: 20px;">
-                        <span>added on October 15 2018 at 5:33 am by Haseeb</span>
-                        <button class="btn btn-primary">
-                            <i>Delete Note</i>
-                        </button>
+                        <div class="status-outer">
+                            <div class="status1">
+                                <p>Awaiting BACS payment Order status changed from Pending Payment to On Hold.</p>
+                            </div>
+                            <div class="added d-flex">
+                                <span>added on October 15 2018 at 5:33 am by Haseeb</span>
+                                <button class="btn btn-primary">
+                                    <i>Delete Note</i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="status-outer">
+                            <div class="status1">
+                                <p>Barcode generated successfully: <em>http://ukprintplus.co.uk/? ws barcode=10530</em></p>
+                            </div>
+                            <div class="added d-flex" style=" border-bottom: 1px solid gainsboro; padding-bottom: 20px;">
+                                <span>added on October 15 2018 at 5:33 am by Haseeb</span>
+                                <button class="btn btn-primary">
+                                    <i>Delete Note</i>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                     <div class="add-note">
                         <h6>Add note</h6>
@@ -229,6 +335,7 @@
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
 
