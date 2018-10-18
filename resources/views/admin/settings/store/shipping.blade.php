dd6518617c1a800e60b8b59626589b8c7f722d97
Hovo <hovoaep@gmail.com>
2018-10-18 17:55:36 +0400

    fix js

--- resources/views/admin/settings/store/shipping.blade.php
+++ resources/views/admin/settings/store/shipping.blade.php
@@ -438,16 +438,96 @@
         })
 
         $('body').on('change','#ShippingZones',function (e) {
+            console.log(1111)
             e.preventDefault();
             let val = $(this).val();
+            let text = $(this).closest("tr").find("#ShippingZones :selected").text();
             let id = $(this).closest("tr").attr("data-table-id")
 
-            let text = $(this).closest("tr").find("#ShippingZones :selected").text();
+            let html2 = `
+<table class="table table-responsive table--store-settings container-for-table-remove" data-table-id="${id}">
+                <tr class="bg-my-light-blue">
+                <td>Shipping Zone - <span class="shipzone">${val}</span></td>
+                <td colspan="3">Tax Rate - <span class="taxzone">${text}</span></td>
+                <td colspan="2" class="text-right"><button type="button" data-table-id="${id}" class="btn btn-primary delete-all-option"><i class="fa fa-trash"></i></button></span></td>
+                    </tr>
+                    <tbody>
 
-            console.log($(`table[data-table-id="${id}"]`))
-            $(`table[data-table-id="${id}"]`).find('.shipzone').text(text);
-            console.log($(`table[data-table-id="${id}"]`).find('.shipzone').text())
-            $(`table[data-table-id="${id}"]`).find('.taxzone').text(val);
+                    <tr class="bg-my-light-pink">
+                        <th>Order Amount</th>
+                        <th>Courier</th>
+                        <th>Cost</th>
+                        <th colspan="3">Time</th>
+                    </tr>
+                    <tr>
+                        <td class="table--store-settings_vert-top">
+                            <input type="number" min="1" max="5" class="form-control" style="display: inline-block; width: auto">
+                            <span>To</span>
+                            <input type="number" min="1" max="50" class="form-control" style="display: inline-block; width: auto">
+                        </td>
+                        <td>
+                            <select id="PosType" class="form-control">
+                                <option selected>Normal Post</option>
+                                <option>...</option>
+                            </select>
+                        </td>
+                        <td>
+                            <span class="form-control">
+                                5
+                            </span>
+                        </td>
+                        <td>
+                            <span class="form-control">
+                                3 days
+                            </span>
+                        </td>
+                        <td colspan="2" class="text-right">
+                            <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <select id="dhl" class="form-control">
+                                <option selected>DHL</option>
+                                <option>...</option>
+                            </select>
+                        </td>
+                        <td>
+                            <span class="form-control">
+                                5
+                            </span>
+                        </td>
+                        <td>
+                            <span class="form-control">
+                                1 day
+                            </span>
+                        </td>
+                        <td colspan="2" class="text-right">
+                            <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
+                        </td>
+                    </tr>
+                    <tr class="add-new-ship-filed-container">
+                        <td colspan="6" class="text-right">
+                            <button type="button" class="btn btn-primary add-new-ship-filed"><i class="fa fa-plus-circle"></i></button>
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td colspan="5" class="text-center table--store-settings_add-options add-new-order-filed">
+                            <span><i class="fa fa-plus"></i></span> Add more option
+                        </td>
+                    </tr>
+                    </tfoot>
+                </table>`
+
+
+            // console.log($(`table[data-table-id="${id}"]`))
+            // $(`table[data-table-id="${id}"]`).find('.shipzone').text(text);
+            // console.log($(`table[data-table-id="${id}"]`).find('.shipzone').text())
+            // $(`table[data-table-id="${id}"]`).find('.taxzone').text(val);
+            $("#myTabContent").append(html2)
 
         });
     </script>
