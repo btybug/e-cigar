99fdef5ec39e747847ca4d6e24c5c9bb68a55e90
Nune <hakobyan.sahak88@gmail.com>
2018-10-23 15:50:56 +0400

    cleaning html

--- resources/views/frontend/common/support.blade.php
+++ resources/views/frontend/common/support.blade.php
@@ -6,8 +6,8 @@
             <h1 class="mb-5 text-uppercase" style="color: #F0CA4D">Support</h1>
             <div class="row">
                 <a href="{!! route('faq') !!}" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
-                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-file-text" aria-hidden="true"></i></span>
-                        <strong>FAQ</strong>
+                    <span class="d-inline-block mb-3"><i class="fa fa-5x fa-file-text" aria-hidden="true"></i></span>
+                    <strong>FAQ</strong>
                     <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, sequi..</span>
                 </a>
                 <a href="{!! route('knowledge_base') !!}" class="col-sm-3 text-center mb-3 px-5 d-flex flex-column d-block">
@@ -42,7 +42,6 @@
                 </a>
 
             </div>
-            <div class="clearbox"></div>
         </div>
     </section>
 </div>
