583e1194d8908d42a39a2aab5a4b35ce8556ce85
Nune <hakobyan.sahak88@gmail.com>
2018-10-24 12:43:59 +0400

    manuals ui

--- resources/views/frontend/guest/manuals.blade.php
+++ resources/views/frontend/guest/manuals.blade.php
@@ -1,4 +1,53 @@
 @extends('layouts.frontend')
 @section('content')
+    <div class="container">
+        <section class="qcbox_section">
+            <div class="product-introduce">
+                <h1 class="mb-5 text-uppercase" style="color: #F0CA4D">download</h1>
+                <div class="row">
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                    <a href="#" class="col-sm-3 text-center mb-4 px-5 d-flex flex-column d-block">
+                        <span class="d-inline-block mb-3"><i class="fa fa-5x fa-download" aria-hidden="true"></i></span>
+                        <span style="color: #F0CA4D">Aspire Zelos 50W 2.0 Kit User Manual</span>
+                    </a>
+                </div>
+            </div>
+        </section>
+    </div>
 
 @stop
