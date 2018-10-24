9949f106e9d6f0ada282dba8b912fce816c4534a
Nune <hakobyan.sahak88@gmail.com>
2018-10-24 15:03:40 +0400

    favorites ui

--- resources/views/frontend/my_account/favourites.blade.php
+++ resources/views/frontend/my_account/favourites.blade.php
@@ -6,6 +6,107 @@
                 @include('frontend.my_account._partials.left_menu',['activeItem' => 'my_account_favourites'])
             </div>
             <div class="col-md-8">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="view-fav-item mb-4">
+                            <div class="text-right">
+                                <button class="btn btn-danger view-fav-item_btn-dlt" href="#"><i class="fa fa-trash"></i></button>
+                            </div>
+                            <a href="http://e-cigar.loc/products/vape/10"  class="text-center  px-5 py-3 d-flex flex-column d-block">
+                                <span class="d-inline-block mb-3 view-fav-item_img-outer">
+                                    <img class="img-fluid"
+                                         src="http://e-cigar.loc/public/media/drive/53b44d8993ca974c87170c051232bada.jpg"
+                                         alt="CLASSIC FIT SOFT-TOUCH POLO">
+                                </span>
+                                <span style="color: #F0CA4D">Product Name</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="view-fav-item mb-4">
+                            <div class="text-right">
+                                <button class="btn btn-danger view-fav-item_btn-dlt" href="#"><i class="fa fa-trash"></i></button>
+                            </div>
+                            <a href="http://e-cigar.loc/products/vape/7"  class="text-center  px-5 py-3 d-flex flex-column d-block">
+                                <span class="d-inline-block mb-3 view-fav-item_img-outer">
+                                    <img class="img-fluid"
+                                         src="http://e-cigar.loc/public/media/drive/af1d8604163611f0cf6da924324acc7a.jpg"
+                                         alt="CLASSIC FIT SOFT-TOUCH POLO">
+                                </span>
+                                <span style="color: #F0CA4D">Product Name</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="view-fav-item mb-4">
+                            <div class="text-right">
+                                <button class="btn btn-danger view-fav-item_btn-dlt" href="#"><i class="fa fa-trash"></i></button>
+                            </div>
+                            <a href="http://e-cigar.loc/products/vape/6" class="text-center  px-5 py-3 d-flex flex-column d-block">
+                                <span class="d-inline-block mb-3 view-fav-item_img-outer">
+                                    <img class="img-fluid"
+                                         src="http://e-cigar.loc/public/media/drive/4e86f817e1de91452bfaf79824c4205c.jpg"
+                                         alt="CLASSIC FIT SOFT-TOUCH POLO">
+                                </span>
+                                <span style="color: #F0CA4D">Product Name</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="view-fav-item mb-4">
+                            <div class="text-right">
+                                <button class="btn btn-danger view-fav-item_btn-dlt" href="#"><i class="fa fa-trash"></i></button>
+                            </div>
+                            <a href="http://e-cigar.loc/products/vape/10"  class="text-center  px-5 py-3 d-flex flex-column d-block">
+                                <span class="d-inline-block mb-3 view-fav-item_img-outer">
+                                    <img class="img-fluid"
+                                         src="http://e-cigar.loc/public/media/drive/53b44d8993ca974c87170c051232bada.jpg"
+                                         alt="CLASSIC FIT SOFT-TOUCH POLO">
+                                </span>
+                                <span style="color: #F0CA4D">Product Name</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="view-fav-item mb-4">
+                            <div class="text-right">
+                                <button class="btn btn-danger view-fav-item_btn-dlt" href="#"><i class="fa fa-trash"></i></button>
+                            </div>
+                            <a href="http://e-cigar.loc/products/vape/7"  class="text-center  px-5 py-3 d-flex flex-column d-block">
+                                <span class="d-inline-block mb-3 view-fav-item_img-outer">
+                                    <img class="img-fluid"
+                                         src="http://e-cigar.loc/public/media/drive/af1d8604163611f0cf6da924324acc7a.jpg"
+                                         alt="CLASSIC FIT SOFT-TOUCH POLO">
+                                </span>
+                                <span style="color: #F0CA4D">Product Name</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <div class="view-fav-item mb-4">
+                            <div class="text-right">
+                                <button class="btn btn-danger view-fav-item_btn-dlt" href="#"><i class="fa fa-trash"></i></button>
+                            </div>
+                            <a href="http://e-cigar.loc/products/vape/6" class="text-center  px-5 py-3 d-flex flex-column d-block">
+                                <span class="d-inline-block mb-3 view-fav-item_img-outer">
+                                    <img class="img-fluid"
+                                         src="http://e-cigar.loc/public/media/drive/4e86f817e1de91452bfaf79824c4205c.jpg"
+                                         alt="CLASSIC FIT SOFT-TOUCH POLO">
+                                </span>
+                                <span style="color: #F0CA4D">Product Name</span>
+                            </a>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
 
             </div>
         </div>
