5ade794407cf2335f61cb190654f42b42e6378c7
Sahakavatar <hakobyan.sahak88@gmail.com>
2018-10-27 22:56:11 +0400

    up

--- app/Http/Middleware/ActivityMiddleware.php
+++ app/Http/Middleware/ActivityMiddleware.php
@@ -11,24 +11,27 @@ class ActivityMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        try {
-            $log = geoip($request->ip())->toArray();
-            unset($log['default']);
-            unset($log['cached']);
-            $log['subject'] = '';
-            $log['url'] = $request->url();
-            $log['method'] = $request->method();
-            $log['ip'] = $request->ip();
-            $log['agent'] = $request->header('user-agent');
-            $log['user_id'] = \Auth::id() ;
-            LogActivities::create($log);
-        } catch (\Exception $e) {
+        if (env('ACTIVITY_LOG', false)) {
+            try {
+                $log = geoip($request->ip())->toArray();
+                unset($log['default']);
+                unset($log['cached']);
+                $log['subject'] = '';
+                $log['url'] = $request->url();
+                $log['method'] = $request->method();
+                $log['ip'] = $request->ip();
+                $log['agent'] = $request->header('user-agent');
+                $log['user_id'] = \Auth::id();
+                LogActivities::create($log);
+            } catch (\Exception $e) {
+dd($e->getMessage());
+            }
         }
         return $next($request);
     }
