cb180b90d629d3389a4375caa31a156a1a5e2a0a
Nune <hakobyan.sahak88@gmail.com>
2018-10-29 13:02:58 +0400

    cart page ui

--- resources/views/frontend/shop/cart.blade.php
+++ resources/views/frontend/shop/cart.blade.php
@@ -22,10 +22,11 @@
                                     <table class="table">
                                         <thead>
                                         <tr>
-                                            <th align="left">Item(s)</th>
-                                            <th align="right">Price</th>
+                                            <th align="left">Product</th>
+                                            <th align="left">Options</th>
                                             <th align="right">Qty</th>
-                                            <th align="right">Sub Total</th>
+                                            <th align="right">Price</th>
+                                            <th align="right">Subtotal</th>
                                         </tr>
                                         </thead>
 
@@ -37,22 +38,44 @@
                                                 <a href="http://demo.laravelcommerce.com/product-detail/custom-slim-fit-weathered-polo"
                                                    class="cart-thumb">
                                                     <img class="img-fluid"
-                                                         src="http://demo.laravelcommerce.com/resources/assets/images/product_images/1502177321.pPOLO2-26314634_standard_v400.jpg"
+                                                         src="http://core.bootydev.co.uk/public/media/drive/3f44266c7fa59df324ec315e97e8579c.jpg"
                                                          alt="CUSTOM SLIM FIT WEATHERED POLO" alt="">
                                                 </a>
-                                                <div class="cart-product-detail">
-                                                    <a href="http://demo.laravelcommerce.com/product-detail/custom-slim-fit-weathered-polo"
-                                                       class="title">
-                                                        CUSTOM SLIM FIT WEATHERED POLO
-                                                    </a>
-                                                    <ul>
-                                                        <li>Size<span>Small</span></li>
-                                                    </ul>
-                                                </div>
                                             </td>
 
-                                            <td align="right" class="price"><span>$89.5</span></td>
-                                            <td align="right" class="Qty">
+                                            <td align="right" class="options">
+                                                <div class="input-group">
+                                                    <div class="form-group row align-items-center">
+                                                        <label class="mr-2" for="color1">Colors</label>
+                                                        <select class="form-control" name="color1" id="colors">
+                                                            <option value="blue">blue</option>
+                                                            <option value="red">red</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="input-group">
+                                                    <div class="form-group row align-items-center">
+                                                        <label class="mr-2" for="color2">Colors</label>
+                                                        <select class="form-control" name="color2" id="colors">
+                                                            <option value="blue">blue</option>
+                                                            <option value="red">red</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+
+                                            </td>
+                                            <td align="right" class="Qty" rowspan="2">
+                                                <div class="input-group mb-4">
+                                                  <span class="input-group-btn qtyminus_4925">
+                                                    	<i class="fa fa-minus" aria-hidden="true"></i>
+                                                  </span>
+                                                    <input name="quantity[]" type="text" readonly value="1"
+                                                           class="form-control qty">
+                                                    <span class="input-group-btn qtypluscart_4925">
+                                                  		<i class="fa fa-plus" aria-hidden="true"></i>
+                                                  </span>
+                                                </div>
                                                 <div class="input-group">
                                                   <span class="input-group-btn qtyminus_4925">
                                                     	<i class="fa fa-minus" aria-hidden="true"></i>
@@ -65,8 +88,81 @@
                                                 </div>
                                             </td>
 
-                                            <td align="right" class="subtotal"><span class="cart_price_4925">$89.5</span>
+                                            <td align="right" class="price">
+                                                <span class="d-block cart_price_4925 mb-4">$89.5</span>
+                                                <span class="d-block cart_price_4925">$59.5</span>
                                             </td>
+                                            <td align="right" class="subtotal" style="vertical-align: middle"><span>$140</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="4" class="buttons">
+                                                <a href="http://demo.laravelcommerce.com/editcart?id=4925"
+                                                   class="btn btn-sm btn-secondary">Edit</a>
+                                                <a href="http://demo.laravelcommerce.com/deleteCart?id=4925"
+                                                   class="btn btn-sm btn-secondary">Remove Item</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td align="left" class="item">
+                                                <input type="hidden" name="cart[]" value="4925">
+                                                <a href="http://demo.laravelcommerce.com/product-detail/custom-slim-fit-weathered-polo"
+                                                   class="cart-thumb">
+                                                    <img class="img-fluid"
+                                                         src="http://core.bootydev.co.uk/public/media/drive/3f44266c7fa59df324ec315e97e8579c.jpg"
+                                                         alt="CUSTOM SLIM FIT WEATHERED POLO" alt="">
+                                                </a>
+                                            </td>
+
+                                            <td align="right" class="options">
+                                                <div class="input-group">
+                                                    <div class="form-group row align-items-center">
+                                                        <label class="mr-2" for="color1">Colors</label>
+                                                        <select class="form-control" name="color1" id="colors">
+                                                            <option value="blue">blue</option>
+                                                            <option value="red">red</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="input-group">
+                                                    <div class="form-group row align-items-center">
+                                                        <label class="mr-2" for="color2">Colors</label>
+                                                        <select class="form-control" name="color2" id="colors">
+                                                            <option value="blue">blue</option>
+                                                            <option value="red">red</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+
+                                            </td>
+                                            <td align="right" class="Qty" rowspan="2">
+                                                <div class="input-group mb-4">
+                                                  <span class="input-group-btn qtyminus_4925">
+                                                    	<i class="fa fa-minus" aria-hidden="true"></i>
+                                                  </span>
+                                                    <input name="quantity[]" type="text" readonly value="1"
+                                                           class="form-control qty">
+                                                    <span class="input-group-btn qtypluscart_4925">
+                                                  		<i class="fa fa-plus" aria-hidden="true"></i>
+                                                  </span>
+                                                </div>
+                                                <div class="input-group">
+                                                  <span class="input-group-btn qtyminus_4925">
+                                                    	<i class="fa fa-minus" aria-hidden="true"></i>
+                                                  </span>
+                                                    <input name="quantity[]" type="text" readonly value="1"
+                                                           class="form-control qty">
+                                                    <span class="input-group-btn qtypluscart_4925">
+                                                  		<i class="fa fa-plus" aria-hidden="true"></i>
+                                                  </span>
+                                                </div>
+                                            </td>
+
+                                            <td align="right" class="price">
+                                                <span class="d-block cart_price_4925 mb-4">$89.5</span>
+                                                <span class="d-block cart_price_4925">$59.5</span>
+                                            </td>
+                                            <td align="right" class="subtotal" style="vertical-align: middle"><span>$140</span></td>
                                         </tr>
                                         <tr>
                                             <td colspan="4" class="buttons">
@@ -78,53 +174,7 @@
                                         </tr>
                                         </tbody>
 
-                                        <tbody>
-                                        <tr>
-                                            <td align="left" class="item">
-                                                <input type="hidden" name="cart[]" value="4926">
-                                                <a href="http://demo.laravelcommerce.com/product-detail/custom-slim-fit-cotton-shirt"
-                                                   class="cart-thumb">
-                                                    <img class="img-fluid"
-                                                         src="http://demo.laravelcommerce.com/resources/assets/images/product_images/1502176579.pPOLO2-26316348_standard_v400.jpg"
-                                                         alt="CUSTOM SLIM FIT COTTON SHIRT" alt="">
-                                                </a>
-                                                <div class="cart-product-detail">
-                                                    <a href="http://demo.laravelcommerce.com/product-detail/custom-slim-fit-cotton-shirt"
-                                                       class="title">
-                                                        CUSTOM SLIM FIT COTTON SHIRT
-                                                    </a>
-                                                    <ul>
-                                                        <li>Size<span>Small</span></li>
-                                                    </ul>
-                                                </div>
-                                            </td>
 
-                                            <td align="right" class="price"><span>$98.5</span></td>
-                                            <td align="right" class="Qty">
-                                                <div class="input-group">
-                                                  <span class="input-group-btn qtyminus_4926">
-                                                    	<i class="fa fa-minus" aria-hidden="true"></i>
-                                                  </span>
-                                                    <input name="quantity[]" type="text" readonly value="1"
-                                                           class="form-control qty">
-                                                    <span class="input-group-btn qtypluscart_4926">
-                                                  		<i class="fa fa-plus" aria-hidden="true"></i>
-                                                  </span>
-                                                </div>
-                                            </td>
-
-                                            <td align="right" class="subtotal"><span class="cart_price_4926">$98.5</span>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="4" class="buttons">
-                                                <a href="http://demo.laravelcommerce.com/editcart?id=4926"
-                                                   class="btn btn-sm btn-secondary">Edit</a>
-                                                <a href="http://demo.laravelcommerce.com/deleteCart?id=4926"
-                                                   class="btn btn-sm btn-secondary">Remove Item</a>
-                                            </td>
-                                        </tr>
-                                        </tbody>
                                     </table>
                                 </div>
                             </form>
