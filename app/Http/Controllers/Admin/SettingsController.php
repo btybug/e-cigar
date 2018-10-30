c3fd05f97b10de206083f74a20c7f3e87c7d698e
Sahakavatar <hakobyan.sahak88@gmail.com>
2018-10-30 12:16:30 +0400

    up

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -204,7 +204,7 @@ class SettingsController extends Controller
     {
         $countries = new Countries();
         $regions = ['all_selected' => 'All Regions'] + $countries->whereNameCommon($request->get('country'))->first()->hydrateStates()->states->pluck('name', 'name')->toArray();
-        $html = \Form::select('region[' . $request->get('count') . ']', $regions, null, ['class' => 'form-control region'])->toHtml();
+        $html = \Form::select('region[' . $request->get('count') . ']', $regions, null, ['class' => 'form-control region','multiple'=>'multiple'])->toHtml();
         return ['error' => false, 'html' => $html];
     }
 
