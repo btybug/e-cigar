583020f810d9cce9f08e135cd051bbbf8946e40c
Nune <hakobyan.sahak88@gmail.com>
2018-11-02 12:10:07 +0400

    my account left column ui fixings

--- resources/views/frontend/my_account/index.blade.php
+++ resources/views/frontend/my_account/index.blade.php
@@ -6,131 +6,110 @@
                 @include('frontend.my_account._partials.left_menu',['activeItem' => 'my_account'])
             </div>
             <div class="col-md-8">
-                <div class="my-account">
+                <div class="my-account p-5 card">
                     <div class="container">
-                       {!! Form::model($user) !!}
-                            <div class="form-row">
-                                <div class="col-sm-6">
-                                    <div class="form-group row">
-                                        <label for="username" class="col-sm-4">
-                                            First Name
-                                            <span class="required">*</span>
-                                        </label>
-                                        <div class="col-sm-8">
-                                            {!! Form::text('name',null,['class'=>'form-control']) !!}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group row">
-                                        <label for="username" class="col-sm-4">
-                                            Last Name
-                                            <span class="required">*</span>
-                                        </label>
-                                        <div class="col-sm-8">
-                                            {!! Form::text('last_name',null,['class'=>'form-control']) !!}
-                                        </div>
-                                    </div>
+                        {!! Form::model($user) !!}
+                        <div class="mb-4">
+                            <div class="form-group row">
+                                <label for="username" class="col-md-4">
+                                    First Name
+                                    <span class="required text-danger">*</span>
+                                </label>
+                                <div class="col-md-8">
+                                    {!! Form::text('name',null,['class'=>'form-control']) !!}
                                 </div>
                             </div>
-                            <div class="row mail mt-3">
-                                <label for="username" class="col-sm-2">
+                            <div class="form-group row">
+                                <label for="username" class="col-md-4">
+                                    Last Name
+                                    <span class="required text-danger">*</span>
+                                </label>
+                                <div class="col-md-8">
+                                    {!! Form::text('last_name',null,['class'=>'form-control']) !!}
+                                </div>
+                            </div>
+                            <div class="form-group row mail">
+                                <label for="username" class="col-md-4">
                                     Email address
-                                    <span class="required">*</span>
+                                    <span class="required text-danger">*</span>
                                 </label>
 
-                                <div class="col-sm-10">
+                                <div class="col-md-8">
                                     {!! Form::email('email',null,['class'=>'form-control','id'=>'exampleInputEmail1','aria-describedby'=>"emailHelp"]) !!}
                                 </div>
                             </div>
-                        <div class="form-row mt-3">
-                            <div class="col-sm-4">
-                                <div class="form-group row">
-                                    <label for="username" class="col-sm-4">
-                                        Gender
-                                        <span class="required">*</span>
-                                    </label>
-                                    <div class="col-sm-8">
-                                        {!! Form::text('gender',null,['class'=>'form-control']) !!}
-                                    </div>
+                            <div class="form-group row">
+                                <label for="username" class="col-md-4">
+                                    Gender
+                                    <span class="required text-danger">*</span>
+                                </label>
+                                <div class="col-md-8">
+                                    {!! Form::text('gender',null,['class'=>'form-control']) !!}
                                 </div>
                             </div>
-                            <div class="col-sm-4">
-                                <div class="form-group row">
-                                    <label for="username" class="col-sm-3">
-                                        Country
-                                        <span class="required">*</span>
-                                    </label>
-                                    <div class="col-sm-8">
-                                        {!! Form::text('country',null,['class'=>'form-control']) !!}
-                                    </div>
+                            <div class="form-group row">
+                                <label for="username" class="col-md-4">
+                                    Country
+                                    <span class="required text-danger">*</span>
+                                </label>
+                                <div class="col-md-8">
+                                    {!! Form::text('country',null,['class'=>'form-control']) !!}
                                 </div>
                             </div>
-                            <div class="col-sm-4">
-                                <div class="form-group row">
-                                    <label for="username" class="col-sm-4">
-                                        Phone
-                                        <span class="required">*</span>
-                                    </label>
-                                    <div class="col-sm-8">
-                                        {!! Form::text('phone',null,['class'=>'form-control']) !!}
-                                    </div>
+                            <div class="form-group row">
+
+                                <label for="username" class="col-md-4">
+                                    Phone
+                                    <span class="required text-danger">*</span>
+                                </label>
+                                <div class="col-md-8">
+                                    {!! Form::text('phone',null,['class'=>'form-control']) !!}
                                 </div>
                             </div>
                         </div>
                         {!! Form::close() !!}
-                        <div class="form-bg mt-5 ">
-                            <div class="change">
-                                <span>Password Change</span>
-                            </div>
-                            <div class="row username">
-                                <label for="username" class="col-sm-2">
+                        <div>
+                            <h3 class="mb-4">Password Change</h3>
+                            <div class="form-group row username">
+                                <label for="username" class="col-md-4">
                                     Current Password
                                 </label>
-                                <div class="col-sm-10">
+                                <div class="col-md-8">
                                     <input type="password" class="form-control" id="exampleInputPassword1">
                                 </div>
                             </div>
-                            <div class="row confirm mt-2">
-                                <div class="col-sm-6">
-                                    <div class="row">
-                                        <label for="username" class="col-sm-4">
-                                            New Password
-                                        </label>
-                                        <div class="col-sm-8">
-                                            <input type="password" class="form-control" id="exampleInputPassword2">
-                                        </div>
-                                    </div>
-
-
+                            <div class="form-group row confirm">
+                                <label for="username" class="col-md-4">
+                                    New Password
+                                </label>
+                                <div class="col-md-8">
+                                    <input type="password" class="form-control" id="exampleInputPassword2">
                                 </div>
-                                <div class="col-sm-6">
-                                    <div class="row">
-                                        <label for="username" class="one col-sm-4">
-                                            Confirm New Password
-                                        </label>
-                                        <div class="col-sm-8">
-                                            <input type="password" class="form-control" id="exampleInputPassword3">
-                                        </div>
-                                    </div>
-
-
+                            </div>
+                            <div class="form-group row confirm">
+                                <label for="username" class="one col-md-4">
+                                    Confirm New Password
+                                </label>
+                                <div class="col-md-8">
+                                    <input type="password" class="form-control" id="exampleInputPassword3">
                                 </div>
+                            </div>
+                            <div class="form-group row avatar align-items-center mb-4 border-top border-bottom py-3">
+                                <span class="col-md-4">Avatar</span>
+                                <div class="col-md-8">
+                                    <img width="150" src="/public/images/{!!$user->gender!!}.png" alt="">
+                                    <div>
+                                        <button type="button" class="btn btn-secondary">Change Avatar</button>
+                                    </div>
+                                </div>
+                            </div>
 
-
+                            <div>
+                                <input type="submit" class="btn btn-primary" value="Save changes">
                             </div>
                         </div>
-                        <div class="avatar d-flex">
-                            <span>Avatar</span>
-                            <img src="/public/images/{!!$user->gender!!}.png" alt="">
-                        </div>
-                        <div class="bat">
-                            <button type="button" class="btn btn-primary">Change Avatar</button>
-                        </div>
-                        <div class="log mt-5">
-                            <input type="submit" class="button" name="login" value="Save changes">
-                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -139,5 +118,5 @@
 
 @stop
 @section('css')
-    <link rel="stylesheet" href="{{asset('public/frontend/css/my-account.css?v='.rand(111,999))}}">
+    {{--<link rel="stylesheet" href="{{asset('public/frontend/css/my-account.css?v='.rand(111,999))}}">--}}
 @stop
