91f9aa33a17e999a0654a0ca52b723320fea4339
sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-02 10:23:17 +0400

    stock page

--- resources/views/admin/inventory/stock_new.blade.php
+++ resources/views/admin/inventory/stock_new.blade.php
@@ -4,7 +4,7 @@
 @stop
 @section('content')
     <section class="content-header">
-        <h1> Admin Profile </h1>
+        {{--<h1> Admin Profile </h1>--}}
         <ol class="breadcrumb">
             <li><a href="http://demo0.laravelcommerce.com/admin/dashboard/this_month"><i class="fa fa-dashboard"></i>
                     Dashboard</a></li>
@@ -15,38 +15,38 @@
     <section class="content stock-page">
 
         <div class="row">
-            <div class="col-md-3">
-                <!-- Profile Image -->
-                <div class="box box-primary mb-30">
-                    <div class="box-body box-profile">
-                        <img class="profile-user-img img-responsive img-circle"
-                             src="http://demo0.laravelcommerce.com/resources/views/admin/images/admin_profile/1539074891.42792796_2710973195795309_228747741981835264_n.png"
-                             alt="Václav profile picture">
+            {{--<div class="col-md-3">--}}
+                {{--<!-- Profile Image -->--}}
+                {{--<div class="box box-primary mb-30">--}}
+                    {{--<div class="box-body box-profile">--}}
+                        {{--<img class="profile-user-img img-responsive img-circle"--}}
+                             {{--src="http://demo0.laravelcommerce.com/resources/views/admin/images/admin_profile/1539074891.42792796_2710973195795309_228747741981835264_n.png"--}}
+                             {{--alt="Václav profile picture">--}}
 
-                        <h3 class="profile-username text-center">Václav Kutiš</h3>
+                        {{--<h3 class="profile-username text-center">Václav Kutiš</h3>--}}
 
-                        <p class="text-muted text-center">Administrator</p>
+                        {{--<p class="text-muted text-center">Administrator</p>--}}
 
-                        <!-- <ul class="list-group list-group-unbordered">
-                           <li class="list-group-item">
-                             <b>Followers</b> <a class="pull-right">1,322</a>
-                           </li>
-                           <li class="list-group-item">
-                             <b>Following</b> <a class="pull-right">543</a>
-                           </li>
-                           <li class="list-group-item">
-                             <b>Friends</b> <a class="pull-right">13,287</a>
-                           </li>
-                         </ul>
+                        {{--<!-- <ul class="list-group list-group-unbordered">--}}
+                           {{--<li class="list-group-item">--}}
+                             {{--<b>Followers</b> <a class="pull-right">1,322</a>--}}
+                           {{--</li>--}}
+                           {{--<li class="list-group-item">--}}
+                             {{--<b>Following</b> <a class="pull-right">543</a>--}}
+                           {{--</li>--}}
+                           {{--<li class="list-group-item">--}}
+                             {{--<b>Friends</b> <a class="pull-right">13,287</a>--}}
+                           {{--</li>--}}
+                         {{--</ul>--}}
 
-                         <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>-->
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
+                         {{--<a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>-->--}}
+                    {{--</div>--}}
+                    {{--<!-- /.box-body -->--}}
+                {{--</div>--}}
+                {{--<!-- /.box -->--}}
 
-                <!-- /.box -->
-            </div>
+                {{--<!-- /.box -->--}}
+            {{--</div>--}}
             <div class="col-md-12">
                 <ul class="nav nav-tabs admin-profile-left">
                     <li class="active"><a data-toggle="tab" href="#basic">Basic Details</a></li>
