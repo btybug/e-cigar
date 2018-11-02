8adc453b204f6b2be682c450ba3ed8f66345be03
Sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-02 14:01:34 +0400

    update

--- resources/views/admin/orders/manage.blade.php
+++ resources/views/admin/orders/manage.blade.php
@@ -192,8 +192,6 @@
                                         Country:{!! getCountryByZone($order->shippingAddress->country)->name !!}<br>
                                         Region:{!! getCountryByZone($order->shippingAddress->country)->region->name !!}
                                         <br>
-                                        City:{!! getCountryByZone($order->shippingAddress->country)->region->name !!}
-                                        <br>
                                         First line:{!! $order->shippingAddress->first_line_address !!}<br>
                                         Second line:{!! $order->shippingAddress->second_line_address !!}
                                         Post code:{!! $order->shippingAddress->post_code !!}
