5fa28ada7a1db91aca66c79f28bd5a8901701ee7
Sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-05 09:57:30 +0400

    update

--- resources/views/admin/inventory/statuses.blade.php
+++ resources/views/admin/inventory/statuses.blade.php
@@ -8,39 +8,74 @@
             <div class="col-md-6 pull-left"><h2>Statuses</h2></div>
             <div class="col-md-6 "><a class="btn btn-primary pull-right" href="{!! route('admin_stock_statuses_manage') !!}">Add new</a></div>
         </div>
-        <div class="col-xs-12">
-            <table id="stocks-table" class="table table-style table-bordered" cellspacing="0" width="100%">
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>type</th>
-                    <th>Description</th>
-                    <th>icon</th>
-                    <th>Color</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-            </table>
+
+        <div class="payment_gateways_tab">
+            <ul class="list_paymant">
+                <li class="item">
+                    <div class="chek-title">
+
+                        <label for="cash_paymant" class="title">Order</label>
+                    </div>
+                    <a href="{!! '#' !!}" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i></a>
+                </li>
+                <li class="item">
+                    <div class="chek-title">
+                        <label for="stripe_paymant"  class="title">User</label>
+                    </div>
+                    <a href="{!! '#' !!}" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i></a>
+                </li>
+                <li class="item">
+                    <div class="chek-title">
+                        <label for="paypal" class="title">Posts</label>
+                    </div>
+                    <a href="#" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i></a>
+                </li>
+                <li class="item">
+                    <div class="chek-title">
+                        <label for="paypal" class="title">Stock</label>
+                    </div>
+                    <a href="#" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i></a>
+                </li>
+
+            </ul>
         </div>
+
+        {{--<div class="col-xs-12">--}}
+            {{--<table id="stocks-table" class="table table-style table-bordered" cellspacing="0" width="100%">--}}
+                {{--<thead>--}}
+                {{--<tr>--}}
+                    {{--<th>#</th>--}}
+                    {{--<th>Name</th>--}}
+                    {{--<th>type</th>--}}
+                    {{--<th>Description</th>--}}
+                    {{--<th>icon</th>--}}
+                    {{--<th>Color</th>--}}
+                    {{--<th>Actions</th>--}}
+                {{--</tr>--}}
+                {{--</thead>--}}
+            {{--</table>--}}
+        {{--</div>--}}
     </div>
 @stop
-@section('js')
-    <script>
-        $(function () {
-            $('#stocks-table').DataTable({
-                ajax: "{!! route('datatable_all_statuses') !!}",
-                columns: [
-                    {data: 'id', name: 'id'},
-                    {data: 'name', name: 'name'},
-                    {data: 'type', name: 'type'},
-                    {data: 'description', name: 'description'},
-                    {data: 'icon', name: 'icon'},
-                    {data: 'color', name: 'color'},
-                    {data: 'actions', name: 'actions'}
-                ]
-            });
-        });
-
-    </script>
+@section('css')
+    <link rel="stylesheet" href="{{asset('public/css/custom.css?v='.rand(111,999))}}">
 @stop
+{{--@section('js')--}}
+    {{--<script>--}}
+        {{--$(function () {--}}
+            {{--$('#stocks-table').DataTable({--}}
+                {{--ajax: "{!! route('datatable_all_statuses') !!}",--}}
+                {{--columns: [--}}
+                    {{--{data: 'id', name: 'id'},--}}
+                    {{--{data: 'name', name: 'name'},--}}
+                    {{--{data: 'type', name: 'type'},--}}
+                    {{--{data: 'description', name: 'description'},--}}
+                    {{--{data: 'icon', name: 'icon'},--}}
+                    {{--{data: 'color', name: 'color'},--}}
+                    {{--{data: 'actions', name: 'actions'}--}}
+                {{--]--}}
+            {{--});--}}
+        {{--});--}}
+
+    {{--</script>--}}
+{{--@stop--}}
