4d79a50744a3e0c5efdb9bdd5928531c2f708d93
Nune <hakobyan.sahak88@gmail.com>
2018-11-05 10:13:16 +0400

    juice page left items ui

--- resources/views/frontend/products/juice.blade.php
+++ resources/views/frontend/products/juice.blade.php
@@ -1,127 +1,143 @@
 @extends('layouts.frontend')
 @section('content')
 <div class="container">
-    <div class="col-md-8 mx-auto">
-        <div class="card juice-poster juice-poster--type-1 mb-5 bg-light">
-            <div class="row no-gutters">
-                <div class="col-md-5">
-                    <h3 class="card-title text-center mb-0 p-2 text-white">Kaliony Hit</h3>
-                    <div class="img-outer card-img-outer p-4">
-                        <img class="card-img-top" src="http://core.bootydev.co.uk/public/media/drive/3f44266c7fa59df324ec315e97e8579c.jpg" alt="">
-                    </div>
-                </div>
-
-                <div class="col-md-7">
-                    <div class="card-body p-0 h-100 d-flex flex-column">
-                        <div class="text-right">
-                            <a href="#" class="btn btn-primary">View more</a>
-                        </div>
-                        <div class="pt-4 px-3 text-justify">
-                            <p class="card-text mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dignissimos earum eum eveniet ex laudantium nobis. Atque dignissimos explicabo impedit ipsum laborum modi necessitatibus obcaecati optio quia quos. Quo!</p>
-                        </div>
-
-
-                        <div class="row no-gutters mt-auto">
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 1">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 2">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 3">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 4">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
+    <div class="row">
+        <div class="col-md-3">
+            <ul class="list-unstyled list-group">
+                <li class="list-group-item">
+                    <a href="#" class="d-inline-block w-100 text-center">item 1</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="#" class="d-inline-block w-100 text-center">item 2</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="#" class="d-inline-block w-100 text-center">item 3</a>
+                </li>
+            </ul>
         </div>
-        <div class="card juice-poster juice-poster--type-2 mb-5">
-            <div class="row no-gutters">
-                <div class="col-md-5">
-                    <h3 class="card-title text-center mb-0 p-2 text-white">Kaliony Cloud</h3>
-                    <div class="img-outer card-img-outer p-4">
-                        <img class="card-img-top" src="http://core.bootydev.co.uk/public/media/drive/3f44266c7fa59df324ec315e97e8579c.jpg" alt="">
+        <div class="col-md-9">
+            <div class="card juice-poster juice-poster--type-1 mb-5 bg-light">
+                <div class="row no-gutters">
+                    <div class="col-md-5">
+                        <h3 class="card-title text-center mb-0 p-2 text-white">Kaliony Hit</h3>
+                        <div class="img-outer card-img-outer p-4">
+                            <img class="card-img-top" src="http://core.bootydev.co.uk/public/media/drive/3f44266c7fa59df324ec315e97e8579c.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-md-7">
+                        <div class="card-body p-0 h-100 d-flex flex-column">
+                            <div class="text-right">
+                                <a href="#" class="btn btn-primary">View more</a>
+                            </div>
+                            <div class="pt-4 px-3 text-justify">
+                                <p class="card-text mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dignissimos earum eum eveniet ex laudantium nobis. Atque dignissimos explicabo impedit ipsum laborum modi necessitatibus obcaecati optio quia quos. Quo!</p>
+                            </div>
+
+
+                            <div class="row no-gutters mt-auto">
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 1">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 2">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 3">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 4">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-md-7">
-                    <div class="card-body p-0 h-100 d-flex flex-column">
-                        <div class="text-right">
-                            <a href="#" class="btn btn-primary">View more</a>
-                        </div>
-                        <div class="pt-4 px-3 text-justify">
-                            <p class="card-text mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dignissimos earum eum eveniet ex laudantium nobis. Atque dignissimos explicabo impedit ipsum laborum modi necessitatibus obcaecati optio quia quos. Quo!</p>
-                        </div>
 
-
-                        <div class="row no-gutters mt-auto">
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 1">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 2">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 3">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 4">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
+            <div class="card juice-poster juice-poster--type-2 mb-5">
+                <div class="row no-gutters">
+                    <div class="col-md-5">
+                        <h3 class="card-title text-center mb-0 p-2 text-white">Kaliony Cloud</h3>
+                        <div class="img-outer card-img-outer p-4">
+                            <img class="card-img-top" src="http://core.bootydev.co.uk/public/media/drive/3f44266c7fa59df324ec315e97e8579c.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-md-7">
+                        <div class="card-body p-0 h-100 d-flex flex-column">
+                            <div class="text-right">
+                                <a href="#" class="btn btn-primary">View more</a>
+                            </div>
+                            <div class="pt-4 px-3 text-justify">
+                                <p class="card-text mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dignissimos earum eum eveniet ex laudantium nobis. Atque dignissimos explicabo impedit ipsum laborum modi necessitatibus obcaecati optio quia quos. Quo!</p>
+                            </div>
 
 
-        </div>
-        <div class="card juice-poster juice-poster--type-3 mb-5">
-            <div class="row no-gutters">
-                <div class="col-md-5">
-                    <h3 class="card-title text-center mb-0 p-2 text-white">Kaliony Mix</h3>
-                    <div class="img-outer card-img-outer p-4">
-                        <img class="card-img-top" src="http://core.bootydev.co.uk/public/media/drive/dbeb80a1e5db49c639aea4138ec8caa7.png" alt="">
+                            <div class="row no-gutters mt-auto">
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 1">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 2">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 3">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 4">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-md-7">
-                    <div class="card-body p-0 h-100 d-flex flex-column">
-                        <div class="text-right">
-                            <a href="#" class="btn btn-primary">View more</a>
-                        </div>
-                        <div class="pt-4 px-3 text-justify">
-                            <p class="card-text mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dignissimos earum eum eveniet ex laudantium nobis. Atque dignissimos explicabo impedit ipsum laborum modi necessitatibus obcaecati optio quia quos. Quo!</p>
-                        </div>
 
-
-                        <div class="row no-gutters mt-auto">
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 1">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 2">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 3">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-                            <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 4">
-                                <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
             </div>
+            <div class="card juice-poster juice-poster--type-3 mb-5">
+                <div class="row no-gutters">
+                    <div class="col-md-5">
+                        <h3 class="card-title text-center mb-0 p-2 text-white">Kaliony Mix</h3>
+                        <div class="img-outer card-img-outer p-4">
+                            <img class="card-img-top" src="http://core.bootydev.co.uk/public/media/drive/dbeb80a1e5db49c639aea4138ec8caa7.png" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-md-7">
+                        <div class="card-body p-0 h-100 d-flex flex-column">
+                            <div class="text-right">
+                                <a href="#" class="btn btn-primary">View more</a>
+                            </div>
+                            <div class="pt-4 px-3 text-justify">
+                                <p class="card-text mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut dignissimos earum eum eveniet ex laudantium nobis. Atque dignissimos explicabo impedit ipsum laborum modi necessitatibus obcaecati optio quia quos. Quo!</p>
+                            </div>
 
 
+                            <div class="row no-gutters mt-auto">
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 1">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 2">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 3">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+                                <div class="col-md-3 flavor-img-outer p-3" data-title="Flavor 4">
+                                    <img class="img-fluid main-transition" src="https://images.pexels.com/photos/41957/banana-fruit-healthy-yellow-41957.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
     </div>
+
 
 </div>
 @stop
