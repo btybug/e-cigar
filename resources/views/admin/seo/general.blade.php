cdcf3de62297abfdc3a001b8bf3c7685b0d72e3d
sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-06 11:16:00 +0400

    seo panel

--- resources/views/admin/seo/general.blade.php
+++ resources/views/admin/seo/general.blade.php
@@ -21,4 +21,53 @@
 
         </div>
     </div>
+    <div class="container-fluid">
+        <div class="seo-page-general">
+            <div class="panel panel-default mt-20">
+                <div class="panel-heading">Posts</div>
+                <div class="panel-body">
+                    <div class="form-group">
+                        <div class="row">
+                            <label for="seo-title" class="col-md-2 col-xs-12">Title</label>
+                            <div class="col-md-5 col-xs-12">
+                                <input id="seo-title" type="text" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <label for="seo-desc" class="col-md-2 col-xs-12">Description</label>
+                            <div class="col-md-5 col-xs-12">
+                                <input id="seo-desc" type="text" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <label for="seo-keywords" class="col-md-2 col-xs-12">Focus keywords</label>
+                            <div class="col-md-5 col-xs-12">
+                                <input id="seo-keywords" type="text" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <label for="seo-meta-robots" class="col-md-2 col-xs-12">Meta Robots</label>
+                            <div class="col-md-5 col-xs-12">
+                                <select name="" id="seo-meta-robots" class="form-control">
+                                    <option value="">Index</option>
+                                    <option value="">Noindex</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     @stop
+@section('css')
+    <link rel="stylesheet" href="{{asset('public/css/custom.css?v='.rand(111,999))}}">
+@stop
