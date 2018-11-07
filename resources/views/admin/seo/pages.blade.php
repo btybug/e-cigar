f9cc7559a3127613b635e0161e635e2413f7799f
sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-07 15:16:05 +0400

    seo-page

--- resources/views/admin/seo/pages.blade.php
+++ resources/views/admin/seo/pages.blade.php
@@ -21,10 +21,92 @@
 
         </div>
     </div>
-    <div class="container-fluid">
+    <div class="seo-pages mt-20">
+        <div class="container-fluid">
+            <div class="row m-0">
+                <button class="btn btn-info pull-right">Save</button>
+            </div>
+            <div class="row mt-20">
+                <div class="col-md-3">
+                    <div class="seo-pages-left-list">
+                        <ul>
+                            <li>
+                                <a href="#" class="link">Item 1</a>
+                            </li>
+                            <li>
+                                <a href="#" class="link">Item 2</a>
+                                <ul>
+                                    <li>
+                                        <a href="#" class="sub-link">Sub Item 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="sub-link">Sub Item 1</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="link">Item 1</a>
+                            </li>
+                            <li>
+                                <a href="#" class="link">Item 3</a>
+                                <ul>
+                                    <li>
+                                        <a href="#" class="sub-link">Sub Item 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="sub-link">Sub Item 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="sub-link">Sub Item 3</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">General</div>
+                        <div class="panel-body">
+                            <div class="form-group">
+                                <div class="row">
+                                    <label for="seo-title" class="col-md-2 col-xs-12">Title</label>
+                                    <div class="col-md-5 col-xs-12">
+                                        {!! Form::text('go:title',null,['class'=>'form-control']) !!}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <label for="seo-desc" class="col-md-2 col-xs-12">Description</label>
+                                    <div class="col-md-5 col-xs-12">
+                                        {!! Form::text('go:description',null,['class'=>'form-control']) !!}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <label for="seo-keywords" class="col-md-2 col-xs-12">Focus keywords</label>
+                                    <div class="col-md-5 col-xs-12">
+                                        {!! Form::text('go:keywords',null,['class'=>'form-control']) !!}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <label for="seo-meta-robots" class="col-md-2 col-xs-12">Meta Robots</label>
+                                    <div class="col-md-5 col-xs-12">
+                                        {!! Form::select('robots',['1'=>'Index','0'=>'No Index'],isset($robot)?$robot->robots:null,['class'=>'form-control']) !!}
 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
 
 @stop
 @section('css')
