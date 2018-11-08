bd0c596a1c8ac391eeded300bc0c8190daa8a4f8
Nune <hakobyan.sahak88@gmail.com>
2018-11-08 16:33:19 +0400

    settings info tab ui

--- resources/views/admin/settings/general.blade.php
+++ resources/views/admin/settings/general.blade.php
@@ -14,8 +14,127 @@
                    aria-controls="general" aria-selected="true" aria-expanded="true">Accounts</a>
             </li>
         </ul>
-        <div class="" id="myTabContent">
+        <div class="nav-content">
+            <div class="tab-pane fade active in">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Basics</div>
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <label for="SiteName">Site Name</label>
+                            <input type="text" class="form-control" id="SiteName" aria-describedby="name" placeholder="Enter name">
+                        </div>
+                        <div class="form-group">
+                            <label for="siteLogo">Site Logo</label>
+                            <input type="file" class="form-control" id="siteLogo">
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea  class="form-control" id="description"  rows="5" aria-describedby="description" placeholder="Enter description"></textarea>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="panel panel-default">
+                    <div class="panel-heading">Address</div>
+                    <div class="panel-body">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="city">City</label>
+                                <select id="city" class="form-control">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="country">Country</label>
+                                <select id="country" class="form-control">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-offset-1 col-md-6">
+                            <div class="settings-map-outer"></div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">Oppening Hours</div>
+                    <div class="panel-body form-inline">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <table class="table">
+                                    <tr>
+                                        <td>
+                                            <div class="form-group">
+                                                <label for="weekday">Weekday</label>
+                                                <select id="weekday" class="form-control">
+                                                    <option selected>Choose...</option>
+                                                    <option>...</option>
+                                                </select>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="form-group">
+                                                <label for="hours">From</label>
+                                                <select id="hours" class="form-control">
+                                                    <option selected>Choose...</option>
+                                                    <option>00:00</option>
+                                                </select>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="form-group">
+                                                <label for="hours">To</label>
+                                                <select id="hours" class="form-control">
+                                                    <option selected>Choose...</option>
+                                                    <option>00:00</option>
+                                                </select>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <button type="button" class="btn btn-primary pull-right"><i class="fa fa-plus"></i></button>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">Holidays</div>
+                    <div class="row">
+                        <div class="col-md-7">
+                            <table class="table">
+                                <tr>
+                                    <td>
+                                        <select id="calendar" class="form-control">
+                                            <option selected>Calendar</option>
+                                            <option>.....</option>
+                                            <option>....</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" placeholder="Christmas">
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary pull-right"><i class="fa fa-plus"></i></button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+                </div>
         </div>
+
     </div>
 @stop
--- resources/views/frontend/my_account/ticket_view.blade.php
+++ resources/views/frontend/my_account/ticket_view.blade.php
@@ -2,77 +2,26 @@
 @section('content')
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-3">
                 @include('frontend.my_account._partials.left_menu',['activeItem' => 'my_account_tickets'])
             </div>
-            <div class="col-md-8">
+            <div class="col-md-6">
                 <div class="col-md-12 mb-4">
                     <a class="btn btn-primary" href="{!! route('my_account_tickets') !!}">Back</a>
                 </div>
                 <div class="col-md-12">
                     <div class="content">
-                        <div class="card well well-sm mb-5">
-                            <h2 class="header card-header">
-                                {{ $ticket->subject }}
-                                <span class="pull-right">
-                                    <a href="#"
-                                       class="btn btn-success">Mark Complete</a>
-                                                                </span>
-                            </h2>
-                            <div class="panel-body card-body">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <p><strong>Owner</strong>: {{ $ticket->author->name }}</p>
-                                        <p>
-                                            <strong>Status</strong>:
-                                            <span style="color: #e69900">{{ $ticket->status->name }}</span>
 
-                                        </p>
-                                        <p>
-                                            <strong>Priority</strong>:
-                                            <span style="color: #069900">{{ $ticket->priority->name }}</span>
-                                        </p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p><strong>Responsible</strong>: {{ ($ticket->staff) ? $ticket->staff->name : 'not assigned yet' }}</p>
-                                        <p>
-                                            <strong>Category</strong>:
-                                            <span style="color: #0014f4">{{ $ticket->category->name }}</span>
-                                        </p>
-                                        <p><strong>Created</strong>: {!! time_ago($ticket->created_at) !!}</p>
-                                        <p><strong>Last Update</strong>: {!! time_ago($ticket->updated_at) !!}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
-                        {{--files--}}
-                        <div class="row mb-4">
-                            <div class="col-sm-4">
-                                <a href="#" class="ticket-file-holder ticket-file-holder--img d-inline-block w-100 border">
-                                    <img src="https://images.pexels.com/photos/236111/pexels-photo-236111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                                </a>
-                            </div>
-                            <div class="col-sm-4">
-                                <a href="#" class="ticket-file-holder ticket-file-holder--img d-inline-block w-100 border">
-                                    <img src="https://images.pexels.com/photos/236111/pexels-photo-236111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                                </a>
-                            </div>
-                            <div class="col-sm-4">
-                                <a href="#" class="ticket-file-holder ticket-file-holder--img d-inline-block w-100 border">
-                                    <img src="https://images.pexels.com/photos/236111/pexels-photo-236111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
-                                </a>
-                            </div>
-                        </div>
 
                         <div class="row user-comment-img user-commmet-add">
-                            <div class="col-lg-1 col-md-1 hidden-xsd-none d-sm-block">
+                            <div class="col-lg-2 col-md-2 hidden-xsd-none d-sm-block">
                                 <figure class="thumbnail">
                                     <img class="img-fluid" src="http://www.tangoflooring.ca/wp-content/uploads/2015/07/user-avatar-placeholder.png">
                                     <figcaption class="text-center">User</figcaption>
                                 </figure>
                             </div>
-                            <div class="col-lg-11 col-md-11 comment-list">
+                            <div class="col-lg-10 col-md-10 comment-list">
                                 <div class="card arrow arrow--ticket left mb-3">
                                     <div class="card-body">
                                         <header class="text-left">
@@ -84,7 +33,23 @@
                                             <time class="comment-date" datetime="2018-11-07 15:41:35"><i class="fa fa-clock-o"></i> 18 hours ago</time>
                                         </header>
                                         <div class="comment-post">
-                                            <p>sedrth</p>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias cumque delectus dignissimos explicabo illo non nulla ullam. Minus porro, possimus!</p>
+                                            <div class="ticket-attachments">
+                                                <span class="title">Attachments</span>
+                                                <ul>
+                                                    <li class="item-attach">
+                                                        <img src="http://dqudrat.com/wp-content/uploads/2018/08/25-1-10.jpg" alt="">
+                                                    </li>
+                                                    <li class="item-attach">
+                                                        <iframe src="https://eloquentjavascript.net/Eloquent_JavaScript.pdf" style="width: 100%;height: 100%;border: none;"></iframe>
+                                                    </li>
+                                                    <li class="item-attach">
+                                                        <audio controls="">
+                                                            <source src="https://www.computerhope.com/jargon/m/example.mp3">
+                                                        </audio>
+                                                    </li>
+                                                </ul>
+                                            </div>
                                         </div>
                                         <p class="text-right">
                                             <a href="#" data-id="1" class="btn btn-secondary btn-sm reply">
@@ -126,13 +91,46 @@
                             </div>
                         </div>
 
-                        <div class="col-md-12">
-                            <p></p>
-                            <p>{!! $ticket->summary !!}</p>
-                            <p></p>
-                        </div>
+
+
                     </div>
 
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="col-md-12">
+                    <p></p>
+                    <p>{!! $ticket->summary !!}</p>
+                    <p></p>
+                </div>
+
+                <div class="card well well-sm mb-5">
+                    <h2 class="header card-header">
+                        {{ $ticket->subject }}
+                        <a href="#" class="btn btn-success">Mark Complete</a>
+                    </h2>
+                    <div class="panel-body card-body">
+                        <p>
+                            <strong>Owner</strong>: {{ $ticket->author->name }}
+                        </p>
+                        <p>
+                            <strong>Status</strong>:
+                            <span style="color: #e69900">{{ $ticket->status->name }}</span>
+
+                        </p>
+                        <p>
+                            <strong>Priority</strong>:
+                            <span style="color: #069900">{{ $ticket->priority->name }}</span>
+                        </p>
+                        <p><strong>Responsible</strong>: {{ ($ticket->staff) ? $ticket->staff->name : 'not assigned yet' }}</p>
+                        <p>
+                            <strong>Category</strong>:
+                            <span style="color: #0014f4">{{ $ticket->category->name }}</span>
+                        </p>
+                        <p><strong>Created</strong>: {!! time_ago($ticket->created_at) !!}</p>
+                        <p><strong>Last Update</strong>: {!! time_ago($ticket->updated_at) !!}</p>
+                    </div>
                 </div>
             </div>
         </div>
