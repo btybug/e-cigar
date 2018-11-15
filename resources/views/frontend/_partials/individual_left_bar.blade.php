5db63f7c4dc3e5af9f567bd0facf88f3760a36d5
sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-15 10:00:42 +0400

    left bar

--- resources/views/frontend/_partials/individual_left_bar.blade.php
+++ resources/views/frontend/_partials/individual_left_bar.blade.php
@@ -1,11 +1,10 @@
-<div class="nav flex-column justify-content-center nav-pills" id="v-pills-tab" role="tablist"
-     aria-orientation="vertical">
-    <a href="{!! route('blog') !!}" class="nav-link {{ ($type == 'active') ? 'active' : '' }} d-flex flex-column align-items-center" role="tab" >
+<div class="nav flex-column justify-content-center nav-pills" aria-orientation="vertical">
+    <a href="{!! route('blog') !!}" class="nav-link {{ ($type == 'active') ? 'active' : '' }} d-flex flex-column align-items-center">
         <span class="name">News</span></a>
-    <a href="#" class="nav-link {{ ($type == 'active') ? 'forums' : '' }} d-flex flex-column align-items-center" role="tab" >
+    <a href="#" class="nav-link {{ ($type == 'active') ? 'forums' : '' }} d-flex flex-column align-items-center">
         <span class="name">Forums</span>
     </a>
-    <a href="#" class="nav-link {{ ($type == 'active') ? 'socials' : '' }} d-flex flex-column align-items-center" role="tab" >
+    <a href="#" class="nav-link {{ ($type == 'active') ? 'socials' : '' }} d-flex flex-column align-items-center">
         <span class="name">Socials</span>
     </a>
 </div>
