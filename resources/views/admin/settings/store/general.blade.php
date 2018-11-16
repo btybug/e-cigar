fe2048deb3660991878ab812d9bb19d58ea873df
sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-16 17:14:52 +0400

    store general panel

--- resources/views/admin/settings/store/general.blade.php
+++ resources/views/admin/settings/store/general.blade.php
@@ -14,7 +14,8 @@
                    aria-controls="shipping" aria-selected="false">Shipping</a>
             </li>
             <li class="nav-item ">
-                <a class="nav-link" id="payment_gateways" href="{!! route('admin_settings_payment_gateways') !!}" role="tab"
+                <a class="nav-link" id="payment_gateways" href="{!! route('admin_settings_payment_gateways') !!}"
+                   role="tab"
                    aria-controls="shipping" aria-selected="false">Payment gateways</a>
             </li>
             <li class="nav-item ">
@@ -49,6 +50,93 @@
                 </div>
             </div>
             {!! Form::close() !!}
+        </div>
+        <div>
+            <div class="panel panel-default">
+                <div class="panel-heading">Stock availability</div>
+                <div class="panel-body">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="row">
+                                    <label class="col-md-4">Availabile stock status</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="row">
+                                    <label class="col-md-4">Out of stock status</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <label class="radio-inline">
+                                    <input type="radio" name="optradio" checked>Enable Back order
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="optradio">Disable order
+                                </label>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">Currency</div>
+                <div class="panel-body">
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="row">
+                                    <label class="col-md-4">Allow these Currency</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="row">
+                                    <label class="col-md-4">Out of stock status</label>
+                                    <div class="col-md-6">
+                                        <select name="" id="" class="form-control">
+                                            <option value="">1</option>
+                                            <option value="">2</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @stop
