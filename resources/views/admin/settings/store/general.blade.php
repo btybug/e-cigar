454eb3b709f696f894fdc194c41ef5079032a21b
sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-21 11:56:26 +0400

    table fix

--- resources/views/admin/settings/store/general.blade.php
+++ resources/views/admin/settings/store/general.blade.php
@@ -136,69 +136,6 @@
                         </div>
                     </div>
 
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-md-5">
-                                <table class="table table-responsive table--store-settings" data-table-id="20">
-
-                                    <tbody>
-
-                                    <tr class="bg-my-light-pink">
-                                        <th>Currency</th>
-                                        <th>Countries</th>
-                                        <th></th>
-                                    </tr>
-                                    <tr>
-
-
-                                        <td>
-                                            <select class="form-control" name="">
-                                                <option value="1" selected="selected">USD</option>
-                                                <option value="2">GBP</option>
-                                                <option value="3">AMD</option>
-                                            </select>
-                                        </td>
-                                        <td>
-                                            <input class="form-control" name="" type="text">
-
-                                        </td>
-                                        <td colspan="2" class="text-right">
-                                            <button type="button" class="btn btn-danger remove-ship-filed"><i
-                                                        class="fa fa-minus-circle"></i></button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-
-
-                                        <td>
-                                            <select class="form-control" name="">
-                                                <option value="1" selected="selected">USD</option>
-                                                <option value="2">GBP</option>
-                                                <option value="3">AMD</option>
-                                            </select>
-                                        </td>
-                                        <td>
-                                            <input class="form-control" name="" type="text">
-                                        </td>
-                                        <td colspan="2" class="text-right">
-                                            <button type="button" class="btn btn-danger remove-ship-filed"><i
-                                                        class="fa fa-minus-circle"></i></button>
-                                        </td>
-                                    </tr>
-                                    <tr class="add-new-ship-filed-container">
-                                        <td colspan="6" class="text-right">
-                                            <button type="button" data-id="2" data-options-count="6" data-exists="true"
-                                                    class="btn btn-primary add-new-ship-filed"><i
-                                                        class="fa fa-plus-circle"></i></button>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-
-                                </table>
-
-                            </div>
-                        </div>
-                    </div>
 
                     <div class="form-group">
                           <table class="table table-responsive table-striped table-bordered">
@@ -228,10 +165,10 @@
                                             <input class="form-control" name="" type="text">
                                         </td>
                                         <td>
-                                            <button type="button" class="btn btn-secondary">Update now</button>
+                                            <button type="button" class="btn btn-secondary">Update now <i class="fa fa-repeat"></i></button>
                                         </td>
                                         <td colspan="2" class="text-right">
-                                            <button type="button" class="btn btn-danger remove-ship-filed"><i
+                                            <button type="button" class="btn btn-danger"><i
                                                         class="fa fa-minus-circle"></i></button>
                                         </td>
                                     </tr>
@@ -250,10 +187,10 @@
                                             <input class="form-control" name="" type="text">
                                         </td>
                                         <td>
-                                            <button type="button" class="btn btn-secondary">Update now</button>
+                                            <button type="button" class="btn btn-secondary">Update now <i class="fa fa-repeat"></i></button>
                                         </td>
                                         <td colspan="2" class="text-right">
-                                            <button type="button" class="btn btn-danger remove-ship-filed"><i
+                                            <button type="button" class="btn btn-danger"><i
                                                         class="fa fa-minus-circle"></i></button>
                                         </td>
                                     </tr>
