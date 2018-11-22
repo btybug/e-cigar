9385b0c48c00a96d0450be6506f4580cc88d3318
sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-22 16:52:28 +0400

    moved

--- resources/views/admin/settings/regions.blade.php
+++ resources/views/admin/settings/regions.blade.php
@@ -94,6 +94,62 @@
                             </div>
                         </div>
                     </div>
+                    <div class="mb-20">
+                            <table class="table table-responsive table--store-settings" data-table-id="20">
+
+                                <tbody>
+
+                                <tr class="bg-my-light-pink">
+                                    <th>Currency</th>
+                                    <th>Countries</th>
+                                    <th></th>
+                                </tr>
+                                <tr>
+
+
+
+                                    <td>
+                                        <select class="form-control" name="">
+                                            <option value="1" selected="selected">USD</option>
+                                            <option value="2">GBP</option>
+                                            <option value="3">AMD</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input class="form-control"  name="" type="text" >
+
+                                    </td>
+                                    <td colspan="2" class="text-right">
+                                        <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
+                                    </td>
+                                </tr>
+                                <tr>
+
+
+
+                                    <td>
+                                        <select class="form-control" name="">
+                                            <option value="1" selected="selected">USD</option>
+                                            <option value="2">GBP</option>
+                                            <option value="3">AMD</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input class="form-control"  name="" type="text" >
+                                    </td>
+                                    <td colspan="2" class="text-right">
+                                        <button type="button" class="btn btn-danger remove-ship-filed"><i class="fa fa-minus-circle"></i></button>
+                                    </td>
+                                </tr>
+                                <tr class="add-new-ship-filed-container">
+                                    <td colspan="6" class="text-right">
+                                        <button type="button" data-id="2" data-options-count="6" data-exists="true" class="btn btn-primary add-new-ship-filed"><i class="fa fa-plus-circle"></i></button>
+                                    </td>
+                                </tr>
+                                </tbody>
+
+                            </table>
+                    </div>
                 </div>
             </div>
             {!! Form::close() !!}
