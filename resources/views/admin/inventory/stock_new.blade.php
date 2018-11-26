2eab1026119ab0a2bef4530c3120f52445c70d36
sahakavatar <hakobyan.sahak88@gmail.com>
2018-11-26 09:52:07 +0400

    add table

--- resources/views/admin/inventory/stock_new.blade.php
+++ resources/views/admin/inventory/stock_new.blade.php
@@ -274,7 +274,30 @@
                                                 </div>
                                             </div>
                                             <div id="mediaspecifications" class="tab-pane fade ">
-                                                Specifications
+                                                {!! Form::open(['id' => 'v-option-form']) !!}
+                                                <table class="table table-responsive table--store-settings">
+                                                    <thead>
+                                                    <tr class="bg-my-light-pink">
+                                                        <th>Attributes</th>
+                                                        <th></th>
+                                                        <th></th>
+                                                    </tr>
+                                                    </thead>
+
+                                                    <tbody class="v-options-list">
+                                                    @include("admin.inventory._partials.variation_option_item")
+                                                    </tbody>
+
+                                                    <tfoot>
+                                                    <tr class="add-new-ship-filed-container">
+                                                        <td colspan="4" class="text-right">
+                                                            <button type="button" class="btn btn-primary"><i
+                                                                        class="fa fa-plus-circle add-new-v-option"></i></button>
+                                                        </td>
+                                                    </tr>
+                                                    </tfoot>
+                                                </table>
+                                                {!! Form::close() !!}
                                             </div>
                                         </div>
                                     </div>
