96032dcbf26a0c51e95930feac85a6b0e258acee
Nune <hakobyan.sahak88@gmail.com>
2018-11-26 12:13:45 +0400

    inventory items tabs basics ui

--- resources/views/admin/items/new.blade.php
+++ resources/views/admin/items/new.blade.php
@@ -20,33 +20,55 @@
             <li><a data-toggle="tab" href="#sales">Sales</a></li>
         </ul>
         <div class="tab-content">
-            <div id="info" class="tab-pane fade in active">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <div class="col-md-7">
-                                <form class="form-horizontal">
-                                    <div class="form-group">
-                                        <label for="name" class="col-sm-2 control-label"><span data-toggle="tooltip" title="" data-original-title="Attribute Name Title">Product Name</span></label>
-                                        <div class="col-sm-10">
-                                            <input id="name" class="form-control" type="text" value="Apple Hit">
+            <div id="info" class="tab-pane basic-details-tab info-new-tab fade active in">
+                            <div class="row d-flex">
+                                <div class="col-md-3">
+                                    <div class="basic-left basic-wall h-100">
+                                        <div class="all-list">
+                                            <ul class="nav nav-tabs info-list">
+                                                <li class="active"><a data-toggle="tab" href="#innerBasics">Basics</a></li>
+                                                <li><a data-toggle="tab" href="#mediavideos">Videos</a>
+                                                <li><a data-toggle="tab" href="#mediastickers">Stickers</a></li>
+                                                <li><a data-toggle="tab" href="#mediarelatedproducts">Related
+                                                        Products</a></li>
+                                                <li><a data-toggle="tab" href="#mediaspecifications">Specifications</a>
+                                                </li>
+                                            </ul>
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="shortDescr" class="col-sm-2 control-label"><span data-toggle="tooltip" title="" data-original-title="Short Description">Short Description</span></label>
-                                        <div class="col-sm-10">
-                                            <textarea id="shortDescr" class="form-control" cols="30" rows="2">Short description for apple hits</textarea>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="basic-center basic-wall scrollbar_media_tab">
+                                        <div class="tab-content">
+                                            <div id="innerBasics" class="tab-pane fade in active">
+                                                    <form class="form-horizontal">
+                                                        <div class="form-group">
+                                                            <label for="name" class="col-sm-2 control-label"><span data-toggle="tooltip" title="" data-original-title="Attribute Name Title">Product Name</span></label>
+                                                            <div class="col-sm-10">
+                                                                <input id="name" class="form-control" type="text" value="Apple Hit">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="shortDescr" class="col-sm-2 control-label"><span data-toggle="tooltip" title="" data-original-title="Short Description">Short Description</span></label>
+                                                            <div class="col-sm-10">
+                                                                <textarea id="shortDescr" class="form-control" cols="30" rows="2">Short description for apple hits</textarea>
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="longDescr" class="col-sm-2 control-label"><span data-toggle="tooltip" title="" data-original-title="Long Description">Long Description</span></label>
+                                                            <div id="longDescr" class="col-sm-10">
+                                                                <textarea id="longDescr" class="form-control tinyMcArea" cols="30" rows="10">long description for apple hits</textarea>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="longDescr" class="col-sm-2 control-label"><span data-toggle="tooltip" title="" data-original-title="Long Description">Long Description</span></label>
-                                        <div id="longDescr" class="col-sm-10">
-                                            <textarea id="longDescr" class="form-control tinyMcArea" cols="30" rows="10">long description for apple hits</textarea>
-                                        </div>
-                                    </div>
-                                </form>
+                                </div>
+
                             </div>
-                        </div>
-                    </div>
+
+
 
             </div>
             <div id="purchases" class="tabe-pane fade"></div>
@@ -55,4 +77,7 @@
     </div>
 
 
+@stop
+@section('css')
+    <link rel="stylesheet" href="{{asset('public/css/custom.css?v='.rand(111,999))}}">
 @stop
