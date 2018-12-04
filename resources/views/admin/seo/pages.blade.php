2d830ca30d7b24422805ba12d9617fa5e1e7cd69
Nune <hakobyan.sahak88@gmail.com>
2018-12-04 10:38:12 +0400

    admin seo pages ui fixing

--- resources/views/admin/seo/pages.blade.php
+++ resources/views/admin/seo/pages.blade.php
@@ -1,124 +1,128 @@
 @extends('layouts.admin')
 @section('content')
 
-    <div class="container-fluid">
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-
-            <li class="nav-item ">
-                <a class="nav-link" id="shipping-tab" href="{!! route('admin_seo') !!}" role="tab"
-                   aria-controls="shipping" aria-selected="false">Posts</a>
-            </li>
-
-            <li class="nav-item ">
-                <a class="nav-link" id="payment_gateways" href="{!! route('admin_seo_stocks') !!}" role="tab"
-                   aria-controls="shipping" aria-selected="false">Stocks</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" id="payment_gateways" href="{!! route('admin_seo_pages') !!}" role="tab"
-                   aria-controls="shipping" aria-selected="false">Pages</a>
-            </li>
-        </ul>
-        <div class="" id="myTabContent">
-
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="m-0">SEO</h2>
         </div>
-    </div>
-    <div class="seo-pages mt-20">
-        <div class="container-fluid">
-            <div class="row m-0">
-                <button class="btn btn-info pull-right" id="form-submit">Save</button>
-            </div>
-            <div class="row mt-20">
-                <div class="col-md-3">
-                    <div class="seo-pages-left-list">
-                        <ul>
-                            @foreach($pages as $key=>$page)
-                                <li>
-                                    <a href="?p={!! $page['text'] !!}" class="link">{!! $page['url'] !!}</a>
-                                </li>
-                            @endforeach
-                            {{--<li>--}}
-                            {{--<a href="#" class="link">Categories</a>--}}
-                            {{--<ul>--}}
-                            {{--<li>--}}
-                            {{--<a href="#vape" class="sub-link">Vape</a>--}}
-                            {{--</li>--}}
-                            {{--<li>--}}
-                            {{--<a href="#parts" class="sub-link">Parts</a>--}}
-                            {{--</li>--}}
-                            {{--<li>--}}
-                            {{--<a href="#juice" class="sub-link">Juice</a>--}}
-                            {{--<ul>--}}
-                            {{--<li>--}}
-                            {{--<a href="#vape" class="sub-link">Vape</a>--}}
-                            {{--</li>--}}
-                            {{--</ul>--}}
-                            {{--</li>--}}
-                            {{--</ul>--}}
-                            {{--</li>--}}
-                            {{--<li>--}}
-                            {{--<a href="#" class="link">Item 2</a>--}}
-                            {{--<ul>--}}
-                            {{--<li>--}}
-                            {{--<a href="#" class="sub-link">Sub Item 1</a>--}}
-                            {{--</li>--}}
-                            {{--<li>--}}
-                            {{--<a href="#" class="sub-link">Sub Item 1</a>--}}
-                            {{--</li>--}}
-                            {{--</ul>--}}
-                            {{--</li>--}}
-                            {{--<li>--}}
-                            {{--<a href="#categories" class="link"></a>--}}
-                            {{--</li>--}}
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-9">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">General</div>
-                        {!! Form::model($model) !!}
-                        <div class="panel-body">
-                            <div class="form-group">
-                                <div class="row">
-                                    <label for="seo-title" class="col-md-2 col-xs-12">Title</label>
-                                    <div class="col-md-5 col-xs-12">
-                                        {!! Form::text('go:title',null,['class'=>'form-control']) !!}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <label for="seo-desc" class="col-md-2 col-xs-12">Description</label>
-                                    <div class="col-md-5 col-xs-12">
-                                        {!! Form::text('go:description',null,['class'=>'form-control']) !!}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <label for="seo-keywords" class="col-md-2 col-xs-12">Focus keywords</label>
-                                    <div class="col-md-5 col-xs-12">
-                                        {!! Form::text('go:keywords',null,['class'=>'form-control']) !!}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <label for="seo-meta-robots" class="col-md-2 col-xs-12">Meta Robots</label>
-                                    <div class="col-md-5 col-xs-12">
-                                        {!! Form::select('robots',['1'=>'Index','0'=>'No Index'],isset($robot)?$robot->robots:null,['class'=>'form-control']) !!}
+        <div class="panel-body">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
 
-                                    </div>
-                                </div>
+                <li class="nav-item ">
+                    <a class="nav-link" id="shipping-tab" href="{!! route('admin_seo') !!}" role="tab"
+                       aria-controls="shipping" aria-selected="false">Posts</a>
+                </li>
+
+                <li class="nav-item ">
+                    <a class="nav-link" id="payment_gateways" href="{!! route('admin_seo_stocks') !!}" role="tab"
+                       aria-controls="shipping" aria-selected="false">Stocks</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" id="payment_gateways" href="{!! route('admin_seo_pages') !!}" role="tab"
+                       aria-controls="shipping" aria-selected="false">Pages</a>
+                </li>
+            </ul>
+            <div class="seo-pages mt-20">
+                <div class="container-fluid">
+                    <div class="row m-0">
+                        <button class="btn btn-info pull-right" id="form-submit">Save</button>
+                    </div>
+                    <div class="row mt-20">
+                        <div class="col-md-3">
+                            <div class="seo-pages-left-list">
+                                <ul>
+                                    @foreach($pages as $key=>$page)
+                                        <li>
+                                            <a href="?p={!! $page['text'] !!}" class="link">{!! $page['url'] !!}</a>
+                                        </li>
+                                    @endforeach
+                                    {{--<li>--}}
+                                    {{--<a href="#" class="link">Categories</a>--}}
+                                    {{--<ul>--}}
+                                    {{--<li>--}}
+                                    {{--<a href="#vape" class="sub-link">Vape</a>--}}
+                                    {{--</li>--}}
+                                    {{--<li>--}}
+                                    {{--<a href="#parts" class="sub-link">Parts</a>--}}
+                                    {{--</li>--}}
+                                    {{--<li>--}}
+                                    {{--<a href="#juice" class="sub-link">Juice</a>--}}
+                                    {{--<ul>--}}
+                                    {{--<li>--}}
+                                    {{--<a href="#vape" class="sub-link">Vape</a>--}}
+                                    {{--</li>--}}
+                                    {{--</ul>--}}
+                                    {{--</li>--}}
+                                    {{--</ul>--}}
+                                    {{--</li>--}}
+                                    {{--<li>--}}
+                                    {{--<a href="#" class="link">Item 2</a>--}}
+                                    {{--<ul>--}}
+                                    {{--<li>--}}
+                                    {{--<a href="#" class="sub-link">Sub Item 1</a>--}}
+                                    {{--</li>--}}
+                                    {{--<li>--}}
+                                    {{--<a href="#" class="sub-link">Sub Item 1</a>--}}
+                                    {{--</li>--}}
+                                    {{--</ul>--}}
+                                    {{--</li>--}}
+                                    {{--<li>--}}
+                                    {{--<a href="#categories" class="link"></a>--}}
+                                    {{--</li>--}}
+                                </ul>
                             </div>
                         </div>
-                        {!! Form::hidden('p',$p) !!}
-                        <button type="submit" id="submit" class="hidden"></button>
-                        {!! Form::close() !!}
+                        <div class="col-md-9">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">General</div>
+                                {!! Form::model($model) !!}
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <div class="row">
+                                            <label for="seo-title" class="col-md-2 col-xs-12">Title</label>
+                                            <div class="col-md-5 col-xs-12">
+                                                {!! Form::text('go:title',null,['class'=>'form-control']) !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="row">
+                                            <label for="seo-desc" class="col-md-2 col-xs-12">Description</label>
+                                            <div class="col-md-5 col-xs-12">
+                                                {!! Form::text('go:description',null,['class'=>'form-control']) !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="row">
+                                            <label for="seo-keywords" class="col-md-2 col-xs-12">Focus keywords</label>
+                                            <div class="col-md-5 col-xs-12">
+                                                {!! Form::text('go:keywords',null,['class'=>'form-control']) !!}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="row">
+                                            <label for="seo-meta-robots" class="col-md-2 col-xs-12">Meta Robots</label>
+                                            <div class="col-md-5 col-xs-12">
+                                                {!! Form::select('robots',['1'=>'Index','0'=>'No Index'],isset($robot)?$robot->robots:null,['class'=>'form-control']) !!}
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {!! Form::hidden('p',$p) !!}
+                                <button type="submit" id="submit" class="hidden"></button>
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
+
 
 @stop
 @section('css')
--- resources/views/admin/seo/products.blade.php
+++ resources/views/admin/seo/products.blade.php
@@ -1,49 +1,55 @@
 @extends('layouts.admin')
 @section('content')
-    <div class="container-fluid">
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item ">
-                <a class="nav-link" id="shipping-tab" href="{!! route('admin_seo_bulk') !!}" role="tab"
-                   aria-controls="shipping" aria-selected="false">Posts</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" id="shipping-tab" href="{!! route('admin_seo_bulk_products') !!}" role="tab"
-                   aria-controls="shipping" aria-selected="false">Products</a>
-            </li>
-        </ul>
-        <div class="" id="myTabContent">
-            <div class="row">
-                <div class="col-xs-12">
-                    <div class="col-md-6 pull-left"><h2>Inventory</h2></div>
-                    <div class="col-md-6 "><a class="btn btn-primary pull-right" href="{!! route('admin_stock_new') !!}">Add new</a></div>
-                </div>
-                <div class="col-xs-12">
-                    <table id="stocks-table" class="table table-style table-bordered" cellspacing="0" width="100%">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>OG title</th>
-                            <th>OG image</th>
-                            <th>OG description</th>
-                            <th>OG Keywords</th>
-                            <th>Robots</th>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="m-0">SEO</h2>
+        </div>
+        <div class="panel-body">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link" id="shipping-tab" href="{!! route('admin_seo_bulk') !!}" role="tab"
+                       aria-controls="shipping" aria-selected="false">Posts</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" id="shipping-tab" href="{!! route('admin_seo_bulk_products') !!}" role="tab"
+                       aria-controls="shipping" aria-selected="false">Products</a>
+                </li>
+            </ul>
+            <div class="pt-25">
+                <div class="panel panel-default">
+                    <div class="panel-heading clearfix">
+                     <h3 class="m-0 pull-left">Inventory</h3>
+                        <div class="pull-right"><a class="btn btn-primary pull-right" href="{!! route('admin_stock_new') !!}">Add new</a></div>
+                    </div>
+                    <div class="panel-body">
+                        <table id="stocks-table" class="table table-style table-bordered" cellspacing="0" width="100%">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>OG title</th>
+                                <th>OG image</th>
+                                <th>OG description</th>
+                                <th>OG Keywords</th>
+                                <th>Robots</th>
 
-                            <th>FB title</th>
-                            <th>FB image</th>
-                            <th>FB description</th>
+                                <th>FB title</th>
+                                <th>FB image</th>
+                                <th>FB description</th>
 
-                            <th>TW title</th>
-                            <th>TW image</th>
-                            <th>TW description</th>
+                                <th>TW title</th>
+                                <th>TW image</th>
+                                <th>TW description</th>
 
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                    </table>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 
 
 @stop
