9bae9d89d37ffededb15f7b2da36e485eac7b727
Sahakavatar <hakobyan.sahak88@gmail.com>
2018-12-15 23:11:04 +0400

    up

--- resources/views/admin/manage_api/index.blade.php
+++ resources/views/admin/manage_api/index.blade.php
@@ -61,6 +61,13 @@
                         </div>
                     </div>
                 </div>
+                <!-- Button -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="singlebutton"></label>
+                    <div class="col-md-4">
+                        <button id="singlebutton" name="singlebutton" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
 
             </fieldset>
         {!! Form::close() !!}
