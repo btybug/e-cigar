1d8471623a19820b2232f7be0d7f75099e2cee98
Sahakavatar <hakobyan.sahak88@gmail.com>
2018-12-25 20:15:34 +0400

    up

--- resources/views/admin/emails_notifications/notifications/index.blade.php
+++ resources/views/admin/emails_notifications/notifications/index.blade.php
@@ -1,4 +1,34 @@
 @extends('layouts.admin')
 @section('content')
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="m-0 pull-left">Notifications</h2>
+            <div class="text-right">
+                <a class="btn btn-primary" href="#">Create Notifications</a>
+            </div>
 
+        </div>
+        <div class="panel-body">
+            <table id="users-table" class="table table-style table-bordered" cellspacing="0" width="100%">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Slug</th>
+                    <th>Status</th>
+                    <th>Module</th>
+                    <th>Created At</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+@stop
+@section('js')
+    <script>
+        $(function () {
+            $('#users-table').DataTable();
+        });
+
+    </script>
 @stop
