3e9a707851b6b4be1e3c5f11e5a3c1a592ad93c5
Sahakavatar <hakobyan.sahak88@gmail.com>
2018-12-27 14:40:49 +0400

    up

--- database/seeds/StatusTableSeeder.php
+++ database/seeds/StatusTableSeeder.php
@@ -11,30 +11,45 @@ class StatusTableSeeder extends Seeder
      */
     public function run()
     {
+        $settings = new \App\Models\Settings();
+        $data = array();
         $locale = 'gb';
-        $status = new \App\Models\Statuses(['is_default' => true,'type' => 'order']);
+        $status = new \App\Models\Statuses(['is_default' => 1, 'type' => 'order']);
         $status->save();
         $status->translateOrNew($locale)->name = 'Submitted';
         $status->translateOrNew($locale)->description = '';
         $status->save();
+        $data['submitted'] = $status->id;
 
-        $status = new \App\Models\Statuses(['is_default' => true,'type' => 'order']);
+        $status = new \App\Models\Statuses(['is_default' => 1, 'type' => 'order']);
         $status->save();
         $status->translateOrNew($locale)->name = 'Caneclled';
         $status->translateOrNew($locale)->description = '';
         $status->save();
+        $data['canceled'] = $status->id;
 
-        $status = new \App\Models\Statuses(['is_default' => true,'type' => 'order']);
+        $status = new \App\Models\Statuses(['is_default' => 1, 'type' => 'order']);
         $status->save();
         $status->translateOrNew($locale)->name = 'Completed';
         $status->translateOrNew($locale)->description = '';
         $status->save();
+        $data['completed'] = $status->id;
 
-        $status = new \App\Models\Statuses(['is_default' => true,'type' => 'order']);
+        $status = new \App\Models\Statuses(['is_default' => 1, 'type' => 'order']);
         $status->save();
         $status->translateOrNew($locale)->name = 'Collected';
         $status->translateOrNew($locale)->description = '';
         $status->save();
+        $data['collected'] = $status->id;
+
+        $status = new \App\Models\Statuses(['is_default' => 1, 'type' => 'order']);
+        $status->save();
+        $status->translateOrNew($locale)->name = 'Partially Collected';
+        $status->translateOrNew($locale)->description = '';
+        $status->save();
+        $data['partially_collected'] = $status->id;
+
+        $settings->updateOrCreateSettings('orders_statuses', $data);
 
 
     }
