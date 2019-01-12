8c50a03ed454fb28f2cffcfa42bbf89f7c0832e4
terakyan <edo.terakyan@gmail.com>
2019-01-12 19:54:50 +0400

    fix

--- resources/views/frontend/products/_partials/filters/checkbox.blade.php
+++ resources/views/frontend/products/_partials/filters/checkbox.blade.php
@@ -1,14 +1,14 @@
 <div class="filter-wall cat-name">
     <h5 class="font-sec-bold font-16 text-uppercase">{{ $filter->name }}</h5>
     @foreach($filter->stickers as $sticker)
-    <div class="single-wrap">
-        <div class="custom-control custom-checkbox custom-control-inline align-items-center">
-            {!! Form::radio(str_slug($filter->name),$sticker->id,null,['class' => 'custom-control-input','id' => 'customCheck'.$filter->id.$sticker->id]) !!}
-            <label class="custom-control-label text-gray-clr font-15"
-                   for="customCheck{{ $filter->id.$sticker->id }}">{{ $sticker->name }}
-                {{--<span class="amount">(189)</span>--}}
-            </label>
+        <div class="single-wrap">
+            <div class="custom-control custom-checkbox custom-control-inline align-items-center">
+                {!! Form::checkbox(str_slug($filter->name),$sticker->id,null,['class' => 'custom-control-input','id' => 'customCheck'.$filter->id.$sticker->id]) !!}
+                <label class="custom-control-label text-gray-clr font-15"
+                       for="customCheck{{ $filter->id.$sticker->id }}">{{ $sticker->name }}
+                    {{--<span class="amount">(189)</span>--}}
+                </label>
+            </div>
         </div>
-    </div>
     @endforeach
 </div>
--- resources/views/frontend/products/_partials/filters/radio.blade.php
+++ resources/views/frontend/products/_partials/filters/radio.blade.php
@@ -1,46 +1,14 @@
 <div class="filter-wall cat-name">
-    <h5 class="font-sec-bold font-16 text-uppercase">CATEGORY NAME</h5>
-    <div class="single-wrap">
-        <div class="custom-control custom-checkbox custom-control-inline align-items-center">
-            <input type="checkbox" class="custom-control-input" id="customCheck1"
-                   name="example1">
-            <label class="custom-control-label text-gray-clr font-15"
-                   for="customCheck1">DaVinci <span
-                        class="amount">(189)</span></label>
+    <h5 class="font-sec-bold font-16 text-uppercase">{{ $filter->name }}</h5>
+    @foreach($filter->stickers as $sticker)
+        <div class="single-wrap">
+            <div class="custom-control custom-checkbox custom-control-inline align-items-center">
+                {!! Form::radio(str_slug($filter->name),$sticker->id,null,['class' => 'custom-control-input','id' => 'customCheck'.$filter->id.$sticker->id]) !!}
+                <label class="custom-control-label text-gray-clr font-15"
+                       for="customCheck{{ $filter->id.$sticker->id }}">{{ $sticker->name }}
+                    {{--<span class="amount">(189)</span>--}}
+                </label>
+            </div>
         </div>
-    </div>
-    <div class="single-wrap">
-        <div class="custom-control custom-checkbox custom-control-inline align-items-center">
-            <input type="checkbox" class="custom-control-input" id="customCheck2"
-                   checked="checked" name="example2">
-            <label class="custom-control-label text-gray-clr font-15"
-                   for="customCheck2">Dr. Dabber <span
-                        class="amount">(25)</span></label>
-        </div>
-    </div>
-    <div class="single-wrap">
-        <div class="custom-control custom-checkbox custom-control-inline align-items-center">
-            <input type="checkbox" class="custom-control-input" id="customCheck3"
-                   name="example3">
-            <label class="custom-control-label text-gray-clr font-15"
-                   for="customCheck3">Eleaf <span class="amount">(9)</span></label>
-        </div>
-    </div>
-    <div class="single-wrap">
-        <div class="custom-control custom-checkbox custom-control-inline align-items-center">
-            <input type="checkbox" class="custom-control-input" id="customCheck4"
-                   name="example4">
-            <label class="custom-control-label text-gray-clr font-15"
-                   for="customCheck4">SMOK <span class="amount">(70)</span></label>
-        </div>
-    </div>
-    <div class="single-wrap">
-        <div class="custom-control custom-checkbox custom-control-inline align-items-center">
-            <input type="checkbox" class="custom-control-input" id="customCheck5"
-                   name="example5">
-            <label class="custom-control-label text-gray-clr font-15"
-                   for="customCheck5">Unbranded <span
-                        class="amount">(3)</span></label>
-        </div>
-    </div>
+    @endforeach
 </div>
