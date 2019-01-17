6353865339500f687c9b0b9b8dcc3ac35b93e781
terakyan <edo.terakyan@gmail.com>
2019-01-17 15:28:58 +0400

    checkbox

--- resources/views/frontend/products/_partials/filters/checkbox.blade.php
+++ resources/views/frontend/products/_partials/filters/checkbox.blade.php
@@ -3,7 +3,7 @@
     @foreach($filter->stickers as $sticker)
         <div class="single-wrap">
             <div class="custom-control custom-checkbox custom-control-inline align-items-center">
-                {!! Form::checkbox(str_slug($filter->name),$sticker->id,null,['class' => 'custom-control-input','id' => 'customCheck'.$filter->id.$sticker->id]) !!}
+                {!! Form::checkbox("select_filter[$filter->id][]",$sticker->id,null,['class' => 'select-filter custom-control-input','id' => 'customCheck'.$filter->id.$sticker->id]) !!}
                 <label class="custom-control-label text-gray-clr font-15"
                        for="customCheck{{ $filter->id.$sticker->id }}">{{ $sticker->name }}
                     {{--<span class="amount">(189)</span>--}}
