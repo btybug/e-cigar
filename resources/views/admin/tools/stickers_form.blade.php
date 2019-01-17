8e1300dc242de5ed86e8ce06f98c917f9eca3fbb
terakyan <edo.terakyan@gmail.com>
2019-01-17 16:30:44 +0400

    colors

--- resources/views/admin/tools/stickers_form.blade.php
+++ resources/views/admin/tools/stickers_form.blade.php
@@ -48,22 +48,9 @@
                     {{--{!! Form::text('color',null,['class'=>'form-control','required'=>true]) !!}--}}
                     <div class="color_select_wall">
                         <select name="color" id="colorselector_2">
-                            <option value="#A0522D" data-color="#A0522D" {!! ($model && $model->color == '#A0522D') ? 'selected' : '' !!}>sienna</option>
-                            <option value="#CD5C5C" data-color="#CD5C5C" {!! ($model && $model->color == '#CD5C5C') ? 'selected' : '' !!}>indianred</option>
-                            <option value="#FF4500" data-color="#FF4500" {!! ($model && $model->color == '#FF4500') ? 'selected' : '' !!}>orangered</option>
-                            <option value="#008B8B" data-color="#008B8B" {!! ($model && $model->color == '#008B8B') ? 'selected' : '' !!}>darkcyan</option>
-                            <option value="#B8860B" data-color="#B8860B" {!! ($model && $model->color == '#B8860B') ? 'selected' : '' !!}>darkgoldenrod</option>
-                            <option value="#32CD32" data-color="#32CD32" {!! ($model && $model->color == '#32CD32') ? 'selected' : '' !!}>limegreen</option>
-                            <option value="#FFD700" data-color="#FFD700" {!! ($model && $model->color == '#FFD700') ? 'selected' : '' !!}>gold</option>
-                            <option value="#48D1CC" data-color="#48D1CC" {!! ($model && $model->color == '#48D1CC') ? 'selected' : '' !!}>mediumturquoise</option>
-                            <option value="#87CEEB" data-color="#87CEEB" {!! ($model && $model->color == '#87CEEB') ? 'selected' : '' !!}>skyblue</option>
-                            <option value="#FF69B4" data-color="#FF69B4" {!! ($model && $model->color == '#FF69B4') ? 'selected' : '' !!}>hotpink</option>
-                            <option value="#87CEFA" data-color="#87CEFA" {!! ($model && $model->color == '#87CEFA') ? 'selected' : '' !!}>lightskyblue</option>
-                            <option value="#6495ED" data-color="#6495ED" {!! ($model && $model->color == '#6495ED') ? 'selected' : '' !!}>cornflowerblue</option>
-                            <option value="#DC143C" data-color="#DC143C" {!! ($model && $model->color == '#DC143C') ? 'selected' : '' !!}>crimson</option>
-                            <option value="#FF8C00" data-color="#FF8C00" {!! ($model && $model->color == '#FF8C00') ? 'selected' : '' !!}>darkorange</option>
-                            <option value="#C71585" data-color="#C71585" {!! ($model && $model->color == '#C71585') ? 'selected' : '' !!}>mediumvioletred</option>
-                            <option value="#000000" data-color="#000000" {!! ($model && $model->color == '#000000') ? 'selected' : '' !!}>black</option>
+                            @foreach(colors() as $name => $hex)
+                                <option value="{{ $hex }}" data-color="{{ $hex }}" {!! ($model && $model->color == $hex) ? 'selected' : '' !!}>{{ $name }}</option>
+                            @endforeach
                         </select> <br />
                     </div>
                 </div>
