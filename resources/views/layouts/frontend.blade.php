96b0b5ade872fdc7dd305237b583c0aedffae4a9
Nune <hakobyan.sahak88@gmail.com>
2019-02-08 10:22:58 +0400

    update favicon

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -9,8 +9,8 @@
     <meta name="google-recaptcha-key" content="{!! env('GOOGLE_RECAPTCHA_KEY') !!}">
     @yield('meta')
     <title>Document</title>
-    <link rel="shortcut icon" href="//{!! url('/public/img/favicon.png') !!}">
-    <link rel="shortcut icon" href="/{!! url('/public/img/favicon.ico') !!}">
+    <link rel="shortcut icon" href="{!! url('/public/img/favicon.png') !!}">
+    <link rel="shortcut icon" href="{!! url('/public/img/favicon.ico') !!}">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
           integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
