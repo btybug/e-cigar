c3d02ed57b4fdbe28eb0f746dd77baa4ae3f1fb6
Nune <hakobyan.sahak88@gmail.com>
2019-02-14 13:45:50 +0400

    header up

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,43 +1,46 @@
 @extends('layouts.admin')
 @section('content-header')
-    <h1>
-        Dashboard
-        <small>Control panel</small>
-    </h1>
+    {{--<h1>--}}
+        {{--Dashboard--}}
+        {{--<small>Control panel</small>--}}
+    {{--</h1>--}}
     <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Dashboard</li>
     </ol>
-    <button class="btn btn-info open_dashboard_widget">Add new Widget</button>
 @stop
 @section('content')
+    <div class="col-xs-12">
 
-    <div class="profile-header">
+        <div class="profile-header">
 
-        <div class="profile-header-cover"></div>
+            <div class="profile-header-cover"></div>
 
-        <div class="profile-header-content">
+            <div class="profile-header-content">
 
-            <div class="profile-header-img">
-                <img src="{!!url('public/admin_theme/dist/img/user2-160x160.jpg')!!}" class="user-image" alt="User Image">
+                <div class="profile-header-img">
+                    <img src="{!!url('public/admin_theme/dist/img/user2-160x160.jpg')!!}" class="user-image" alt="User Image">
+                </div>
+
+                <div class="profile-header-info">
+                    <h4 class="m-t-10 m-b-5">{{ Auth::user()->name }}</h4>
+                    <p class="m-b-10">Web Developer</p>
+                    {{--<a href="#" class="btn btn-xs btn-info">Edit Profile</a>--}}
+                </div>
+
             </div>
 
-            <div class="profile-header-info">
-                <h4 class="m-t-10 m-b-5">{{ Auth::user()->name }}</h4>
-                <p class="m-b-10">Web Developer</p>
-                <a href="#" class="btn btn-xs btn-info">Edit Profile</a>
-            </div>
+            <ul class="profile-header-tab nav nav-tabs">
+                <li class="nav-item"><a href="#tabDashboard" class="nav-link active" data-toggle="tab">Dashboard</a></li>
+                <li class="nav-item"><a href="#tabProfile" class="nav-link" data-toggle="tab">Profile</a></li>
+            </ul>
 
         </div>
 
-        <ul class="profile-header-tab nav nav-tabs">
-            <li class="nav-item"><a href="#profile-post" class="nav-link active" data-toggle="tab">POSTS</a></li>
-            <li class="nav-item"><a href="#profile-about" class="nav-link" data-toggle="tab">ABOUT</a></li>
-            <li class="nav-item"><a href="#profile-photos" class="nav-link" data-toggle="tab">PHOTOS</a></li>
-            <li class="nav-item"><a href="#profile-videos" class="nav-link" data-toggle="tab">VIDEOS</a></li>
-            <li class="nav-item"><a href="#profile-friends" class="nav-link" data-toggle="tab">FRIENDS</a></li>
-        </ul>
+        <div class="text-right mb-20">
+            <button class="btn btn-info open_dashboard_widget">Add new Widget</button>
 
+        </div>
     </div>
 
     <div class="col-md-12">
