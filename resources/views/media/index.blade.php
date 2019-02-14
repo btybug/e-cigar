60ebe30d5a17c31339a7cdc3a8752793e3885a42
David <hakobyan.sahak88@gmail.com>
2019-02-14 15:02:59 +0400

    drag&drop area

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -64,12 +64,12 @@
                     <div class="over-auto scrollbar_custom">
                         <div class="row m-0">
                             <div class="col-lg-12 m-b-10 text-right d-flex" style="justify-content: space-between;">
-                            <ul class="multiple-actions">
-                                <li><button class="btn btn-danger"><i class="fa fa-trash"></i></button></li>
-                                <li><button class="btn btn-info"><i class="fa fa-share"></i></button></li>
-                                <li><button class="btn btn-primary"><i class="fa fa-clone"></i></button></li>
-                            </ul>
-                            <div class="upload-content" style="width: 100%; margin-left: 20px;">
+                            {{--<ul class="multiple-actions">--}}
+                                {{--<li><button class="btn btn-danger"><i class="fa fa-trash"></i></button></li>--}}
+                                {{--<li><button class="btn btn-info"><i class="fa fa-share"></i></button></li>--}}
+                                {{--<li><button class="btn btn-primary"><i class="fa fa-clone"></i></button></li>--}}
+                            {{--</ul>--}}
+                            <div class="upload-content" style="width: 100%;">
                             <div class="uploader-container d-none">
                                 <input id="uploader" class="file-loading" data-folder-id="{!! 1 !!}" multiple   name="item[]" type="file" data-upload-url="{!! route('media_upload') !!}">
                             </div>
