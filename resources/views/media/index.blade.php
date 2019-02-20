8698ebcd5594c6dc952c0b3c67d7cabf8adc4e9b
sahakavatar <hakobyan.sahak88@gmail.com>
2019-02-20 17:28:35 +0400

    up media

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -66,19 +66,24 @@
           <div class="over-auto scrollbar_custom right--media-col-wrapper">
             <div class="row m-0">
               <div class="col-lg-12 m-b-10 text-right d-flex" style="justify-content: space-between;">
-                <ul class="multiple-actions">
-                <li><button class="btn btn-danger"><i class="fa fa-trash"></i></button></li>
-                <li><button class="btn btn-info"><i class="fa fa-share"></i></button></li>
-                <li><button class="btn btn-primary"><i class="fa fa-clone"></i></button></li>
-                </ul>
+                {{--<ul class="multiple-actions">--}}
+                {{--<li><button class="btn btn-danger"><i class="fa fa-trash"></i></button></li>--}}
+                {{--<li><button class="btn btn-info"><i class="fa fa-share"></i></button></li>--}}
+                {{--<li><button class="btn btn-primary"><i class="fa fa-clone"></i></button></li>--}}
+                {{--</ul>--}}
                 <div class="upload-content" style="width: 100%;">
-                  <div class="uploader-container mt15 d-none">
+                  <div class="upload--head  mb-20 mt15">
+                      <button class="btn btn-danger">Delete</button>
+                      <button class="btn btn-info">Move</button>
+                      <button class="btn btn-warning">Copy</button>
+                    <button type="button" class="btn btn-primary uploader_button" data-role="btnUploader"
+                            bb-media-click="show_uploader">Uploader
+                    </button>
+                  </div>
+                  <div class="uploader-container mt15  mb-20 d-none">
                     <input id="uploader" class="file-loading" data-folder-id="{!! 1 !!}" multiple name="item[]"
                            type="file" data-upload-url="{!! route('media_upload') !!}">
                   </div>
-                  <button type="button" class="btn btn-primary mb-20 mt15 uploader_button" data-role="btnUploader"
-                          bb-media-click="show_uploader">Uploader
-                  </button>
                 </div>
               </div>
             </div>
