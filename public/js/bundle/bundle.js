620ffa79b84002b8729104789eb65672e9240805
David <hakobyan.sahak88@gmail.com>
2019-02-22 15:10:56 +0400

    up

--- public/js/bundle/bundle.js
+++ public/js/bundle/bundle.js
@@ -3185,6 +3185,70 @@ $(document).ready(function () {
             });
         });
     });
+
+    $('#login-form-checkout').on('submit', function (ev) {
+        var _this4 = this;
+
+        ev.preventDefault();
+
+        var GOOGLE_RECAPTCHA_KEY = $('meta[name="google-recaptcha-key"]').attr("content");
+        grecaptcha.ready(function () {
+            grecaptcha.execute(GOOGLE_RECAPTCHA_KEY, { action: 'action_name' }).then(function (token) {
+                $('.g-recaptcha-response').val(token);
+            }).then(function () {
+                var data = $(_this4).serialize();
+
+                var errorHandler = function errorHandler(fieldElement, errorObject, message, fieldElementName) {
+                    var change = function change(fieldElementChange, fieldElementNameChange) {
+                        fieldElementChange.removeClass('transition-horizontal input-error');
+                        $(fieldElementNameChange + '~p').remove();
+                    };
+                    change(fieldElement, fieldElementName);
+
+                    var pTag = fieldElement.next().prop("tagName") !== 'p';
+
+                    if (errorObject && message && pTag) {
+                        fieldElement.parent().append('<p style="color: red; font-size: 12px; margin-top: 2px;">' + message + '</p>');
+                        fieldElement.addClass('transition-horizontal input-error');
+                        setTimeout(function () {
+                            fieldElement.removeClass('transition-horizontal');
+                        }, 500);
+                    }
+                    fieldElement.on('keypress', function () {
+                        change(fieldElement, fieldElementName);
+                    });
+                    fieldElement.on('change', function () {
+                        change(fieldElement, fieldElementName);
+                    });
+                };
+
+                // $.ajax({
+                //     type: "post",
+                //     url: "/login",
+                //     cache: false,
+                //     datatype: "json",
+                //     data: data,
+                //     headers: {
+                //         "X-CSRF-TOKEN": $("meta[name='csrf-token']").attr("content")
+                //     },
+                //     success: function (data) {
+                //         if (!data.error) {
+                //             location.href = data.redirectPath
+                //             console.log(data)
+                //         } else {
+                //             alert('error')
+                //         }
+                //     },
+                //     error: function (error) {
+                //         var emailEl = $('#loginEmail');
+                //         var passwordEl = $('#loginPass');
+                //         errorHandler(emailEl, error.responseJSON.errors, error.responseJSON.errors.email, '#loginEmail');
+                //         errorHandler(passwordEl, error.responseJSON.errors, error.responseJSON.errors.password, '#loginPass');
+                //     }
+                // });
+            });
+        });
+    });
 })();
 
 /**
