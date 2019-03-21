ceb77fc80023626d366d55659d28039989817571
terakyan <edo.terakyan@gmail.com>
2019-03-21 16:51:43 +0400

    up

--- resources/views/frontend/products/_partials/variation_types/menu.blade.php
+++ resources/views/frontend/products/_partials/variation_types/menu.blade.php
@@ -12,7 +12,7 @@
 
     {!! Form::select('variations[]',$variation->pluck('name','id')->all(),null,
     ['class' => 'select-variation-option select-2 select-2--no-search main-select main-select-2arrows single-product-select product-pack-select select2-hidden-accessible',
-    'multiple' => (($vSettings->count_limit > 1) ? true : false),'data-count' => $vSettings->count_limit]) !!}
+    'multiple' => (($vSettings->count_limit > 1) ? true : false),'data-count' => $vSettings->count_limit,'data-id' => $vSettings->id]) !!}
 </div>
 <div class="col-sm-2 pl-sm-3 p-0 text-sm-center">
     <span class="d-inline-block font-35 font-sec-bold text-uppercase ml-auto price-placee">
