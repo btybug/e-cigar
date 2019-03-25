8d72ee9f7fdccbea775d97509ccb521175f8b5b4
David <hakobyan.sahak88@gmail.com>
2019-03-25 13:28:32 +0400

    Limit

--- resources/views/frontend/products/_partials/multi_menu_variation.blade.php
+++ resources/views/frontend/products/_partials/multi_menu_variation.blade.php
@@ -1,11 +1,11 @@
-<div class="col-sm-12 pl-0 m-l-5 menu-item-selected" data-id="{{ $variation->id }}">
-    <div class="d-flex flex-wrap align-items-center">
+<div class="col-sm-12 pl-0 m-l-5 menu-item-selected mb-2" data-id="{{ $variation->id }}">
+    <div class="d-flex flex-wrap align-items-center ">
         <div class="col-sm-7">
             <a href="javascript:void(0)" data-el-id="{{ $selectElementId }}" class="btn btn-sm delete-menu-item cl-red"><i class="fa fa-times"></i></a>
             {{ $variation->name }}
         </div>
         <div class="col-sm-3 ">
-            <div class="continue-shp-wrapp_qty position-relative product-counts-wrapper">
+            <div class="continue-shp-wrapp_qty position-relative product-counts-wrapper w-100">
                 <!--minus qty-->
                 <span class="d-flex align-items-center h-100 pointer position-absolute product-count-minus">
                             <svg viewBox="0 0 20 3" width="20px" height="3px">
@@ -23,7 +23,7 @@
         </div>
         <div class="col-sm-2 pl-sm-3 p-0 text-sm-center">
             @if($variation->price_per =='item')
-            <span class="d-inline-block font-35 font-sec-bold text-uppercase ml-auto price-placee">
+            <span class="d-inline-block font-35 font-sec-bold text-uppercase ml-auto price-placee lh-1">
                 {{ convert_price($variation->price,$currency) }}
             </span>
             @endif
