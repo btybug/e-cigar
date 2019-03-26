0e544e0acdb18585e79587fabeb6d441c0516113
terakyan <edo.terakyan@gmail.com>
2019-03-26 15:44:43 +0400

    up

--- resources/views/admin/inventory/_partials/render_price_form.blade.php
+++ resources/views/admin/inventory/_partials/render_price_form.blade.php
@@ -8,7 +8,7 @@ $variations = collect($model->variations)->groupBy('variation_id');
             $vSettings = $variation->first();
         @endphp
         <div class="product-single-info_row options-group">
-            <div class="d-flex flex-wrap align-items-center">
+            <div class="d-flex flex-wrap align-items-center" data-group-id="{{ $vSettings->variation_id }}">
                 @if($vSettings->type == 'simple_product')
                     <div class="col-sm-10 pl-0 d-flex">
                         @if(! $vSettings->is_required)
