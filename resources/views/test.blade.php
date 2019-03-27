1a4185e1ddac5fc58b751ed1ee8810eee67fad21
sahakavatar <hakobyan.sahak88@gmail.com>
2019-03-27 15:22:12 +0400

    style modal

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,7 +7,8 @@
     </button>
 
     <!-- Modal -->
-    <div class="modal fade" id="wizardViewModal" tabindex="-1" role="dialog" aria-labelledby="wizardViewLabel" aria-hidden="true">
+    <div class="modal fade" id="wizardViewModal" tabindex="-1" role="dialog" aria-labelledby="wizardViewLabel"
+         aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -149,7 +150,8 @@
                                                 <div class="wrap-item position-relative">
                                                     <a href="#" class="item-link">
                                         <span class="item-img">
-                                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/c0481d96-83b9-423e-a234-42a226980db3/d9xw3a0-a23207d9-a507-4fef-a3ca-4c5c1bf47d64.jpg/v1/fill/w_359,h_250,q_70,strp/vape_girl_by_asiamckinley_ann_d9xw3a0-250t.jpg" alt="item">
+                                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/c0481d96-83b9-423e-a234-42a226980db3/d9xw3a0-a23207d9-a507-4fef-a3ca-4c5c1bf47d64.jpg/v1/fill/w_359,h_250,q_70,strp/vape_girl_by_asiamckinley_ann_d9xw3a0-250t.jpg"
+                                                 alt="item">
                                         </span>
                                                         <span class="name">items</span>
                                                     </a>
@@ -159,7 +161,8 @@
                                                 <div class="wrap-item position-relative">
                                                     <a href="#" class="item-link">
                                         <span class="item-img">
-                                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/c0481d96-83b9-423e-a234-42a226980db3/d9xw3a0-a23207d9-a507-4fef-a3ca-4c5c1bf47d64.jpg/v1/fill/w_359,h_250,q_70,strp/vape_girl_by_asiamckinley_ann_d9xw3a0-250t.jpg" alt="item">
+                                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/c0481d96-83b9-423e-a234-42a226980db3/d9xw3a0-a23207d9-a507-4fef-a3ca-4c5c1bf47d64.jpg/v1/fill/w_359,h_250,q_70,strp/vape_girl_by_asiamckinley_ann_d9xw3a0-250t.jpg"
+                                                 alt="item">
                                         </span>
                                                         <span class="name">items</span>
                                                     </a>
@@ -169,7 +172,8 @@
                                                 <div class="wrap-item position-relative">
                                                     <a href="#" class="item-link">
                                         <span class="item-img">
-                                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/c0481d96-83b9-423e-a234-42a226980db3/d9xw3a0-a23207d9-a507-4fef-a3ca-4c5c1bf47d64.jpg/v1/fill/w_359,h_250,q_70,strp/vape_girl_by_asiamckinley_ann_d9xw3a0-250t.jpg" alt="item">
+                                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/c0481d96-83b9-423e-a234-42a226980db3/d9xw3a0-a23207d9-a507-4fef-a3ca-4c5c1bf47d64.jpg/v1/fill/w_359,h_250,q_70,strp/vape_girl_by_asiamckinley_ann_d9xw3a0-250t.jpg"
+                                                 alt="item">
                                         </span>
                                                         <span class="name">items</span>
                                                     </a>
@@ -179,7 +183,8 @@
                                                 <div class="wrap-item position-relative">
                                                     <a href="#" class="item-link">
                                         <span class="item-img">
-                                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/c0481d96-83b9-423e-a234-42a226980db3/d9xw3a0-a23207d9-a507-4fef-a3ca-4c5c1bf47d64.jpg/v1/fill/w_359,h_250,q_70,strp/vape_girl_by_asiamckinley_ann_d9xw3a0-250t.jpg" alt="item">
+                                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/c0481d96-83b9-423e-a234-42a226980db3/d9xw3a0-a23207d9-a507-4fef-a3ca-4c5c1bf47d64.jpg/v1/fill/w_359,h_250,q_70,strp/vape_girl_by_asiamckinley_ann_d9xw3a0-250t.jpg"
+                                                 alt="item">
                                         </span>
                                                         <span class="name">items</span>
                                                     </a>
@@ -193,7 +198,9 @@
                                         </div>
                                         <div class="next-item">
                                             <button class="btn btn-secondary next-btn">Next</button>
-                                            <button class="btn btn-primary add-items-btn d-none"><i class="fa fa-plus"></i><span class="ml-1">Add selected items</span></button>
+                                            <button class="btn btn-primary add-items-btn d-none"><i
+                                                        class="fa fa-plus"></i><span
+                                                        class="ml-1">Add selected items</span></button>
                                         </div>
                                     </div>
                                 </div>
@@ -204,6 +211,165 @@
             </div>
         </div>
     </div>
+@stop
+@section('css')
+    <style>
+        .shopping-cart_wrapper .form-control:focus {
+            -webkit-box-shadow: none;
+            -moz-box-shadow: none;
+            box-shadow: none;
+        }
+
+        .shopping-cart_wrapper .text-quatr-clr {
+            color: #5184e5;
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head {
+            padding-right: 58px;
+        }
+
+        @media screen and (max-width: 992px) {
+            .shopping-cart_wrapper .shopping-cart-head {
+                padding-right: 6px;
+            }
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .nav {
+            margin-bottom: 53px;
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .nav-item {
+            padding-left: 0;
+            padding-right: 6px;
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .nav-item:first-of-type .item:before {
+            display: none;
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .item {
+            position: relative;
+            display: block;
+            color: #b9b7c1;
+            background-color: #f2f1f5;
+            height: 46px;
+            padding: 0 5px 0 36px;
+            line-height: 1;
+            margin-bottom: 5px;
+            border-top: none;
+            cursor: default;
+        }
+
+        @media screen and (min-width: 768px) {
+            .shopping-cart_wrapper .shopping-cart-head .item {
+                margin-bottom: 0;
+            }
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .item:after {
+            content: "";
+            position: absolute;
+            right: -16px;
+            width: 0;
+            height: 0;
+            border-top: 23px solid transparent;
+            border-bottom: 23px solid transparent;
+            border-left: 16px solid #f2f1f5;
+            z-index: 7;
+            top: 0;
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .item:before {
+            content: "";
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 0;
+            height: 0;
+            border-top: 23px solid transparent;
+            border-bottom: 23px solid transparent;
+            border-left: 16px solid #ffffff;
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .item .icon {
+            opacity: 0;
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .item.visited .icon {
+            opacity: 1;
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .item.active {
+            color: #ffffff;
+            background-color: #5184e5;
+        }
+
+        .shopping-cart_wrapper .shopping-cart-head .item.active:after {
+            border-left: 16px solid #5184e5;
+        }
+
+        /*------------------------*/
+        .shopping-cart_wrapper .content-wrap{
+            margin-bottom: 5px;
+        }
+        .shopping-cart_wrapper .content-wrap>ul{
+            padding: 0;
+            list-style: none;
+        }
+        .shopping-cart_wrapper .content-wrap .item-content{
+            box-shadow: 0 0 4px #000;
+            border: 1px solid #ccc;
+            margin-bottom: 20px;
+        }
+        .shopping-cart_wrapper .content-wrap .item-content .item-photo{
+            height: 150px;
+        }
+        .shopping-cart_wrapper .content-wrap .item-content .item-title{
+            padding:5px 15px;
+            border-top: 1px solid #ccc;
+            font-weight: bold;
+        }
+        .shopping-cart_wrapper .content-wrap .item-content img{
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+        }
+        .shopping-cart_wrapper .content-wrap .item-content.active{
+            border: 1px solid #000000;
+            box-shadow: 0px 0 4px 3px #f39c12;
+        }
+        .shopping-cart_wrapper .content-wrap{
+            min-height: 408px;
+        }
+        .shopping-cart_wrapper .content-wrap.confirm-wrapper .wrap-item .item-img{
+            height: 250px;
+        }
+        .shopping-cart_wrapper .content-wrap.confirm-wrapper .wrap-item{
+            margin-bottom: 15px;
+        }
+        .shopping-cart_wrapper .content-wrap.confirm-wrapper .wrap-item img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+        .shopping-cart_wrapper .content-wrap.confirm-wrapper .wrap-item .buttons {
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+        .shopping-cart_wrapper .content-wrap.confirm-wrapper .wrap-item .name {
+            text-align: center;
+            display: block;
+            background-color: #eee;
+            padding: 8px;
+            text-transform: uppercase;
+            color: #000;
+        }
+        .shopping-cart_wrapper .content-wrap.confirm-wrapper .wrap-item.active{
+            border: 1px solid #000000;
+            box-shadow: 0px 0 4px 3px #f39c12;
+        }
+    </style>
 @stop
 @section('js')
     <script>
