658f2e9af330535339ba3b23aa08316159bc71cf
Sahakavatar <hakobyan.sahak88@gmail.com>
2019-03-29 14:54:24 +0400

    up

--- resources/views/filters/button.blade.php
+++ resources/views/filters/button.blade.php
@@ -1,2 +1,1 @@
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#wizardViewModal" data-action="{!! $category !!}">{!! $text !!}</button>
-
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#wizardViewModal" data-action="{!! $category->id !!}">{!! $text !!}</button>
