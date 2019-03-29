5e19b768d7fb480b5ebdcdc2038c81a61188d6f9
Sahakavatar <hakobyan.sahak88@gmail.com>
2019-03-29 18:06:27 +0400

    up

--- resources/views/filters/filters.blade.php
+++ resources/views/filters/filters.blade.php
@@ -11,18 +11,18 @@
             </div>
         </li>
     @endforeach
-    @else
+@else
 
-@foreach($filters->last()->children as $key=>$filter)
-            <li class="col-md-3" data-id="{!! $filter->id !!}">
-                <div class="item-content">
-                    <div class="item-photo">
-                        <img src="{!! ($filter->image)?url($filter->image):null !!}" alt="photo">
-                    </div>
-                    <div class="item-title">
-                        <span>{!! $filter->name !!}</span>
-                    </div>
+    @foreach($filters->last()->children as $key=>$filter)
+        <li class="col-md-3" data-id="{!! $filter->id !!}">
+            <div class="item-content">
+                <div class="item-photo">
+                    <img src="{!! ($filter->image)?url($filter->image):null !!}" alt="photo">
                 </div>
-            </li>
-@endforeach
+                <div class="item-title">
+                    <span>{!! $filter->name !!}</span>
+                </div>
+            </div>
+        </li>
+    @endforeach
 @endif
