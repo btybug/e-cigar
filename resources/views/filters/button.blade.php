110a8d43874a4cea8b1bdfb52933af2be679e8ea
Sahakavatar <hakobyan.sahak88@gmail.com>
2019-03-29 17:42:36 +0400

    up

--- resources/views/filters/button.blade.php
+++ resources/views/filters/button.blade.php
@@ -1,2 +1,1 @@
 <button type="button" class="btn btn-primary" data-toggle="modal" data-name="{!! $name !!}" data-multiple="{!! $is_multiple !!}" data-target="#wizardViewModal" data-action="{!! $category->id !!}">{!! $text !!}</button>
-
