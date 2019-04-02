170dd5ddba852c1c31955319a3df9ac8df7dad96
Sahakavatar <hakobyan.sahak88@gmail.com>
2019-04-02 11:14:16 +0400

    up

--- resources/views/filters/items.blade.php
+++ resources/views/filters/items.blade.php
@@ -1,6 +1,6 @@
 @foreach($items as $item)
     <li class="col-md-3" >
-        <div class="wrap-item position-relative" data-id="{!! $item->id !!}">
+        <div class="wrap-item position-relative" data-id="{!! $item->id !!}" data-price="{!! $item->price !!}">
             <a href="#" class="item-link">
                                         <span class="item-img">
                                             <img src="{!! $item->image !!}"
