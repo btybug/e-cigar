3800ea672a7ca2bf932a5bb1e6a55f89292c77ab
Sahakavatar <hakobyan.sahak88@gmail.com>
2019-04-02 14:04:57 +0400

    up

--- resources/views/filters/filter_select.blade.php
+++ resources/views/filters/filter_select.blade.php
@@ -1,4 +1,4 @@
-<div id="{!! $uniqId !!}" data-type="{!! $type !!}"  data-toggle="modal" data-group="{!! $group !!}" data-name="{!! $name.(($is_multiple)?'[]':null) !!}" data-multiple="{!! $is_multiple !!}"  data-action="{!! $category->id !!}">
+<div class="filters-select-wizard" id="{!! $uniqId !!}" data-type="{!! $type !!}"  data-toggle="modal" data-group="{!! $group !!}" data-name="{!! $name.(($is_multiple)?'[]':null) !!}" data-multiple="{!! $is_multiple !!}"  data-action="{!! $category->id !!}">
 
 {!! Form::open(['id'=>'filter-form']) !!}
 <div class="d-flex flex-wrap justify-content-center mb-2">
@@ -30,3 +30,29 @@
     </div>
 </div>
 </div>
+@push('javascript')
+    <script>
+        (function() {
+            $("body").on('change', '.filter-select', function () {
+                let data = $('form#filter-form').serialize();
+                AjaxCall("/filters", data, function (res) {
+                    if (!res.error) {
+                        switch (res.type) {
+                            case 'filter':
+                                $('.filter-children-items').empty();
+                                $('.filter-children-selects').html(res.html);
+                                break;
+                            case 'items':
+                                $('.filter-children-selects').html(res.html);
+                                $('.filter-children-items').html(res.items_html);
+                                $('#view-result .modal-footer').html('<div class="d-flex flex-wrap justify-content-between mb-1">\n' +
+                                    '<button type="button" class="btn btn-primary"><i class="fa fa-plus fa-sm mr-10"></i>Add</button>\n' +
+                                    '</div>');
+                                break;
+                        }
+                    }
+                });
+            });
+        })()
+    </script>
+@endpush
