17ce7e59bbf40f7234329d39fb5095cbf7fe40c1
David <hakobyan.sahak88@gmail.com>
2019-04-02 13:55:59 +0400

    up

--- resources/views/frontend/products/_partials/filter_types/select_filter.blade.php
+++ resources/views/frontend/products/_partials/filter_types/select_filter.blade.php
@@ -1,4 +1,4 @@
-<div class="col-sm-10 pl-0 limit" data-limit="{{ $vSettings->count_limit }}" data-id="{{ $vSettings->id }}"  data-price="{{ $vSettings->price }}" data-min-limit="{{ $vSettings->min_count_limit }}">
+<div class="col-sm-10 pl-0 limit" data-limit="{{ $vSettings->count_limit }}" data-id="{{ $vSettings->id }}"  data-price="{{ $vSettings->price }}" data-per-price="{{  $vSettings->price_per}}" data-min-limit="{{ $vSettings->min_count_limit }}">
     <div class="col-sm-12 pl-0 d-flex">
         @if(! $vSettings->is_required)
             {!! Form::checkbox('checkbox',1,null,['class' => 'custom-control-input ','id' => 'opt'.$vSettings->id]) !!}
