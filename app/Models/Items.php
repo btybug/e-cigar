fb62c64b8357930709f1a5cc0bb06f399474610d
terakyan <edo.terakyan@gmail.com>
2019-04-29 19:59:24 +0400

    up

--- app/Models/Items.php
+++ app/Models/Items.php
@@ -19,7 +19,7 @@ class Items extends Translatable
     protected $guarded = ['id'];
 
     public $translationModel = ItemTranslations::class;
-    
+
     public $translatedAttributes = ['name', 'short_description', 'long_description'];
 
     protected $appends = array('qty');
