456f90fc435236440014c21fc804417d020db316
sahakavatar <hakobyan.sahak88@gmail.com>
2019-05-31 11:05:03 +0400

    fix

--- resources/views/frontend/common/support.blade.php
+++ resources/views/frontend/common/support.blade.php
@@ -3,13 +3,13 @@
    <main class="main-content">
        <div class="container">
            <section class="qcbox_section">
-               <div class="product-introduce">
-                   <h1 class="mb-5 text-uppercase text-my-yellow">Support</h1>
-                   <div class="row">
+               <div class="product-introduce mb-5">
+                   <h1 class="mb-5 text-uppercase text-my-yellow mt-3">Support</h1>
+                   <div class="row justify-content-center">
                        <div class="col-sm-3">
                            <a href="{!! route('faq_page') !!}"
-                              class="text-center mb-4 px-5 py-4 d-flex flex-column d-block shadow-sm bg-white">
-                            <span class="d-inline-block mb-3"><i class="fa fa-5x fa-file-text"
+                              class="text-center mb-4 px-5 py-4 d-flex flex-column d-block shadow-sm bg-white h-100 justify-content-center">
+                            <span class="d-inline-block mb-3"><i class="fa fa-5x fa-file"
                                                                  aria-hidden="true"></i></span>
                                <strong>FAQ</strong>
                                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, sequi..</span>
@@ -19,7 +19,7 @@
 
                        <div class="col-sm-3">
                            <a href="{!! route('terms_conditions') !!}"
-                              class="text-center mb-4 px-5 py-4 d-flex flex-column d-block shadow-sm bg-white">
+                              class="text-center mb-4 px-5 py-4 d-flex flex-column d-block shadow-sm bg-white h-100 justify-content-center">
                                <span class="d-inline-block mb-3"><i class="fa fa-5x fa-list" aria-hidden="true"></i></span>
                                <strong>Terms & conditions</strong>
                                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, sequi..</span>
@@ -28,7 +28,7 @@
 
                        <div class="col-sm-3">
                            <a href="{!! route('delivery') !!}"
-                              class="text-center mb-4 px-5 py-4 d-flex flex-column d-block shadow-sm bg-white">
+                              class="text-center mb-4 px-5 py-4 d-flex flex-column d-block shadow-sm bg-white h-100 justify-content-center">
                                <span class="d-inline-block mb-3"><i class="fa fa-5x fa-paper-plane" aria-hidden="true"></i></span>
                                <strong>Delivery</strong>
                                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, sequi..</span>
