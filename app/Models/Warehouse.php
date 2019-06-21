5ec158783b4e0591aba639a51e60e554fa82b707
terakyan <edo.terakyan@gmail.com>
2019-06-21 16:10:14 +0400

    up

---
+++ app/Models/Warehouse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Common\Translatable;
+use App\Models\Translations\WarehouseTranslations;
+
+class Warehouse extends Translatable
+{
+    /**
+     * @var string
+     */
+
+    protected $table = 'warehouses';
+
+    public $translationModel = WarehouseTranslations::class;
+
+    public $translatedAttributes = ['name','description', 'address'];
+    /**
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    public function items()
+    {
+        return $this->belongsToMany(Items::class, 'filter_items', 'filter_id', 'item_id');
+    }
+}
