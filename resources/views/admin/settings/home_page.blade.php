4f826ff8e32e47b45f309598e6ab9cf42fd40a28
terakyan <edo.terakyan@gmail.com>
2019-07-01 17:48:23 +0400

    up

--- resources/views/admin/settings/home_page.blade.php
+++ resources/views/admin/settings/home_page.blade.php
@@ -63,6 +63,54 @@
 
                                 </div>
                             </div>
+
+                            <div class="card panel panel-default social-profile-page mb-3">
+                                <div class="card-header panel-heading">Bottom banners</div>
+                                <div class="card-body panel-body">
+                                    <div class="form-group d-flex flex-wrap align-items-center social-media-group">
+                                        @if($model && isset($model->social_media) && @json_decode($model->social_media,true))
+                                            @php
+                                                $social_medias=json_decode($model->social_media,true);
+                                            @endphp
+                                            @foreach($social_medias as $key=>$social_media)
+                                                <div class="clearfix"></div>
+                                                <div class="col-sm-6 p-0">
+                                                    <div class="input-group">
+                                                        <div class="input-group-prepend">
+                                                            {!! Form::file('home_page['.$key.'][slider]') !!}
+                                                            {{--{!! Form::hidden('social_media['.$key.'][social]',$social_media['social'],['class'=>'social_type']) !!}--}}
+                                                        </div>
+                                                        {{--{!! Form::text('social_media['.$key.'][url]', $social_media['url'], ['class' => 'form-control','id' => 'socialMedia','placeholder' => 'Profile URL','aria-label' => 'Text input with dropdown button']) !!}--}}
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-3">
+                                                    @if(!$key)
+                                                        <button type="button" class="btn btn-primary add-new-social-input">
+                                                            <i
+                                                                class="fa fa-plus"></i></button>
+                                                    @else
+                                                        <button class="plus-icon remove-new-social-input btn btn-danger">
+                                                            <i class="fa fa-minus"></i></button>
+                                                    @endif
+                                                </div>
+                                            @endforeach
+                                        @else
+                                            <div class="col-sm-6 p-0">
+                                                <div class="input-group">
+                                                    <div class="input-group-prepend">
+                                                        {!! Form::file('home_page[0][slider]') !!}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-3">
+                                                <button type="button" class="btn btn-primary add-new-social-input"><i
+                                                        class="fa fa-plus"></i></button>
+                                            </div>
+                                        @endif
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
