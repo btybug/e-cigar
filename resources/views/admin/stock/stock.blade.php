88a74ec51441c00b87adece870c281a48ad5f7a8
David <galstyan.david1993@gmail.com>
2019-07-01 17:59:04 +0400

    stock table header

--- resources/views/admin/stock/stock.blade.php
+++ resources/views/admin/stock/stock.blade.php
@@ -9,6 +9,13 @@
             @ok('admin_stock_new')<div class="pull-right "><a class="btn btn-primary pull-right" href="{!! route('admin_stock_new') !!}">Add new</a></div>@endok
         </div>
         <div class="card-body panel-body">
+            <select name="table_head" id="table_head_id" multiple>
+                <option value="#" data-column="0" data-name="id">#</option>
+                <option value="Name" data-column="1" data-name="name">Name</option>
+                <option value="Image" data-column="2" data-name="image">Image</option>
+                <option value="Added/Last Modified Date" data-column="3" data-name="created_at">Added/Last Modified Date</option>
+                <option value="Actions" data-column="4" data-name="actions">Actions</option>
+            </select>
             <table id="stocks-table" class="table table-style table-bordered" cellspacing="0" width="100%">
                 <thead>
                 <tr>
@@ -25,8 +32,52 @@
 @stop
 @section('js')
     <script>
-        $(function () {
-            $('#stocks-table').DataTable({
+        $(document).ready(function() {
+
+            if(!localStorage.getItem("stock_table")) {
+                localStorage.setItem("stock_table", JSON.stringify([
+                    {id: '#', text: '#', name: 'id'},
+                    {id: 'Name', text: 'Name', name: 'name'},
+                    {id: 'Image', text: 'Image', name: 'image'},
+                    {id: 'Added/Last Modified Date', text: 'Added/Last Modified Date', name: 'created_at'},
+                    {id: 'Actions', text: 'Actions', name: 'actions'}
+                ]))
+            }
+            JSON.parse(localStorage.getItem("stock_table")).map((el) => {
+                $('#table_head_id').find(`[data-name="${el.name}"]`).attr('selected', 'selected')
+            });
+            $('#table_head_id').select2({
+                multiple: true,
+                initSelection: function (element, callback) {
+                    var selected_items = JSON.parse(localStorage.getItem("stock_table"));
+
+                    callback(selected_items);
+                }
+            });
+
+            var tableHeadArray = [
+                {data: 'id', name: 'id'},
+                {data: 'name', name: 'name'},
+                {data: 'image', name: 'image'},
+                {data: 'created_at', name: 'created_at'},
+                {data: 'actions', name: 'actions'}
+            ];
+
+            tableArray = tableHeadArray.map((head) => {
+                const id = head.data;
+                var visible = JSON.parse(localStorage.getItem("stock_table")).find((el) => {
+                    return el.name === id;
+                });
+                if(visible) {
+                    return head;
+                } else {
+                    return {
+                        ...head,
+                        visible: false
+                    };
+                }
+            });
+            var table = $('#stocks-table').DataTable({
                 ajax: "{!! route('datatable_all_stocks') !!}",
                 "processing": true,
                 "serverSide": true,
@@ -35,13 +86,31 @@
                 buttons: [
                     'csv', 'excel', 'pdf', 'print'
                 ],
-                columns: [
-                    {data: 'id', name: 'id'},
-                    {data: 'name', name: 'name'},
-                    {data: 'image', name: 'image'},
-                    {data: 'created_at', name: 'created_at'},
-                    {data: 'actions', name: 'actions'}
-                ]
+                columns: tableHeadArray
+            });
+
+            function init() {
+                var selected_items = [];
+                $("#table_head_id option").each(function() {
+                    var column = table.column($(this).attr('data-column'));
+                    if($(this).is(':selected')) {
+                        selected_items.push({
+                            id: $(this).val(),
+                            text: $(this).val(),
+                            name: $(this).attr("data-name")
+                        });
+                        column.visible(true);
+                    } else {
+                        column.visible(false);
+                    }
+                });
+                localStorage.setItem("stock_table", JSON.stringify(selected_items))
+            }
+
+            init();
+
+            $('#table_head_id').on('change.select2', function (e) {
+                init();
             });
         });
 
