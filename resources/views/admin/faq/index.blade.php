3905c837317d7e9b00d6192e5a01b99b78386b27
David <galstyan.david1993@gmail.com>
2019-07-01 23:50:08 +0400

    faq table

--- resources/views/admin/faq/index.blade.php
+++ resources/views/admin/faq/index.blade.php
@@ -9,6 +9,15 @@
            @ok('admin_faq_create') <div class="pull-right"><a class="btn btn-primary pull-right" href="{!! route('admin_faq_create') !!}">Add new</a></div>@endok
         </div>
         <div class="card-body panel-body">
+            <select name="table_head" id="table_head_id" multiple>
+                <option value="ID" data-column="0" data-name="id">ID</option>
+                <option value="Author" data-column="1" data-name="user_id">Author</option>
+                <option value="Question" data-column="2" data-name="question">Question</option>
+                <option value="Answer" data-column="3" data-name="answer">Answer</option>
+                <option value="Status" data-column="4" data-name="status">Status</option>
+                <option value="Added/Last Modified Date" data-column="5" data-name="created_at">Added/Last Modified Date</option>
+                <option value="Action" data-column="6" data-name="actions">Action</option>
+            </select>
             <table id="posts-table" class="table table-style table-bordered" cellspacing="0" width="100%">
                 <thead>
                 <tr>
@@ -27,17 +36,91 @@
 @stop
 @section('js')
     <script>
-        $(function () {
-            $('#posts-table').DataTable({
-                ajax: "{!! route('datatable_all_faq') !!}",
-                "processing": true,
-                "serverSide": true,
-                "bPaginate": true,
-                dom: 'Bfrtip',
-                buttons: [
-                    'csv', 'excel', 'pdf', 'print'
+        $(document).ready(function() {
+
+            function tableInit(storageName, selectData, selectId, tableData, tableId, ajaxUrl) {
+                if(!localStorage.getItem(storageName)) {
+                    localStorage.setItem(storageName, JSON.stringify(selectData))
+                }
+                JSON.parse(localStorage.getItem(storageName)).map((el) => {
+                    $(selectId).find(`[data-name="${el.name}"]`).attr('selected', 'selected')
+                });
+                $(selectId).select2({
+                    multiple: true,
+                    initSelection: function (element, callback) {
+                        var selected_items = JSON.parse(localStorage.getItem(storageName));
+
+                        callback(selected_items);
+                    }
+                });
+
+                var tableHeadArray = tableData;
+
+                tableArray = tableHeadArray.map((head) => {
+                    const id = head.data;
+                    var visible = JSON.parse(localStorage.getItem(storageName)).find((el) => {
+                        return el.name === id;
+                    });
+                    if(visible) {
+                        return head;
+                    } else {
+                        return {
+                            ...head,
+                            visible: false
+                        };
+                    }
+                });
+                
+                var table = $(tableId).DataTable({
+                    ajax: ajaxUrl,
+                    "processing": true,
+                    "serverSide": true,
+                    "bPaginate": true,
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'csv', 'excel', 'pdf', 'print'
+                    ],
+                    columns: tableHeadArray
+                });
+
+                function init() {
+                    var selected_items = [];
+                    $(`${selectId} option`).each(function() {
+                        var column = table.column($(this).attr('data-column'));
+                        if($(this).is(':selected')) {
+                            selected_items.push({
+                                id: $(this).val(),
+                                text: $(this).val(),
+                                name: $(this).attr("data-name")
+                            });
+                            column.visible(true);
+                        } else {
+                            column.visible(false);
+                        }
+                    });
+                    localStorage.setItem(storageName, JSON.stringify(selected_items))
+                }
+
+                init();
+
+                $(selectId).on('change.select2', function (e) {
+                    init();
+                });
+            }
+
+            tableInit(
+                "faq_table",
+                [
+                    {id: 'ID', text: 'ID', name: 'id'},
+                    {id: 'Author', text: 'Author', name: 'user_id'},
+                    {id: 'Question', text: 'Question', name: 'question'},
+                    {id: 'Answer', text: 'Answer', name: 'answer'},
+                    {id: 'Status', text: 'Status', name: 'status'},
+                    {id: 'Added/Last Modified Date', text: 'Added/Last Modified Date', name: 'created_at'},
+                    {id: 'Action', text: 'Action', name: 'actions'},
                 ],
-                columns: [
+                '#table_head_id',
+                [
                     {data: 'id', name: 'id'},
                     {data: 'user_id', name: 'user_id'},
                     {data: 'question', name: 'question'},
@@ -45,8 +128,10 @@
                     {data: 'status', name: 'status'},
                     {data: 'created_at', name: 'created_at'},
                     {data: 'actions', name: 'actions'}
-                ]
-            });
+                ],
+                '#posts-table',
+                "{!! route('datatable_all_faq') !!}"
+            )
         });
 
     </script>
