3f3bcbc73be6a2cd6ccdda4d3c56486e7e898948
terakyan <edo.terakyan@gmail.com>
2019-07-02 14:39:59 +0400

    up

--- resources/views/admin/stock/_partials/variation.blade.php
+++ resources/views/admin/stock/_partials/variation.blade.php
@@ -9,24 +9,37 @@
             <div class="col-md-3">
                 {!! Form::text("variations[$main_unique][title]",($main) ? $main->title : null,['class' => 'form-control mr-1','placeholder' => 'Enter title ...']) !!}
             </div>
-            <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-6">
-                        {!! Form::select("variations[$main_unique][type]",['' => 'Select','package_product' => 'Multiple items','filter' => 'Filters'
-               ],($main) ? $main->type : null,
-               ['class' => 'form-control variation-product-select']) !!}
-                    </div>
-                    <div class="col-md-6 filter-option {{ ($main && $main->type =='filter') ? '' : 'hide' }}">
-                        {!! Form::select("variations[$main_unique][filter_category_id]",['' => 'Select Filter']+$filters,($main) ? $main->filter_category_id : null,
-                        ['class' => 'form-control filter-select']) !!}
-                    </div>
+            <div class="col-md-9">
+                <div class="col-md-6">
+                    {!! Form::select("variations[$main_unique][type]",['' => 'Select','package_product' => 'Multiple items','filter' => 'Filters'
+           ],($main) ? $main->type : null,
+           ['class' => 'form-control variation-product-select']) !!}
+                </div>
+                <div class="col-md-6 filter-option {{ ($main && $main->type =='filter') ? '' : 'hide' }}">
+                    {!! Form::select("variations[$main_unique][filter_category_id]",['' => 'Select Filter']+$filters,($main) ? $main->filter_category_id : null,
+                    ['class' => 'form-control filter-select']) !!}
                 </div>
             </div>
         </div>
-        <div class="col-md-6 d-flex pr-0 head-right justify-content-end">
-            {!! Form::hidden("variations[$main_unique][is_required]",$required) !!}
-            <button type="button" class="btn btn-danger delete-v-option"><i class="fa fa-times"></i></button>
+        <div class="col-md-6 d-flex pr-0">
+            <div class="col-md-6 multi-option {{ ($main && $main->type =='package_product') ? '' : 'hide' }}">
+                <div class="row">
+                    <div class="col-md-6">
+                        Min Limit : {!! Form::number("variations[$main_unique][min_count_limit]",
+                                (($main) ? $main->min_count_limit : null),['class' => 'form-control']) !!}
+                    </div>
+                    <div class="col-md-6">
+                        How Many items user can select : {!! Form::number("variations[$main_unique][count_limit]",
+                                ($main) ? $main->count_limit : null,['class' => 'form-control']) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 d-flex pr-0 head-right justify-content-end">
+                {!! Form::hidden("variations[$main_unique][is_required]",$required) !!}
+                <button type="button" class="btn btn-danger delete-v-option"><i class="fa fa-times"></i></button>
+            </div>
         </div>
+
 </div>
 <div class="card-body panel-body">
 <div class="row">
