0f82cc7dd8c8ccfc3a5f9bcfc7af9a744d30aaa6
terakyan <edo.terakyan@gmail.com>
2019-07-03 14:47:50 +0400

    discount

--- resources/views/admin/stock/_partials/discount_data.blade.php
+++ resources/views/admin/stock/_partials/discount_data.blade.php
@@ -1,26 +1,26 @@
 @if(isset($ajax) && $ajax == false)
-    {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount_type]",$package_variation->discount_type) !!}
+    {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount_type]",$package_variation->discount_type,['data-type-discount' => 'discount_type']) !!}
     @foreach($package_variation->discounts as $key => $datum)
         @if($package_variation->discount_type == 'range')
-            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][from]",$datum->from) !!}
-            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][to]",$datum->to) !!}
-            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][price]",$datum->price) !!}
+            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][from]",$datum->from,['data-type-discount' => 'discount','data-key' => $key]) !!}
+            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][to]",$datum->to,['data-type-discount' => 'discount','data-key' => $key]) !!}
+            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][price]",$datum->price,['data-type-discount' => 'discount','data-key' => $key]) !!}
         @else
-            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][qty]",$datum->qty) !!}
-            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][price]",$datum->price) !!}
+            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][qty]",$datum->qty,['data-type-discount' => 'discount','data-key' => $key]) !!}
+            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][price]",$datum->price,['data-type-discount' => 'discount','data-key' => $key]) !!}
         @endif
     @endforeach
 @else
     @if(count($data))
         @foreach($data as $key => $datum)
-            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount_type]",$type) !!}
+            {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount_type]",$type,['data-type-discount' => 'discount_type']) !!}
             @if($type == 'range')
-                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][from]",$datum['from']) !!}
-                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][to]",$datum['to']) !!}
-                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][price]",$datum['price']) !!}
+                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][from]",$datum['from'],['data-type-discount' => 'discount','data-key' => $key]) !!}
+                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][to]",$datum['to'],['data-type-discount' => 'discount','data-key' => $key]) !!}
+                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][price]",$datum['price'],['data-type-discount' => 'discount','data-key' => $key]) !!}
             @else
-                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][qty]",$datum['qty']) !!}
-                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][price]",$datum['price']) !!}
+                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][qty]",$datum['qty'],['data-type-discount' => 'discount','data-key' => $key]) !!}
+                {!! Form::hidden("variations[$main_unique][variations][$uniqueID][discount][$key][price]",$datum['price'],['data-type-discount' => 'discount','data-key' => $key]) !!}
             @endif
         @endforeach
     @endif
