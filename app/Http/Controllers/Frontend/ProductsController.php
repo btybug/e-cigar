268ccf2a75bb7663958acc3b52552fc894cd9e02
terakyan <edo.terakyan@gmail.com>
2019-07-10 14:42:16 +0400

    up

--- app/Http/Controllers/Frontend/ProductsController.php
+++ app/Http/Controllers/Frontend/ProductsController.php
@@ -195,13 +195,14 @@ class ProductsController extends Controller
         if($id){
             $variation = StockVariation::where('variation_id',$id)->get();
             $selected = $vSettings = $variation->first();
+            $html = \view("frontend.products._partials.stock_variation_option_box", compact(['variation', 'selected','vSettings']))->render();
         }else{
             $selected = StockVariation::findOrFail($request->select_element_id);
             $variation = $selected->stock->variations()->where('variation_id',$selected->variation_id)->get();
             $vSettings = $variation->first();
+            $html = \view("frontend.products._partials.stock_variation_option", compact(['variation', 'selected','vSettings']))->render();
         }
 
-        $html = \view("frontend.products._partials.stock_variation_option", compact(['variation', 'selected','vSettings']))->render();
 
         return \Response::json(['error' => false, 'html' => $html]);
     }
