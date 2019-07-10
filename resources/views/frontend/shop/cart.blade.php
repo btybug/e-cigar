60066e0f896cb4c90e86d36c9f08b2915230d4e0
sahakavatar <hakobyan.sahak88@gmail.com>
2019-07-10 17:45:29 +0400

    shopping cart

--- resources/views/frontend/shop/cart.blade.php
+++ resources/views/frontend/shop/cart.blade.php
@@ -1,66 +1,83 @@
 @extends('layouts.frontend')
 @section('content')
     <main class="main-content">
-        <div class="shopping-cart_wrapper">
+        <div class="shopping-cart_wrapper shopping__cart-wrapper">
             <div class="container main-max-width">
                 <div class="shopping-cart-head">
                     <ul class="nav nav-pills">
                         <li class="nav-item col-md-3">
                             <a class="item active d-flex align-items-center justify-content-between"
                                ref="javascript:void(0);">
-                                <span class="name text-uppercase font-main-bold font-16 text-truncate">SHOPPING CART</span>
+                                <span class="font-sec-reg text-main-clr num">1</span>
+                                <span
+                                    class="name text-uppercase font-main-bold font-16 text-truncate">SHOPPING CART</span>
                                 <span class="icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                         width="21px" height="21px">
-                                    <path fill-rule="evenodd" fill="rgb(81, 132, 229)"
-                                          d="M10.501,0.000 C4.702,0.000 0.000,4.700 0.000,10.499 C0.000,16.298 4.702,21.000 10.501,21.000 C16.300,21.000 21.000,16.298 21.000,10.499 C21.000,4.700 16.300,0.000 10.501,0.000 ZM16.216,7.475 L9.908,14.572 C9.753,14.745 9.535,14.838 9.315,14.838 C9.143,14.838 8.969,14.779 8.824,14.664 L4.880,11.511 C4.542,11.239 4.485,10.742 4.760,10.401 C5.030,10.060 5.528,10.006 5.866,10.278 L9.224,12.964 L15.036,6.425 C15.325,6.101 15.825,6.072 16.150,6.361 C16.475,6.650 16.506,7.149 16.216,7.475 Z"/>
-                                </svg>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    width="24px" height="19px">
+<path fill-rule="evenodd" fill="rgb(81, 229, 109)"
+      d="M7.636,15.030 L1.909,9.075 L-0.000,11.060 L7.636,19.000 L24.000,1.985 L22.091,0.000 L7.636,15.030 Z"/>
+</svg>
                                 </span>
                             </a>
                         </li>
                         <li class="nav-item col-md-3">
                             <a class="item d-flex align-items-center justify-content-between"
                                href="javascript:void(0);">
+                                <span class="font-sec-reg text-main-clr num">2</span>
                                 <span class="name text-uppercase font-main-bold font-16 text-truncate">CHECKOUT</span>
                                 <span class="icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                         width="21px" height="21px">
-                                    <path fill-rule="evenodd" fill="rgb(81, 132, 229)"
-                                          d="M10.501,0.000 C4.702,0.000 0.000,4.700 0.000,10.499 C0.000,16.298 4.702,21.000 10.501,21.000 C16.300,21.000 21.000,16.298 21.000,10.499 C21.000,4.700 16.300,0.000 10.501,0.000 ZM16.216,7.475 L9.908,14.572 C9.753,14.745 9.535,14.838 9.315,14.838 C9.143,14.838 8.969,14.779 8.824,14.664 L4.880,11.511 C4.542,11.239 4.485,10.742 4.760,10.401 C5.030,10.060 5.528,10.006 5.866,10.278 L9.224,12.964 L15.036,6.425 C15.325,6.101 15.825,6.072 16.150,6.361 C16.475,6.650 16.506,7.149 16.216,7.475 Z"/>
-                                </svg>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        width="24px" height="19px">
+<path fill-rule="evenodd" fill="rgb(81, 229, 109)"
+      d="M7.636,15.030 L1.909,9.075 L-0.000,11.060 L7.636,19.000 L24.000,1.985 L22.091,0.000 L7.636,15.030 Z"/>
+</svg>
                                 </span>
                             </a>
                         </li>
                         <li class="nav-item col-md-3">
                             <a class="item d-flex align-items-center justify-content-between"
                                href="javascript:void(0);">
+                                <span class="font-sec-reg text-main-clr num">3</span>
                                 <span class="name text-uppercase font-main-bold font-16 text-truncate">Payment</span>
                                 <span class="icon">
-                                    <svg width="21px" height="21px">
-                                    <path fill-rule="evenodd" fill="rgb(81, 132, 229)"
-                                          d="M10.501,0.000 C4.702,0.000 0.000,4.700 0.000,10.499 C0.000,16.298 4.702,21.000 10.501,21.000 C16.300,21.000 21.000,16.298 21.000,10.499 C21.000,4.700 16.300,0.000 10.501,0.000 ZM16.216,7.475 L9.908,14.572 C9.753,14.745 9.535,14.838 9.315,14.838 C9.143,14.838 8.969,14.779 8.824,14.664 L4.880,11.511 C4.542,11.239 4.485,10.742 4.760,10.401 C5.030,10.060 5.528,10.006 5.866,10.278 L9.224,12.964 L15.036,6.425 C15.325,6.101 15.825,6.072 16.150,6.361 C16.475,6.650 16.506,7.149 16.216,7.475 Z"/>
-                                </svg>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        width="24px" height="19px">
+<path fill-rule="evenodd" fill="rgb(81, 229, 109)"
+      d="M7.636,15.030 L1.909,9.075 L-0.000,11.060 L7.636,19.000 L24.000,1.985 L22.091,0.000 L7.636,15.030 Z"/>
+</svg>
                                 </span>
                             </a>
                         </li>
                         <li class="nav-item col-md-3">
                             <a class="item d-flex align-items-center justify-content-between"
                                href="javascript:void(0);">
-                                <span class="name text-uppercase font-main-bold font-16 text-truncate">Confirmation</span>
+                                <span class="font-sec-reg text-main-clr num">4</span>
+                                <span
+                                    class="name text-uppercase font-main-bold font-16 text-truncate">Confirmation</span>
                                 <span class="icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                         width="21px" height="21px">
-                                    <path fill-rule="evenodd" fill="rgb(81, 132, 229)"
-                                          d="M10.501,0.000 C4.702,0.000 0.000,4.700 0.000,10.499 C0.000,16.298 4.702,21.000 10.501,21.000 C16.300,21.000 21.000,16.298 21.000,10.499 C21.000,4.700 16.300,0.000 10.501,0.000 ZM16.216,7.475 L9.908,14.572 C9.753,14.745 9.535,14.838 9.315,14.838 C9.143,14.838 8.969,14.779 8.824,14.664 L4.880,11.511 C4.542,11.239 4.485,10.742 4.760,10.401 C5.030,10.060 5.528,10.006 5.866,10.278 L9.224,12.964 L15.036,6.425 C15.325,6.101 15.825,6.072 16.150,6.361 C16.475,6.650 16.506,7.149 16.216,7.475 Z"/>
-                                </svg>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                                        width="24px" height="19px">
+<path fill-rule="evenodd" fill="rgb(81, 229, 109)"
+      d="M7.636,15.030 L1.909,9.075 L-0.000,11.060 L7.636,19.000 L24.000,1.985 L22.091,0.000 L7.636,15.030 Z"/>
+</svg>
                                 </span>
                             </a>
                         </li>
                     </ul>
                 </div>
-                <span class="cart-area">
+            </div>
+            <div class="container main-max-width shopping__cart-mw">
+                <div class="cart-area">
                 @include('frontend.shop._partials.cart_table')
-                </span>
+                </div>
             </div>
         </div>
 
