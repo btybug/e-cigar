6182afa619a0f1ea7855deadca5ddf4042d25f87
terakyan <edo.terakyan@gmail.com>
2019-07-11 12:16:28 +0400

    up

--- resources/views/frontend/products/_partials/variation_types/popup.blade.php
+++ resources/views/frontend/products/_partials/variation_types/popup.blade.php
@@ -1,53 +1,31 @@
-<div class="col-sm-10 pl-0 limit"
-     data-req="{{ $vSettings->is_required }}"
-     data-per-price="{{ $vSettings->price_per }}" data-price="{{ convert_price($vSettings->price,$currency,false,true) }}"
-     data-limit="{{ $vSettings->count_limit }}" data-id="{{ $vSettings->id }}"
+<div class="product__single-item-info mb-3 limit {{$vSettings->type}}"
+     data-group-id="{{ $vSettings->variation_id }}"
+     data-req="{{ $vSettings->is_required }}" data-id="{{ $vSettings->id }}" data-limit="{{ $vSettings->count_limit }}"
+     data-per-price="{{ $vSettings->price_per }}"
+     data-price="{{ convert_price($vSettings->price,$currency,false,true) }}"
      data-min-limit="{{ $vSettings->min_count_limit }}">
-    <div class="col-sm-12 pl-0 d-flex">
-      @if(! $vSettings->is_required)
-        {!! Form::checkbox('checkbox',1,null,['class' => 'custom-control-input req_check ','id' => 'opt'.$vSettings->id]) !!}
-        <label class="product-single-info_check-label custom-control-label font-15 text-gray-clr pointer "
-               for="opt{{ $vSettings->id }}">
-          <h4>
-            @if($vSettings->min_count_limit == 1 && $vSettings->count_limit == 1)
-              {{ $vSettings->title }} (you can select one option)
-            @else
-              {{ $vSettings->title }} (select {{ $vSettings->min_count_limit }} - {{ $vSettings->count_limit }} options)
+
+    <div
+        class="d-flex flex-wrap align-items-center lh-1 product__single-item-info-top">
+        <div class="col-md-9 pl-0">
+            <a href="javascript:void(0)" class="font-sec-light font-22 text-uppercase  product_select-link-btn  popup-select" data-group="{{ $vSettings->variation_id }}">
+                Select Products
+            </a>
+        </div>
+        <div class="col-md-3 d-flex justify-content-end pr-0">
+            @if($vSettings->price_per == 'product')
+                <div class="product__single-item-info-price" data-single-price="{{ $vSettings->price }}">
+                    <span class="font-40 product__single-item_price">
+                            {{ convert_price($vSettings->price,$currency, false) }}
+                    </span>
+                </div>
             @endif
-          </h4>
-        </label>
-      @else
-        <h4>
-          @if($vSettings->min_count_limit == 1 && $vSettings->count_limit == 1)
-            {{ $vSettings->title }} (you can select one option)
-          @else
-            {{ $vSettings->title }} (select {{ $vSettings->min_count_limit }} - {{ $vSettings->count_limit }} options)
-          @endif</h4>
-     @endif
-        @php
-          $id = (($vSettings->count_limit > 1) ? "multi_v_select_$vSettings->id" : "single_v_select_$vSettings->id");
-          $class = (($vSettings->count_limit > 1) ? "multi_v_select" : "");
-        @endphp
-
-
+        </div>
     </div>
-  <button type="button" class="btn btn-primary rounded-0 popup-select wall--wrapper" data-group="{{ $vSettings->variation_id }}" >
-    Select options
-  </button>
-</div>
 
-<div class="col-sm-2 pl-sm-3 p-0 text-sm-center">
-    @if($vSettings->price_per == 'product' && ! $vSettings->stock->type)
-        <span class="d-inline-block font-35 font-sec-bold text-uppercase ml-auto price-placee">
-            @if($vSettings->is_required)
-              {{ convert_price($vSettings->price,$currency, false) }}
-            @else
-              Nothing selected
-            @endif
-        </span>
-    @endif
-</div>
-
-<div class="selected-menu-options">
-
+    <div class="d-flex flex-wrap align-items-end mb-2 product__single-item-info-bottom">
+        @if(isset($selected) && $selected)
+            @include("frontend.products._partials.stock_variation_option")
+        @endif
+    </div>
 </div>
