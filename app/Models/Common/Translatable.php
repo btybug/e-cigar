7529d65703390136a239a0a514ae6cba6f1881bf
sahakavatar <hakobyan.sahak88@gmail.com>
2019-07-11 17:19:41 +0400

    up

--- app/Models/Common/Translatable.php
+++ app/Models/Common/Translatable.php
@@ -51,11 +51,13 @@ class Translatable extends Model
     public static function updateOrCreate(int $id = null, array $data,array $translations = [])
     {
         $model = self::find($id)??new static();
-        $translatableData = (count($translations)) ? $translations :\Request::get('translatable');
+        $translatableData = (count($translations)) ? $translations :\Request::get('translatable',[]);
         (isset($model->id)) ? $model->update($data) : $model->fill($data) ;
 
         if ($translatableData && count($translatableData)) {
+
             foreach ($translatableData as $locale => $translateData) {
+
                 //here need check locales later
                 if (count($translateData)) {
                     foreach ($translateData as $column => $translate) {
@@ -66,6 +68,7 @@ class Translatable extends Model
                 }
             }
         }
+        dd($model);
         $model->save();
         return $model;
     }
