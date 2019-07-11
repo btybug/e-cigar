503bfbfc1f92528ff1a19fd8532bbc830dd9c160
sahakavatar <hakobyan.sahak88@gmail.com>
2019-07-11 15:12:26 +0400

    up

--- resources/views/admin/settings/_partials/main_pages/single_product.blade.php
+++ resources/views/admin/settings/_partials/main_pages/single_product.blade.php
@@ -1,1 +1,16 @@
+<div class="form-group">
+    <div class="card panel panel-default">
+        <div class="card-header panel-heading clearfix">
+            <p>Gallery images</p>
+        </div>
+        <div class="card-body">
+            <div>
+                {!! media_button('gallery',$model,true) !!}
+            </div>
+        </div>
+    </div>
+</div>
+@section('js')
+
+@stop
 
