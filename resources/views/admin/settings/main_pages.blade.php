739c4c66c5c61120b243f59a1f9ddc931bc7186f
sahakavatar <hakobyan.sahak88@gmail.com>
2019-07-11 12:40:05 +0400

    up

--- resources/views/admin/settings/main_pages.blade.php
+++ resources/views/admin/settings/main_pages.blade.php
@@ -9,7 +9,19 @@
             @include("admin.settings._partials.menu",['active'=> 'main_pages'])
         </div>
         <div>
-            <div class="col-3"></div>
+            <div class="col-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home page</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">T&C</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+                </ul>
+            </div>
             <div class="col-9"></div>
         </div>
 
