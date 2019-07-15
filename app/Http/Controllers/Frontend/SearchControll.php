24be8367407ed0d75f12d792a1ac549f9d066e40
sahakavatar <hakobyan.sahak88@gmail.com>
2019-07-15 13:53:30 +0400

    up

---
+++ app/Http/Controllers/Frontend/SearchControll.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Http\Controllers\Frontend;
+
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Stock;
+use App\ProductSearch\ProductSearch;
+use Illuminate\Http\Request;
+
+class SearchControll extends Controller
+{
+    public function postSearch(Request $request)
+    {
+        $category = $request->get('category', false);
+        $name = $request->get('name');
+
+        $query = Stock::leftJoin('stock_translations', 'stocks.id', '=', 'stock_translations.stock_id');
+        if ($category) {
+            $query->leftJoin('stock_categories', 'stocks.id', '=', 'stock_categories.stock_id');
+            $query->where('stock_categories.slug', $category);
+        }
+        $result = $query->select(
+            'stocks.image',
+            'stocks.slug',
+            'stock_translations.*'
+        )
+            ->where('stock_translations.locale', app()->getLocale())
+            ->where('stocks.status', true)
+            ->where('stocks.is_offer', false)
+            ->where('stock_translations.name', 'LIKE', '%' . $name . '%')
+            ->get();
+        return response()->json(['data' => $result->toArray(), 'code' => 200]);
+    }
+
+}
