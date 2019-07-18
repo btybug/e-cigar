e565c9da64f44572accd97cd2fd2333cd216fd75
sahakavatar <hakobyan.sahak88@gmail.com>
2019-07-18 16:39:29 +0400

    up

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -570,8 +570,8 @@ class SettingsController extends Controller
 
     public function getMainPages(Settings $settings, Request $request)
     {
-        $p = $request->get('p', 'home_page');
-        if ($p == 'home_page' || $p == "single_product" || $p == "single_post" || $p == "my_account") {
+        $p = $request->get('p', 'banners');
+        if ($p == 'banners' || $p == "single_product" || $p == "single_post" || $p == "my_account") {
             $model = $settings->getEditableData($p);
         } else {
             $model = Common::where('type', $p)->first();
@@ -589,8 +589,8 @@ class SettingsController extends Controller
 
     public function postMainPages(Request $request, Settings $settings)
     {
-        $p = $request->get('p', 'hom_page');
-        if ($p == "home_page" || $p == "single_product" || $p == "single_post" || $p == "my_account") {
+        $p = $request->get('p', 'banners');
+        if ($p == "banners" || $p == "single_product" || $p == "single_post" || $p == "my_account") {
             $banners = array_filter($request->get($p, []));
             $settings->updateOrCreateSettings($p, ['data' => $banners]);
         } else {
