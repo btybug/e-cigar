0f721e144142b4d503c6b36a3af2cc5e79d49aad
terakyan <edo.terakyan@gmail.com>
2019-07-26 15:56:15 +0400

    up

--- resources/views/frontend/guest/contact_us.blade.php
+++ resources/views/frontend/guest/contact_us.blade.php
@@ -144,7 +144,8 @@
                             <div class="info-wall">
                                 <div class="map-block">
                                     <iframe
-                                        src="https://www.google.com/maps/embed?q={{ $settings->first_address. '+'.$settings->second_address. '+'.$settings->city. '+'.$settings->post_code. '+'.$settings->country }}"
+                                        src="https://www.google.com/maps/embed?bp=Armenia"
+{{--{{ $settings->first_address. '+'.$settings->second_address. '+'.$settings->city. '+'.$settings->post_code. '+'.$settings->country }}"--}}
                                         width="100%" height="100%" frameborder="0" style="border:0"
                                         allowfullscreen></iframe>
                                 </div>
