e8addc6c72885e8a05a30b8185c238057adc2f4a
sahakavatar <hakobyan.sahak88@gmail.com>
2019-09-30 13:16:20 +0400

    fix

--- resources/views/frontend/_partials/footer.blade.php
+++ resources/views/frontend/_partials/footer.blade.php
@@ -42,79 +42,83 @@
                         <p class="mb-0">There are many variations of passages<br/>
                             of Lorem Ipsum available, but the majority</p>
                     </div>
-                    {{--<div class="footer-payment">--}}
-                        {{--<div>--}}
-                            {{--<img src="/public/img/paypal.png" alt="paypal">--}}
-                            {{--<img src="/public/img/visa.png" alt="visa">--}}
-                            {{--<img src="/public/img/maestro.png" alt="maestro">--}}
-                            {{--<img src="/public/img/mc.png" alt="mc">--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-
-                    <div class="footer-social mb-3">
-                        <ul class="d-inline-flex flex-wrap">
-                            <li><a href="#" class="d-flex align-items-center justify-content-center"><span><i
-                                                class="fab fa-facebook-f"></i></span></a></li>
-                            <li class="active"><a href="#"
-                                                  class="d-flex align-items-center justify-content-center"><span><i
-                                                class="fab fa-instagram"></i></span></a></li>
-                            <li><a href="#" class="d-flex align-items-center justify-content-center"><span><i
-                                                class="fab fa-twitter"></i></span></a></li>
-                            <li><a href="#" class="d-flex align-items-center justify-content-center"><span><i
-                                                class="fab fa-google-plus-g"></i></span></a></li>
-                            <li><a href="#" class="d-flex align-items-center justify-content-center"><span><i
-                                                class="fab fa-youtube"></i></span></a></li>
-                        </ul>
+                    <div class="footer-payment">
+                        <div>
+                            <img src="/public/img/paypal.png" alt="paypal">
+                            <img src="/public/img/visa.png" alt="visa">
+                            <img src="/public/img/maestro.png" alt="maestro">
+                            <img src="/public/img/mc.png" alt="mc">
+                        </div>
                     </div>
-                    {!! Form::open(['url' => route('subscribe_to_newsletter')]) !!}
-                        <div class="footer-subscribe">
 
-                            <div class="d-flex position-relative align-items-center">
-                                    <label for="footer-subscribe" class="font-main-bold text-uppercase mb-0 text-nowrap">SUBSCRIBE
-                                        |</label>
-                                    {!! Form::email('subscribe_email',null,['class' => 'form-control','placeholder' => 'Your email','id' => 'footer-subscribe']) !!}
-                                    <span class="arrow position-absolute">
+
+
+{{--                    <div class="copyright">--}}
+{{--                        &#9400; Copyright 2019 Kaliony UK--}}
+{{--                    </div>--}}
+                </div>
+            </div>
+            <div class="d-flex flex-wrap justify-content-between align-items-center footer-subscribe-social-wrap">
+
+                {!! Form::open(['url' => route('subscribe_to_newsletter')]) !!}
+                <div class="footer-subscribe">
+
+                    <div class="d-flex position-relative align-items-center">
+                        <label for="footer-subscribe" class="font-main-bold text-uppercase mb-0 text-nowrap">SUBSCRIBE
+                            |</label>
+                        {!! Form::email('subscribe_email',null,['class' => 'form-control','placeholder' => 'Your email','id' => 'footer-subscribe']) !!}
+                        <span class="arrow position-absolute">
                                         <svg viewBox="0 0 24 8"
-                                                width="24px" height="8px">
+                                             width="24px" height="8px">
         <path fill-rule="evenodd" fill="rgb(81, 132, 229)"
               d="M23.791,3.998 L18.090,0.000 L18.090,2.888 L0.791,2.888 L0.791,5.109 L18.090,5.109 L18.090,7.997 L23.791,3.998 Z"/>
         </svg>
                                     </span>
-                            </div>
-                            @if ($errors->has('subscribe_email'))
-                                <span class="invalid-feedback" style="display: block" role="alert">
+                    </div>
+                    @if ($errors->has('subscribe_email'))
+                        <span class="invalid-feedback" style="display: block" role="alert">
                                     <strong>{{ $errors->first('subscribe_email') }}</strong>
                                 </span>
-                            @endif
-                        </div>
-                    {!! Form::close() !!}
-
-                    <div class="copyright">
-                        &#9400; Copyright 2019 Kaliony UK
-                    </div>
+                    @endif
+                </div>
+                {!! Form::close() !!}
+                <div class="footer-social">
+                    <ul class="d-inline-flex flex-wrap">
+                        <li><a href="#" class="d-flex align-items-center justify-content-center"><span><i
+                                        class="fab fa-facebook-f"></i></span></a></li>
+                        <li class="active"><a href="#"
+                                              class="d-flex align-items-center justify-content-center"><span><i
+                                        class="fab fa-instagram"></i></span></a></li>
+                        <li><a href="#" class="d-flex align-items-center justify-content-center"><span><i
+                                        class="fab fa-twitter"></i></span></a></li>
+                        <li><a href="#" class="d-flex align-items-center justify-content-center"><span><i
+                                        class="fab fa-google-plus-g"></i></span></a></li>
+                        <li><a href="#" class="d-flex align-items-center justify-content-center"><span><i
+                                        class="fab fa-youtube"></i></span></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
-    {{--<div class="footer-bottom">--}}
-        {{--<div class="container main-max-width">--}}
-            {{--<div class="d-flex flex-wrap justify-content-between align-items-center">--}}
-                {{--<div class="footer_select-lang d-flex align-items-center">--}}
-                    {{--<label for="langSelect" class="text-tert-clr mb-0">Language:</label>--}}
-                    {{--<select id="langSelect"--}}
-                            {{--class="select-2 select-2--no-search main-select main-select-2arrows products-filter-wrap_select not-selected"--}}
-                            {{--style="width: 95px;">--}}
-                        {{--<option class="selected">English</option>--}}
-                        {{--<option>Arm</option>--}}
-                        {{--<option>Russian</option>--}}
-                    {{--</select>--}}
-                {{--</div>--}}
-                {{--<div class="copyright">--}}
-                    {{--&#9400; Copyright 2019 Kaliony UK--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
+    <div class="footer-bottom">
+        <div class="container main-max-width">
+            <div class="d-flex flex-wrap justify-content-end align-items-center">
+{{--                <div class="footer_select-lang d-flex align-items-center">--}}
+{{--                    <label for="langSelect" class="text-tert-clr mb-0">Language:</label>--}}
+{{--                    <select id="langSelect"--}}
+{{--                            class="select-2 select-2--no-search main-select main-select-2arrows products-filter-wrap_select not-selected"--}}
+{{--                            style="width: 95px;">--}}
+{{--                        <option class="selected">English</option>--}}
+{{--                        <option>Arm</option>--}}
+{{--                        <option>Russian</option>--}}
+{{--                    </select>--}}
+{{--                </div>--}}
+                <div class="copyright">
+                    &#9400; Copyright 2019 Kaliony UK
+                </div>
+            </div>
+        </div>
 
-    {{--</div>--}}
+    </div>
 </footer>
 <div class="dark-bg_body"></div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,17 +117,18 @@
 
                 </div>
             </section>
-            <div class="container home_width">
-                <section class="home_products-wrapper">
-                    <h2 class="font-sec-reg home_main-title text-center"><span class="font-sec-bold">TOP</span>
-                        <span>PRODUCTS</span>
-                    </h2>
-                    <p class="font-main-light font-15 text-center home_title-desc mb-0">Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit. Suspendisse lorem risus, molestie tincidunt lacus nec,
-                        <br> sagittis tincidunt neque. Aenean luctus tempor libero eget ultrices. Curabitur at nibh
-                        orci.
-                    </p>
 
+                <section class="home_products-wrapper">
+                    <div class="container home_width">
+                        <h2 class="font-sec-reg home_main-title text-center"><span class="font-sec-bold">TOP</span>
+                            <span>PRODUCTS</span>
+                        </h2>
+                        <p class="font-main-light font-15 text-center home_title-desc mb-0">Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit. Suspendisse lorem risus, molestie tincidunt lacus nec,
+                            <br> sagittis tincidunt neque. Aenean luctus tempor libero eget ultrices. Curabitur at nibh
+                            orci.
+                        </p>
+                    </div>
                     <div class="d-flex home_products-version">
                         <div class="col active">
                             <a href="#" class="products-version-link">
@@ -161,8 +162,8 @@
                         </div>
 
                     </div>
-                    <ul class="row products__list-wrapper">
-                        <li class="col-lg-3 col-sm-6">
+                    <div class="products__list-wrapper home_products-carousel">
+                        <div class="home_products-carousel-item">
                             <div class="products__item-wrapper main-transition">
                                 <div class="products__item-wrapper-inner">
                                     <a href="#" class="products__item-top">
@@ -244,8 +245,8 @@ adipiscing elit. Morbi sodales ...
                                     </div>
                                 </div>
                             </div>
-                        </li>
-                        <li class="col-lg-3 col-sm-6">
+                        </div>
+                        <div class="home_products-carousel-item">
                             <div class="products__item-wrapper main-transition">
                                 <div class="products__item-wrapper-inner">
                                     <a href="#" class="products__item-top">
@@ -327,8 +328,8 @@ adipiscing elit. Morbi sodales ...
                                     </div>
                                 </div>
                             </div>
-                        </li>
-                        <li class="col-lg-3 col-sm-6">
+                        </div>
+                        <div class="home_products-carousel-item">
                             <div class="products__item-wrapper main-transition">
                                 <div class="products__item-wrapper-inner">
                                     <a href="#" class="products__item-top">
@@ -410,8 +411,8 @@ adipiscing elit. Morbi sodales ...
                                     </div>
                                 </div>
                             </div>
-                        </li>
-                        <li class="col-lg-3 col-sm-6">
+                        </div>
+                        <div class="home_products-carousel-item">
                             <div class="products__item-wrapper main-transition">
                                 <div class="products__item-wrapper-inner">
                                     <a href="#" class="products__item-top">
@@ -493,10 +494,177 @@ adipiscing elit. Morbi sodales ...
                                     </div>
                                 </div>
                             </div>
-                        </li>
-                    </ul>
+                        </div>
+                        <div class="home_products-carousel-item">
+                            <div class="products__item-wrapper main-transition">
+                                <div class="products__item-wrapper-inner">
+                                    <a href="#" class="products__item-top">
+                                    <span
+                                        class="font-sec-reg text-uppercase d-block text-center text-truncate products__item-brand-name font-16 text-sec-clr lh-1">BRAND NAME</span>
+                                        <span class="position-relative products__item-photo d-block">
+                                        <img src="/public/img/temp/product-2-1.png" alt="product">
+{{--<span class="position-absolute font-main-bold font-16 products__item-photo-inner products__item-new">new</span>--}}
+<span class="position-absolute font-main-bold font-16 products__item-photo-inner products__item-less">-50%</span>
+                                    </span>
+                                        <span class="products__item-main-content">
+                                         <span class="products__item-photo-thumb">
+
+<span class="products__item-photo-thumb-item active-slider">
+    <img src="/public/img/temp/product-2-1.png" alt="thumb">
+</span>
+                                             <span class="products__item-photo-thumb-item">
+    <img src="/public/img/temp/product-3-1.png" alt="thumb">
+</span>
+                                             <span class="products__item-photo-thumb-item">
+    <img src="/public/img/temp/product-3-1.png" alt="thumb">
+</span>
+                                                                                          <span
+                                                                                              class="products__item-photo-thumb-item">
+    <img src="/public/img/temp/product-4-1.png" alt="thumb">
+</span>
+
+                                        </span>
+                                        <span class="products__item-content-inner">
+                                            <span
+                                                class="font-sec-reg font-21 text-main-clr products__item-title text-truncate">
+60W SMOK² PRIV V8 Starter1
+                                    </span>
+                                            <span class="font-main-light font-15 products__item-desc">Lorem ipsum dolor amet, consectetur
+adipiscing elit. Morbi sodales ...
+                                    </span>
+                                             <span
+                                                 class="d-flex flex-wrap justify-content-between align-items-center products__item-price-discount">
+
+                                              <span
+                                                  class="d-flex flex-wrap align-items-center products__item-discount-all">
+{{--                                                                               <span class="products__item-discount">--}}
+                                                  {{--    <img src="/public/img/discount-70.png" alt="discount">--}}
+                                                  {{--</span>--}}
+                                                 <span class="products__item-discount">
+                                                      <img src="/public/img/discount-50.png" alt="discount">
+                                                  </span>
+
+                                              </span>
+
+
+                                        <span class="d-flex flex-wrap products__item-prices">
+<span class="font-sec-reg text-gray-clr font-18 align-self-end products__item-sec-price">$200</span>
+<span class="font-sec-bold font-24 text-tert-clr products__item-main-price">$100</span>
+                                        </span>
+                                    </span>
+                                        </span>
+
+                                    </span>
+
+                                    </a>
+                                    <div
+                                        class="flex-wrap justify-content-between align-items-center products__item-bottom">
+                                        <a href="#"
+                                           class="d-flex align-items-center justify-content-center font-15 text-tert-clr text-uppercase products__item-view-more">
+                                            view more
+                                        </a>
+                                        <span class="products__item-favourite active">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                                            width="20px" height="18px"
+                                            viewBox="0 0 20 18"
+                                        >
+                                            <path fill-rule="evenodd" opacity="0.949" fill="rgb(227, 230, 237)"
+                                                  d="M14.700,-0.002 C13.057,-0.002 11.419,0.767 10.360,2.016 C9.300,0.767 7.663,-0.002 6.020,-0.002 C3.036,-0.002 0.720,2.306 0.720,5.281 C0.720,8.936 3.996,11.916 9.009,16.336 L10.360,17.678 L11.711,16.336 C16.723,11.916 19.999,8.936 19.999,5.281 C19.999,2.306 17.684,-0.002 14.700,-0.002 L14.700,-0.002 Z"/>
+                                        </svg>
+                                    </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="home_products-carousel-item">
+                            <div class="products__item-wrapper main-transition">
+                                <div class="products__item-wrapper-inner">
+                                    <a href="#" class="products__item-top">
+                                    <span
+                                        class="font-sec-reg text-uppercase d-block text-center text-truncate products__item-brand-name font-16 text-sec-clr lh-1">BRAND NAME</span>
+                                        <span class="position-relative products__item-photo d-block">
+                                        <img src="/public/img/temp/product-2-1.png" alt="product">
+{{--<span class="position-absolute font-main-bold font-16 products__item-photo-inner products__item-new">new</span>--}}
+<span class="position-absolute font-main-bold font-16 products__item-photo-inner products__item-less">-50%</span>
+                                    </span>
+                                        <span class="products__item-main-content">
+                                         <span class="products__item-photo-thumb">
+
+<span class="products__item-photo-thumb-item active-slider">
+    <img src="/public/img/temp/product-2-1.png" alt="thumb">
+</span>
+                                             <span class="products__item-photo-thumb-item">
+    <img src="/public/img/temp/product-3-1.png" alt="thumb">
+</span>
+                                             <span class="products__item-photo-thumb-item">
+    <img src="/public/img/temp/product-3-1.png" alt="thumb">
+</span>
+                                                                                          <span
+                                                                                              class="products__item-photo-thumb-item">
+    <img src="/public/img/temp/product-4-1.png" alt="thumb">
+</span>
+
+                                        </span>
+                                        <span class="products__item-content-inner">
+                                            <span
+                                                class="font-sec-reg font-21 text-main-clr products__item-title text-truncate">
+60W SMOK² PRIV V8 Starter1
+                                    </span>
+                                            <span class="font-main-light font-15 products__item-desc">Lorem ipsum dolor amet, consectetur
+adipiscing elit. Morbi sodales ...
+                                    </span>
+                                             <span
+                                                 class="d-flex flex-wrap justify-content-between align-items-center products__item-price-discount">
+
+                                              <span
+                                                  class="d-flex flex-wrap align-items-center products__item-discount-all">
+{{--                                                                               <span class="products__item-discount">--}}
+                                                  {{--    <img src="/public/img/discount-70.png" alt="discount">--}}
+                                                  {{--</span>--}}
+                                                 <span class="products__item-discount">
+                                                      <img src="/public/img/discount-50.png" alt="discount">
+                                                  </span>
+
+                                              </span>
+
+
+                                        <span class="d-flex flex-wrap products__item-prices">
+<span class="font-sec-reg text-gray-clr font-18 align-self-end products__item-sec-price">$200</span>
+<span class="font-sec-bold font-24 text-tert-clr products__item-main-price">$100</span>
+                                        </span>
+                                    </span>
+                                        </span>
+
+                                    </span>
+
+                                    </a>
+                                    <div
+                                        class="flex-wrap justify-content-between align-items-center products__item-bottom">
+                                        <a href="#"
+                                           class="d-flex align-items-center justify-content-center font-15 text-tert-clr text-uppercase products__item-view-more">
+                                            view more
+                                        </a>
+                                        <span class="products__item-favourite active">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                                            width="20px" height="18px"
+                                            viewBox="0 0 20 18"
+                                        >
+                                            <path fill-rule="evenodd" opacity="0.949" fill="rgb(227, 230, 237)"
+                                                  d="M14.700,-0.002 C13.057,-0.002 11.419,0.767 10.360,2.016 C9.300,0.767 7.663,-0.002 6.020,-0.002 C3.036,-0.002 0.720,2.306 0.720,5.281 C0.720,8.936 3.996,11.916 9.009,16.336 L10.360,17.678 L11.711,16.336 C16.723,11.916 19.999,8.936 19.999,5.281 C19.999,2.306 17.684,-0.002 14.700,-0.002 L14.700,-0.002 Z"/>
+                                        </svg>
+                                    </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="line-home"></div>
                 </section>
-            </div>
+
             <section class="home_reviews-wrapper">
                 <div class="container home_width">
                     <div class="row">
@@ -653,6 +821,19 @@ adipiscing elit. Morbi sodales ...
                     "980px": 3,
                     "1220px": 4,
                     "1630px": 5,
+                }
+            });
+
+            // home products slider
+
+            $(".home_products-carousel").carousel({
+                theme: "fs-light",
+                pagination: false,
+                infinite: true,
+                show: {
+                    "740px": 2,
+                    "980px": 3,
+                    "1220px": 4,
                 }
             });
             // home reviews slider
