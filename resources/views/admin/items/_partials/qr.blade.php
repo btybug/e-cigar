de7555f9bc217d8f6246ab8b5f49205f70d44421
terakyan <edo.terakyan@gmail.com>
2019-10-05 20:31:42 +0400

    up

--- resources/views/admin/items/_partials/qr.blade.php
+++ resources/views/admin/items/_partials/qr.blade.php
@@ -1,5 +1,5 @@
 <div class="col-md-12">
-    {!! \DNS2D::getBarcodeHTML('http://kaliony.com/landings/'.$code, "QRCODE") !!}
+    {!! \DNS2D::getBarcodeHTML('https://kaliony.com/landings/'.$code, "QRCODE") !!}
 </div>
 <div class="col-md-12">
     <a class="btn btn-success" href="{{ route("admin_items_download_code",[$code]) }}">Download QR code</a>
