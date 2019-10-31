9427c48d3254878524103e62258178b1e6af40b0
David <galstyan.david1993@gmail.com>
2019-10-31 16:47:54 +0400

    up

--- public/js/media_button_new.js
+++ public/js/media_button_new.js
@@ -1428,18 +1428,18 @@ var count = 0;
 
           } else if($(e.target).closest('.file').data('type') === 'pdf') {
             console.log('helloooooooooo', $(e.target).closest('.file').find('img').attr('src'));
-            $("body").append(`<iframe src="https://docs.google.com/gview?url=${location.origin}${$(e.target).closest('.file').find('img').attr('src')}&amp;embedded=true" style="width:100%; height:100vh" frameborder="0"></iframe>`)
-              e.target.closest(".file-box").classList.remove("active");
-              const countId = e.target
-                  .closest(".file-box")
-                  .getAttribute("data-image");
-              this.requests.getImageDetails({item_id: id}, res => {
-                  $('#modal_area').html(this.htmlMaker.fullInfoModal(
-                      res,
-                      Number(countId)
-                  ));
-                  return $('body').append(html);
-              });
+            $(".folderitems").append(`<iframe src="https://docs.google.com/gview?url=${location.origin}${$(e.target).closest('.file').find('img').attr('src')}&amp;embedded=true" style="width:100%; height:100vh" frameborder="0"></iframe>`)
+              // e.target.closest(".file-box").classList.remove("active");
+              // const countId = e.target
+              //     .closest(".file-box")
+              //     .getAttribute("data-image");
+              // this.requests.getImageDetails({item_id: id}, res => {
+              //     $('#modal_area').html(this.htmlMaker.fullInfoModal(
+              //         res,
+              //         Number(countId)
+              //     ));
+              //     return $('body').append(html);
+              // });
           }else {
               e.target.closest(".file-box").classList.remove("active");
               const countId = e.target
