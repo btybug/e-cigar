4a88d6a7abc25be7061ff150b1cd5ae07f5cf973
sahakavatar <hakobyan.sahak88@gmail.com>
2019-11-04 14:56:53 +0400

    fix

--- resources/views/admin/emails_notifications/send/email_create.blade.php
+++ resources/views/admin/emails_notifications/send/email_create.blade.php
@@ -17,7 +17,7 @@
 
         <div class="card-body panel-body">
             <div class="row">
-                <div class="tab-content tabs_content col-md-8">
+                <div class="tab-content tabs_content col-xl-8 col-md-6 col-sm-5 pr-sm-0 pr-3">
 
                     <div id="home" class="tab-pane tab_info fade in active show">
 
@@ -32,18 +32,18 @@
                                         <a class="nav-link" id="tab2-tab" data-toggle="tab" href="#tab2">To Admin</a>
                                     </li>
                                 </ul>
-                                <div class="tab-content tab-content-store-settings">
+                                <div class="tab-content tab-content-store-settings pr-3">
                                     <div class="tab-pane fade active in show" id="tab1"
                                          aria-labelledby="tab1-tab">
                                         <div class="form-group row">
-                                            {{Form::label('from', 'From',['class' => 'col-sm-3'])}}
-                                            <div class="col-sm-9">
+                                            {{Form::label('from', 'From',['class' => 'col-xl-3'])}}
+                                            <div class="col-xl-9">
                                                 {{Form::select('from',$froms,null,['class' =>'form-control','id'=>'from'])}}
                                             </div>
                                         </div>
                                         <div class="form-group row">
-                                            {{Form::label('to', 'To',['class' => 'col-sm-3'])}}
-                                            <div class="col-sm-9">
+                                            {{Form::label('to', 'To',['class' => 'col-xl-3'])}}
+                                            <div class="col-xl-9">
                                                 <div class="to_select">
                                                     {!! Form::select('users[]',$users,null,['class' => 'form-control tag-input-v select-user','multiple'=>'multiple']) !!}
                                                 </div>
@@ -54,15 +54,15 @@
                                             </div>
                                         </div>
                                         <div class="form-group row">
-                                            {{Form::label('to_groups', 'To Groups',['class' => 'col-sm-3'])}}
-                                            <div class="col-sm-9">
+                                            {{Form::label('to_groups', 'To Groups',['class' => 'col-xl-3'])}}
+                                            <div class="col-xl-9">
                                                 {!! Form::select('groups[]',$campaings,null,['id' => 'to_groups','class' => 'form-control tag-input-v','multiple'=>'multiple']) !!}
                                             </div>
                                         </div>
 
                                         <div class="form-group row voucher-box hide">
                                             {{Form::label('voucher', 'Voucher',['class' => 'col-sm-3'])}}
-                                            <div class="col-sm-9">
+                                            <div class="col-xl-9">
                                                 {!! Form::select('coupon_id',[''=>'Select'] + $coupons,null,['id' => 'voucher','class' => 'form-control tag-input-v']) !!}
                                             </div>
                                         </div>
@@ -85,14 +85,14 @@
                                                         <div id="{{ strtolower($language->code) }}"
                                                              class="tab-pane fade  @if($loop->first) in active show @endif">
                                                             <div class="form-group row">
-                                                                {{Form::label('subject_'.strtolower($language->code), 'Subject',['class' => 'col-sm-3'])}}
-                                                                <div class="col-sm-9">
+                                                                {{Form::label('subject_'.strtolower($language->code), 'Subject',['class' => 'col-xl-3'])}}
+                                                                <div class="col-xl-9">
                                                                     {{Form::text('translatable['.strtolower($language->code).'][subject]',get_translated($model,strtolower($language->code),'subject'),['class' =>'form-control','id'=>'subject_am','placeholder' => __('Subject')])}}
                                                                 </div>
                                                             </div>
                                                             <div class="form-group row">
-                                                                {{Form::label('content_'.strtolower($language->code), 'Content',['class' => 'col-sm-3'])}}
-                                                                <div class="col-sm-9">
+                                                                {{Form::label('content_'.strtolower($language->code), 'Content',['class' => 'col-xl-3'])}}
+                                                                <div class="col-xl-9">
                                                                     {{Form::textarea('translatable['.strtolower($language->code).'][content]',get_translated($model,strtolower($language->code),'content') ,['class' =>'form-control content_editor','cols'=>30,'rows'=>2,'placeholder' => __('Content')])}}
                                                                 </div>
                                                             </div>
@@ -104,14 +104,14 @@
                                     </div>
                                     <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
                                         <div class="form-group row">
-                                            {{Form::label('admin_from', 'From',['class' => 'col-sm-3'])}}
-                                            <div class="col-sm-9">
+                                            {{Form::label('admin_from', 'From',['class' => 'col-xl-3'])}}
+                                            <div class="col-xl-9">
                                                 {{Form::select('admin[from]',$froms,null,['class' =>'form-control','id'=>'admin_from'])}}
                                             </div>
                                         </div>
                                         <div class="form-group row">
-                                            {{Form::label('to_admin', 'To',['class' => 'col-sm-3'])}}
-                                            <div class="col-sm-9">
+                                            {{Form::label('to_admin', 'To',['class' => 'col-xl-3'])}}
+                                            <div class="col-xl-9">
                                                 {{Form::select('admin[to]',$users,null,['class' =>'form-control','id'=>'to_admin'])}}
                                             </div>
                                         </div>
@@ -134,14 +134,14 @@
                                                         <div id="{{ strtolower($language->code) }}"
                                                              class="tab-pane fade  @if($loop->first) in active show @endif">
                                                             <div class="form-group row">
-                                                                {{Form::label('subject_'.strtolower($language->code), 'Subject',['class' => 'col-sm-3'])}}
-                                                                <div class="col-sm-9">
+                                                                {{Form::label('subject_'.strtolower($language->code), 'Subject',['class' => 'col-xl-3'])}}
+                                                                <div class="col-xl-9">
                                                                     {{Form::text('admin[translatable]['.strtolower($language->code).'][subject]',null ,['class' =>'form-control','id'=>'admin_subject_am','placeholder' => __('Subject')])}}
                                                                 </div>
                                                             </div>
                                                             <div class="form-group row">
-                                                                {{Form::label('content_'.strtolower($language->code), 'Content',['class' => 'col-sm-3'])}}
-                                                                <div class="col-sm-9">
+                                                                {{Form::label('content_'.strtolower($language->code), 'Content',['class' => 'col-xl-3'])}}
+                                                                <div class="col-xl-9">
                                                                     {{Form::textarea('admin[translatable]['.strtolower($language->code).'][content]',null ,['class' =>'form-control content_editor','cols'=>30,'rows'=>2,'placeholder' => __('Content')])}}
                                                                 </div>
                                                             </div>
@@ -156,28 +156,30 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <table class="table table-striped table--email-temp">
-                        <thead>
-                        <tr class="table--email-temp_top">
-                            <th colspan="3">Common Shortcodes</th>
-                        </tr>
-                        <tr class="table--email-temp_bottom">
-                            <th></th>
-                            <th>Property</th>
-                            <th>Description</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($shortcodes->mailShortcodes as $shortcode)
-                            <tr>
-                                <td><i class="fa fa-file-text-o table--email-temp_icon" aria-hidden="true"></i></td>
-                                <td><b>{!! '['.$shortcode['code'].']' !!}</b></td>
-                                <td>{!! $shortcode['description'] !!}</td>
+                <div class="col-xl-4 col-md-6 col-sm-7">
+                    <div class="table-responsive">
+                        <table class="table table-striped table--email-temp">
+                            <thead>
+                            <tr class="table--email-temp_top">
+                                <th colspan="3">Common Shortcodes</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            <tr class="table--email-temp_bottom">
+                                <th></th>
+                                <th>Property</th>
+                                <th>Description</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($shortcodes->mailShortcodes as $shortcode)
+                                <tr>
+                                    <td><i class="fa fa-file-text-o table--email-temp_icon" aria-hidden="true"></i></td>
+                                    <td><b>{!! '['.$shortcode['code'].']' !!}</b></td>
+                                    <td>{!! $shortcode['description'] !!}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                     <div class="form-group row ">
                         <label for="email_type" class="col-sm-3">Type</label>
                         <div class="col-sm-9">
