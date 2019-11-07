f6b146ca23cbf6e58306825e050a84155b012d55
terakyan <edo.terakyan@gmail.com>
2019-11-07 16:49:21 +0400

    up

--- resources/views/filters/filter_modal.blade.php
+++ resources/views/filters/filter_modal.blade.php
@@ -10,59 +10,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="wizardAll-tab" data-toggle="tab" href="#wizardAll" role="tab" aria-controls="wizardAll" aria-selected="true">All</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="wizardItem1-tab" data-toggle="tab" href="#wizardItem1" role="tab" aria-controls="wizardItem1" aria-selected="false">Item 1</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" id="wizardItem2-tab" data-toggle="tab" href="#wizardItem2" role="tab" aria-controls="wizardItem2" aria-selected="false">Item 2</a>
-                    </li>
-                </ul>
-                <div class="tab-content main-scrollbar" id="myTabContent">
-                    <div class="tab-pane fade show active" id="wizardAll" role="tabpanel" aria-labelledby="wizardAll-tab">
-                        <div class="shopping-cart_wrapper p-0">
-                            <div class="content-wrap shoping-card">
-                                <ul class="row content">
-                                    <li class="col-md-3" data-id="1">
-                                        <div class="item-content">
-                                            <div class="item-photo">
-                                                <img src="https://www.enca.com/sites/default/files/2019-09/Vaping.jpg" alt="photo">
-                                            </div>
-                                            <div class="item-title">
-                                                <span>Nicotine salt 20mg</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="col-md-3" data-id="2">
-                                        <div class="item-content">
-                                            <div class="item-photo">
-                                                <img src="https://www.enca.com/sites/default/files/2019-09/Vaping.jpg" alt="photo">
-                                            </div>
-                                            <div class="item-title">
-                                                <span>Throat hit 12mg</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="col-md-3" data-id="3">
-                                        <div class="item-content">
-                                            <div class="item-photo">
-                                                <img src="https://www.enca.com/sites/default/files/2019-09/Vaping.jpg" alt="photo">
-                                            </div>
-                                            <div class="item-title">
-                                                <span>Throat hit 18mg</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="wizardItem1" role="tabpanel" aria-labelledby="wizardItem1-tab">Item1</div>
-                    <div class="tab-pane fade" id="wizardItem2" role="tabpanel" aria-labelledby="wizardItem2-tab">Item2</div>
-                </div>
+              
             </div>
             <div class="modal-footer bord-top d-flex justify-content-between popup-modal-footer py-0">
                 <div class="d-flex selected-items_popup w-100 main-scrollbar">
