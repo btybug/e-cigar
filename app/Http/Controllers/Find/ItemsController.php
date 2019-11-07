5318c9b65d4b051f6d712a5f774e2cfa142172d5
sahakavatar <hakobyan.sahak88@gmail.com>
2019-11-07 18:17:21 +0400

    up

--- app/Http/Controllers/Find/ItemsController.php
+++ app/Http/Controllers/Find/ItemsController.php
@@ -32,7 +32,7 @@ class ItemsController extends Controller
         $barcodes=Items::leftJoin('barcodes','items.barcode_id','barcodes.id')
             ->leftJoin('item_translations', 'items.id', '=', 'item_translations.items_id')
             ->where('item_translations.locale',app()->getLocale())
-            ->select('barcodes.code as value','item_translations.name as fail_name')
+            ->select('barcodes.code as value','item_translations.name as file_name')
             ->whereIn('items.id',$request->get('ids'))->get();
         return response()->json(['barcodes'=>$barcodes]);
     }
