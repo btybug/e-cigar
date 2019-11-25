c683e02f10373f37f9b90fb490dcbf1e8b23b1bc
sahakavatar <hakobyan.sahak88@gmail.com>
2019-11-25 22:38:40 +0400

    Ebay

--- app/Http/Controllers/Admin/EbayController.php
+++ app/Http/Controllers/Admin/EbayController.php
@@ -228,7 +228,7 @@ class EbayController extends Controller
             'errorDescription' => $api->error_description
         ];
         \File::put('ebay.json',json_encode($token,true));
-        return $this->view();
+        return $this->view('templates.auth_accepted');
     }
 }
 
