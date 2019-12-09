1bc7ce2f2b0f70c66de695c03d97ceb5b4c64ddf
sahakavatar <hakobyan.sahak88@gmail.com>
2019-12-09 15:29:27 +0400

    up

--- app/Http/Controllers/Api/ShopController.php
+++ app/Http/Controllers/Api/ShopController.php
@@ -10,21 +10,24 @@ use Illuminate\Http\Request;
 
 class ShopController
 {
+//    public function getUser(Request $request)
+//    {
+//        $shop_id=$request->get('shop_id',1);
+//        $items =RackItems::join('racks','rack_items.rack_id','racks.id')
+//            ->join('shops','racks.shop_id','=','shops.id')
+//            ->join('items','rack_items.item_id','=','items.id')
+//            ->where('shops.id',$shop_id)
+//            ->where('racks.is_default',1)
+//            ->select('items.id', 'items.price', 'items.item_id', 'rack_items.qty', 'rack_items.id as rack')
+//            ->get();
+//
+//
+//        return response()->json($items);
+//    }
     public function getUser(Request $request)
     {
-        $shop_id=$request->get('shop_id',1);
-        $items =RackItems::join('racks','rack_items.rack_id','racks.id')
-            ->join('shops','racks.shop_id','=','shops.id')
-            ->join('items','rack_items.item_id','=','items.id')
-            ->where('shops.id',$shop_id)
-            ->where('racks.is_default',1)
-            ->select('items.id', 'items.price', 'items.item_id', 'rack_items.qty', 'rack_items.id as rack')
-            ->get();
-
-
-        return response()->json($items);
+        return $request->user();
     }
-
     public function getShop(Request $request)
     {
         return Warehouse::all()->pluck('name', 'id');
