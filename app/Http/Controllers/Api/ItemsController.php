0344de0978a64081f80b561cf60b4165d933e203
sahakavatar <hakobyan.sahak88@gmail.com>
2019-12-10 17:13:45 +0400

    up

--- app/Http/Controllers/Api/ItemsController.php
+++ app/Http/Controllers/Api/ItemsController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Category;
 use App\Models\Items;
+use App\Models\Warehouse;
 use Illuminate\Http\Request;
 
 class ItemsController
@@ -13,10 +14,13 @@ class ItemsController
     public function getItems(Request $request)
     {
         $w_id = $request->get('shop_id');
+        $warehouse=Warehouse::findOrFail($w_id);
+        $rack=$warehouse->dafault_rack();
         $items = Items::leftJoin('item_translations', 'items.id', '=', 'item_translations.items_id')
             ->leftJoin("item_locations", "items.id", "=", "item_locations.item_id")
             ->with('categories')
             ->where("item_locations.warehouse_id", "=", $w_id)
+            ->where("item_locations.rack_id", "=", $rack->id)
             ->groupBy('items.id')
             ->get();
 
