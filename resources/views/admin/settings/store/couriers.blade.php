cbd2f2edbebd0c32939596dd72d5c2d7009fc9b3
terakyan <edo.terakyan@gmail.com>
2019-12-12 21:44:19 +0400

    up

--- resources/views/admin/settings/store/couriers.blade.php
+++ resources/views/admin/settings/store/couriers.blade.php
@@ -14,7 +14,7 @@
             @ok('admin_settings_shipping')
             <li class="nav-item ">
                 <a class="nav-link" id="shipping-tab" href="{!! route('admin_settings_shipping') !!}" role="tab"
-                   aria-controls="shipping" aria-selected="false">Shipping</a>
+                   aria-controls="shipping" aria-selected="false">GEO zones</a>
             </li>
             @endok
             @ok('admin_settings_payment_gateways')
