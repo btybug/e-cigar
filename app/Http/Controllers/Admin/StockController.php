ed57b1acc91866542dd0b2cf5ef5e82eeecfd93e
sahakavatar <hakobyan.sahak88@gmail.com>
2020-01-08 22:09:44 +0400

    stock url

--- app/Http/Controllers/Admin/StockController.php
+++ app/Http/Controllers/Admin/StockController.php
@@ -44,7 +44,7 @@ class StockController extends Controller
     {
         $translations = StockTranslation::all();
         foreach ($translations as $translation){
-            $translation->slug=strtolower(preg_replace('!\s+!','-',$translation->slug));
+            $translation->slug=strtolower(str_replace('-',' ',$translation->slug));
             $translation->save();
         }
         dd('done');
