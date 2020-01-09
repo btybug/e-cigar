a466a6c2d87a3ab6273ddd821354c3f7cb8135f9
sahakavatar <hakobyan.sahak88@gmail.com>
2020-01-09 15:56:52 +0400

    up

--- resources/views/admin/users/staff/edit.blade.php
+++ resources/views/admin/users/staff/edit.blade.php
@@ -189,7 +189,7 @@
                                                 <div class="row justify-content-center">
                                                     <div class="col-md-8">
                                                         <div class="card">
-                                                            <div class="card-header">Laravel - Change Password with Current Password Validation Example - ItSolutionStuff.com</div>
+                                                            <div class="card-header">Change Password with Current Password Validation</div>
 
                                                             <div class="card-body">
                                                                 <form method="POST" action="{{ route('change.password') }}">
