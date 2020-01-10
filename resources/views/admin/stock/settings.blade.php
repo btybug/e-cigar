e347c2c7e9b118d93778b5fd44a255a043197b4d
sahakavatar <hakobyan.sahak88@gmail.com>
2020-01-10 15:05:12 +0400

    up

--- resources/views/admin/stock/settings.blade.php
+++ resources/views/admin/stock/settings.blade.php
@@ -35,7 +35,6 @@
                             </div>
                             <div class="card-body panel-body">
                                 <div class="payment_gateways_tab w-100">
-                                    <h2>Categories</h2>
                                     <ul class="list_paymant">
                                         <li class="item">
                                             <div class="chek-title">
@@ -49,13 +48,6 @@
                                                 <label for="stripe_paymant"  class="title font-weight-bold">Offers</label>
                                             </div>
                                             @ok('admin_stock_categories')<a href="{!! route('admin_stock_categories','offers') !!} " class="btn btn-sm btn-warning text-white"><i class="fa fa-edit"></i></a>@endok
-                                        </li>
-
-                                        <li class="item">
-                                            <div class="chek-title">
-                                                <label for="stripe_paymant"  class="title font-weight-bold">Special Filters</label>
-                                            </div>
-                                            @ok('admin_stock_categories')<a href="{!! route('admin_stock_categories','special_filter') !!} " class="btn btn-sm btn-warning text-white"><i class="fa fa-edit"></i></a>@endok
                                         </li>
 
                                         <li class="item">
