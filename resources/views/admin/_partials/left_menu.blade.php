d9aa4c31548f1dddabd689c76c63a457d685f4c0
sahakavatar <hakobyan.sahak88@gmail.com>
2020-01-10 16:37:08 +0400

    up

--- resources/views/admin/_partials/left_menu.blade.php
+++ resources/views/admin/_partials/left_menu.blade.php
@@ -175,24 +175,6 @@
                 </ul>
             </li>
             @endHasAccess
-
-            @hasAccess('tools')
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-wrench"></i>
-                    <span>Tools</span>
-                    <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-                </a>
-                <ul class="treeview-menu">
-
-                    <li><a href="{{route('admin_tools_filters')}}"><i class="fa fa-circle-o"></i>Filters</a></li>
-
-                </ul>
-            </li>
-            @endHasAccess
-
             @hasAccess('emails_notifications')
             <li class="treeview">
                 <a href="#">
