54fc5e68a11ab484e8bf27a94cef0308d89b9f39
terakyan <edo.terakyan@gmail.com>
2020-01-17 09:28:46 +0400

    up

--- resources/views/frontend/products/_partials/stock_variation_option.blade.php
+++ resources/views/frontend/products/_partials/stock_variation_option.blade.php
@@ -149,7 +149,7 @@
                             $to = $selected->discounts()->orderBy('to','asc')->first();
                         @endphp
                         {!! Form::number('qty',1,['class' => 'product-qty product-qty_per_price input-qty',
-                            'data-id' => $selected->id,'min' => 1,'max'=> ($to)?$to->to:'','step' => 1]) !!}
+                            'data-id' => $selected->id,'min' => 1,'max'=> ($to)?$to->to:'','step' => 1,'readonly' => true]) !!}
                         <div class="inp-icons">
                             <span class="inp-up"></span>
                             <span class="inp-down"></span>
