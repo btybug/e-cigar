016ef37a73938ed6b7aedf97d5eef6dafdc31558
terakyan <edo.terakyan@gmail.com>
2020-01-20 09:37:04 +0400

    up

--- database/migrations/2018_11_26_062705_create_items_table.php
+++ database/migrations/2018_11_26_062705_create_items_table.php
@@ -21,8 +21,7 @@ class CreateItemsTable extends Migration
             $table->string('alert',191)->nullable();
             $table->integer('quantity')->default(0);
             $table->text('image');
-
-            $table->integer('default_price')->default(0);
+            $table->decimal('default_price',6,2)->default(0);
             $table->tinyInteger('status')->default(0);
             $table->decimal('length')->nullable();
             $table->decimal('width')->nullable();
