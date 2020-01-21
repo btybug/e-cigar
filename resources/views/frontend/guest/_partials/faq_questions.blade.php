933c8d3b179e17ca87167309f0c26809f07e1372
sahakavatar <hakobyan.sahak88@gmail.com>
2020-01-21 14:19:14 +0400

    fix

--- resources/views/frontend/guest/_partials/faq_questions.blade.php
+++ resources/views/frontend/guest/_partials/faq_questions.blade.php
@@ -1,14 +1,18 @@
 @foreach($category->faqs as $faq)
-    <div class="card card-accordion"><a class="card-header collapsed" href="#"
+    <div class="card card-accordion faq-page-card-wrapper">
+        <div class="card-header d-flex flex-wrap justify-content-between"
                                         data-toggle="collapse"
                                         data-target="#collapseOne-{{ $faq->id }}-content"
                                         id="collapseOne-{{ $faq->id }}-header" aria-expanded="false"
                                         aria-controls="collapseOne-{{ $faq->id }}-content">
-            {{ $faq->question }}</a>
-        <div class="collapse" id="collapseOne-{{ $faq->id }}-content"
-             aria-labelledby="collapseOne-{{ $faq->id }}-header" data-parent="#accordion-2" style="">
-            <div class="card-body">{!! $faq->answer !!}
-            </div>
+
+            <span>{{ $faq->question }}</span>
+            <a href="#"><i class="fas fa-long-arrow-alt-right"></i></a>
         </div>
+{{--        <div class="collapse" id="collapseOne-{{ $faq->id }}-content"--}}
+{{--             aria-labelledby="collapseOne-{{ $faq->id }}-header" data-parent="#accordion-2" style="">--}}
+{{--            <div class="card-body">{!! $faq->answer !!}--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
 @endforeach
