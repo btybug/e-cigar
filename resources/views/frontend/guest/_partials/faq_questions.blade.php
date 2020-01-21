ae18108a59c85b0bcd7907a01a81df14e3bb8c12
sahakavatar <hakobyan.sahak88@gmail.com>
2020-01-21 14:44:14 +0400

    fix

--- resources/views/frontend/guest/_partials/faq_questions.blade.php
+++ resources/views/frontend/guest/_partials/faq_questions.blade.php
@@ -1,14 +1,14 @@
 @foreach($category->faqs as $faq)
     <div class="card card-accordion faq-page-card-wrapper">
-        <div class="card-header d-flex flex-wrap justify-content-between"
+        <a href="{!! route('faq_page_single',$faq->slug) !!}" class="card-header d-flex flex-wrap justify-content-between"
                                         data-toggle="collapse"
                                         data-target="#collapseOne-{{ $faq->id }}-content"
                                         id="collapseOne-{{ $faq->id }}-header" aria-expanded="false"
                                         aria-controls="collapseOne-{{ $faq->id }}-content">
 
             <span>{{ $faq->question }}</span>
-            <a href="{!! route('faq_page_single',$faq->slug) !!}"><i class="fas fa-long-arrow-alt-right"></i></a>
-        </div>
+            <span><i class="fas fa-long-arrow-alt-right"></i></span>
+        </a>
 {{--        <div class="collapse" id="collapseOne-{{ $faq->id }}-content"--}}
 {{--             aria-labelledby="collapseOne-{{ $faq->id }}-header" data-parent="#accordion-2" style="">--}}
 {{--            <div class="card-body">{!! $faq->answer !!}--}}
