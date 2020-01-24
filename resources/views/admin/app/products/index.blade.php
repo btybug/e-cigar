a52ad9fd6b62b5eb9c23daec50145128a8641a84
David <galstyan.david1993@gmail.com>
2020-01-24 13:57:46 +0400

    modal

--- resources/views/admin/app/products/index.blade.php
+++ resources/views/admin/app/products/index.blade.php
@@ -10,4 +10,51 @@
 
         </ul>
     </div>
+    <button type="button" class="btn btn-info select-products">
+        Select
+    </button>
+    <ul class="get-all-products-tab stickers--all--lists">
+        
+    </ul>
+    <div class="modal fade select-products__modal" id="productsModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-md" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Select Products</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                            <select class="form-control search_option_js">
+                                <option value="general" selected>General</option>
+                                <option value="brand">Brands</option>
+                                <option value="category">Categories</option>
+                            </select>
+                        </div>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control search-attr" id="search-product" placeholder="Search">
+                            <select class="form-control d-none" id="brand_select">
+
+                            </select>
+                            <select class="form-control d-none" id="category_select">
+
+                            </select>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-start align-items-center mb-2">
+                        <input type="checkbox" class="all_select_products_js" style="margin: 0 18.240px"/>
+                        <p class="mb-0">Select All</p>
+                    </div>
+                    <ul class="all-list modal-stickers--list" id="stickers-modal-list">
+
+                    </ul>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary done_select_product_js" data-dismiss="modal">Add</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 @stop
