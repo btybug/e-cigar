fecfa6978af4bc1cff5c85d8673e6f7e0da674cd
terakyan <edo.terakyan@gmail.com>
2020-01-29 12:03:06 +0400

    up

--- resources/views/frontend/_partials/top_products.blade.php
+++ resources/views/frontend/_partials/top_products.blade.php
@@ -3,10 +3,13 @@
 @endphp
 <div class="products__list-wrapper home_products-carousel">
 @foreach($products as $product)
+        @php
+            $category = $product->categories()->whereNull('categories.parent_id')->first();
+        @endphp
     <div class="home_products-carousel-item">
         <div class="products__item-wrapper main-transition">
             <div class="products__item-wrapper-inner">
-    <a href="{{ route('product_single', ['type' =>"vape", 'slug' => $product->slug]) }}" class="products__item-top">
+    <a href="{{ route('product_single', ['type' =>($category)?$category->slug:'vape', 'slug' => $product->slug]) }}" class="products__item-top">
             <span class="d-block products__item-photo-brand-name">
                 <span class="font-sec-reg text-uppercase d-block text-center text-truncate products__item-brand-name font-16 text-sec-clr lh-1">
                     @if($product->brand)
@@ -26,17 +29,19 @@
                  <span class="products__item-photo-thumb-item active-slider" title="{{ $product->name }}">
                     <img src="{{ (media_image_tmb($product->image)) }}" alt="{{ $product->name }}" title="{{ $product->name }}">
                  </span>
-                @if($product->variations)
+                @if($product->other_images && count($product->other_images))
                     @php $count = 0; @endphp
-                    @foreach($product->variations()->take(3)->get() as $variation)
-                        @if($variation->image)
-                            @php $count++; @endphp
-                            <span class="products__item-photo-thumb-item" title="{{ $variation->name }}">
-                                <img src="{{ (media_image_tmb($variation->image)) }}" alt="{{ $variation->name }}" title="{{ $variation->name }}">
-                            </span>
+                    @foreach($product->other_images as $other_image)
+                        @if($count == 3)
+                            @break
                         @endif
+                        @php $count++; @endphp
+                        <span class="products__item-photo-thumb-item" title="Extra Image {{ $count }}">
+                                        <img src="{{ (media_image_tmb(checkImage($other_image['image'],'stock'))) }}" alt="{{ $other_image['alt'] }}" title="{{ $other_image['alt'] }}">
+                                    </span>
                     @endforeach
                 @endif
+
             </span>
             <span class="products__item-content-inner">
                 <span class="font-sec-reg font-21 text-main-clr products__item-title text-truncate">
@@ -47,36 +52,58 @@
                 </span>
                 <span class="d-flex flex-wrap justify-content-between align-items-center products__item-price-discount">
                     <span class="d-flex flex-wrap align-items-center products__item-discount-all">
-                        @if(count($product->stickers))
-                            @foreach($product->stickers()->take(2)->get() as $sticker)
-                                <span class="products__item-discount">
-                                     <img src="{{ $sticker->image }}" alt="{{ $sticker->name }}"  title="{{ $sticker->name }}">
-                                 </span>
+                         @if(count($product->stickers))
+                            @foreach($product->stickers()->orderBy('ordering')->take(2)->get() as $sticker)
+                                <span class="products__item-discount" title="{{ $sticker->name }}">
+                                             <img src="{{ $sticker->image }}" alt="{{ $sticker->name }}">
+                                         </span>
                             @endforeach
                         @endif
                     </span>
                     <span class="d-flex flex-wrap products__item-prices">
                         @if($product->new_price)
                             <span class="font-sec-reg text-gray-clr font-18 align-self-end products__item-sec-price">
-                                 {{ convert_price($product->new_price,$currency, false) }}
-                            </span>
+                                         {{ convert_price($product->new_price,$currency, false) }}
+                                    </span>
                             <span class="font-sec-bold font-24 text-tert-clr products__item-main-price">
-                                {{ convert_price($product->price,$currency, false) }}
-                            </span>
+                                        {{ convert_price($product->price,$currency, false) }}
+                                    </span>
                         @else
-                            @if($product->price)
-                                <span class="font-sec-bold font-24 text-tert-clr products__item-main-price">
-                                    {{ convert_price($product->price,$currency, false) }}
-                                </span>
-                            @else
-                                @php
-                                    $firstVariation = ($product->variations && count($product->variations))?$product->variations()->first():null;
-                                @endphp
-                                <span class="font-sec-bold font-24 text-tert-clr products__item-main-price">
-                                    {{ convert_price(($firstVariation)?$firstVariation->common_price:0,$currency, false) }}
-                                </span>
-                            @endif
+                            @php
+                                $firstVariation = ($product->variations && count($product->variations))?$product->variations()->orderBy('ordering','asc')->first():null;
+                            @endphp
+                            <span class="font-sec-bold font-24 text-tert-clr products__item-main-price">
+                                        @php
+                                            $price = 0;
+                                        @endphp
+                                @if($firstVariation)
+                                    @if($firstVariation->price_per == 'product')
+                                        @if($firstVariation->common_price)
+                                            @php $price = $firstVariation->common_price ;@endphp
+                                        @endif
+                                    @else
+                                        @if($firstVariation->price_type == 'dynamic')
+                                            @php $price = $firstVariation->item->default_price; @endphp
+                                        @elseif($firstVariation->price_type == 'fixed')
+                                            @php
+                                                $discount = $firstVariation->discounts()->orderBy('qty','asc')->first();
+                                                $price = ($discount)?$discount->price:0;
+                                            @endphp
+                                        @elseif($firstVariation->price_type == 'range')
+                                            @php
+                                                $discount = $firstVariation->discounts()->orderBy('from','asc')->first();
+                                                $price = ($discount)?$discount->price:0;
+                                            @endphp
+                                        @else
+                                            @if($firstVariation->price)
+                                                @php  $price = $firstVariation->price; @endphp
+                                            @endif
+                                        @endif
+                                    @endif
+                                @endif
 
+                                {{ convert_price($price,$currency, false) }}
+                                    </span>
                         @endif
                     </span>
                 </span>
@@ -84,7 +111,7 @@
         </span>
     </a>
     <div  class="flex-wrap justify-content-between align-items-center products__item-bottom">
-        <a href="{{ route('product_single', ['type' =>"vape", 'slug' => $product->slug]) }}"
+        <a href="{{ route('product_single', ['type' =>($category)?$category->slug:'vape', 'slug' => $product->slug]) }}"
            class="d-flex align-items-center justify-content-center font-15 text-tert-clr text-uppercase products__item-view-more">
             {!! __('view_more') !!}
         </a>
