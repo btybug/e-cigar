b8a37dd34ec382822bba215de5310f4aa6897243
sahakavatar <hakobyan.sahak88@gmail.com>
2020-01-31 14:45:45 +0400

    app

--- app/Http/Controllers/Api/ItemsController.php
+++ app/Http/Controllers/Api/ItemsController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Models\App\AppItems;
 use App\Models\Category;
 use App\Models\Items;
 use App\Models\Warehouse;
@@ -16,13 +17,27 @@ class ItemsController
         $w_id = $request->get('shop_id');
         $warehouse=Warehouse::findOrFail($w_id);
         $rack=$warehouse->default_rack();
-        $items = Items::leftJoin('item_translations', 'items.id', '=', 'item_translations.items_id')
-            ->leftJoin("item_locations", "items.id", "=", "item_locations.item_id")
-            ->with('categories')
-            ->where("item_locations.warehouse_id", "=", $w_id)
-            ->where("item_locations.rack_id", "=", $rack->id)
+        $items = AppItems::join('items', 'app_items.item_id', '=', 'items.id')
+            ->leftJoin('item_translations', 'items.id', '=', 'item_translations.items_id')
+            ->leftJoin('categories', 'items.brand_id', '=', 'categories.id')
+            ->leftJoin('categories_translations', 'categories.id', '=', 'categories_translations.category_id')
+            ->select(
+                'items.barcode_id',
+                'app_items.id',
+                'items.brand_id',
+                'app_items.status',
+                'app_items.price',
+                'app_items.created_at',
+                'app_items.item_id',
+                'item_translations.name',
+                'item_translations.short_description',
+                'barcodes.code',
+                'categories_translations.name as category'
+            )
             ->groupBy('items.id')
-            ->get();
+            ->where('app_items.warehouse_id', $w_id)
+            ->where('items.is_archive', false)->with('item')
+            ->where('item_translations.locale', \Lang::getLocale())->get();
 
         return response()->json($items);
     }
