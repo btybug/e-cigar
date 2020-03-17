5819e62c35ba8e764840e44c534a9174cfbfa9aa
Terakyan <edo.terakyan@gmail.com>
2020-03-17 23:02:07 +0400

    up

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -61,7 +61,7 @@ class CartService
 
     public static function getTotalPriceSum($session = false)
     {
-        $data = ($session) ? Cart::session(Orders::ORDER_NEW_SESSION_ID)->getContent() :Cart::getContent() ;
+        $data = ($session) ? Cart::session(Orders::ORDER_NEW_SESSION_ID)->getContent() : Cart::getContent();
         $price = 0;
         foreach ($data as $cart) {
             $itemPrice = 0;
@@ -79,7 +79,7 @@ class CartService
         $data = Cart::getConditionsByType('coupon');
         $price = 0;
         foreach ($data as $cart) {
-            dd($cart->getValue(),$cart);
+            dd($cart->getValue(), $cart);
 //            if ($cart->getTarget() == 'total') {
 //                $itemPrice = $cart->attributes['extra']['price'];
 //            }
@@ -254,24 +254,24 @@ class CartService
     {
         $error = false;
         $mainVariation = null;
-        if(count($vdata) == 1){
+        if (count($vdata) == 1) {
             $fData = array_first($vdata);
-            $mainVariation = $product->variations()->where('variation_id',$fData['group_id'])->first();
+            $mainVariation = $product->variations()->where('variation_id', $fData['group_id'])->first();
         }
 
-        if($mainVariation && $mainVariation->type == 'filter_discount'){
+        if ($mainVariation && $mainVariation->type == 'filter_discount') {
 
-            if($mainVariation->price_per == 'discount'){
-                $discount = $mainVariation->discounts()->where('qty',$fData['limit'])->first();
-                if($discount){
+            if ($mainVariation->price_per == 'discount') {
+                $discount = $mainVariation->discounts()->where('qty', $fData['limit'])->first();
+                if ($discount) {
                     $options = $fData['products'];
                     $product_limit = 0;
                     $data['group'] = $mainVariation;
                     $data['options'] = [];
                     $data['price'] = $discount->price;
                     $this->price = $discount->price;
-                    if(count($options)){
-                        foreach ($options as $p){
+                    if (count($options)) {
+                        foreach ($options as $p) {
                             $option = $product->variations()->with('item')->where('variation_id', $fData['group_id'])->where('id', $p['id'])->first();
                             if ($option) {
                                 $product_limit += $p['qty'];
@@ -285,21 +285,21 @@ class CartService
                         }
 
                     }
-                    if($product_limit != $fData['limit']){
+                    if ($product_limit != $fData['limit']) {
                         $error = "Please select options according to limit";
                     }
 
                     $this->variations[] = $data;
-                }else{
+                } else {
                     $error = "Section not found";
                 }
-            }else{
+            } else {
                 $error = "Section not found";
             }
-        }else{
+        } else {
             $extraVariations = $product->variations()->with('item')->required()->groupby('stock_variations.variation_id')->get();
 
-            if (($vdata && count($vdata) == count($extraVariations))|| ($vdata && ($product->section_type == 1) )) {
+            if (($vdata && count($vdata) == count($extraVariations)) || ($vdata && ($product->section_type == 1))) {
 //                dd($product,$vdata,count($vdata),count($extraVariations));
                 foreach ($vdata as $k => $item) {
                     $data = [];
@@ -311,8 +311,8 @@ class CartService
 
                         if (isset($item['products']) && $item['products'] == 'no') {
                             $data['price'] = 0;
-                        }else{
-                            if(isset($item['products']) && count($item['products'])) {
+                        } else {
+                            if (isset($item['products']) && count($item['products'])) {
 //                                dd($vdata,$group->price_per,$item['products']);
                                 if ($group->price_per == 'product') {
                                     $data['price'] = $group->price;
@@ -334,29 +334,29 @@ class CartService
                                     foreach ($item['products'] as $p) {
                                         $option = $product->variations()->with('item')->where('variation_id', $item['group_id'])->where('id', $p['id'])->first();
                                         if ($option) {
-                                            $p['qty'] = ($p['qty'])??1;
-                                            if($option->price_type == 'fixed' || $option->price_type == 'range'){
-                                                if($p['discount_id'] == null){
-                                                    $discount = $option->discounts()->where('from','<=',$p['qty'])->where('to','>=',$p['qty'])->first();
-                                                    if($discount) {
+                                            $p['qty'] = ($p['qty']) ?? 1;
+                                            if ($option->price_type == 'fixed' || $option->price_type == 'range') {
+                                                if ($p['discount_id'] == null) {
+                                                    $discount = $option->discounts()->where('from', '<=', $p['qty'])->where('to', '>=', $p['qty'])->first();
+                                                    if ($discount) {
                                                         $this->price += $p['qty'] * $discount->price;
                                                         $itemPrice += $p['qty'] * $discount->price;
-                                                    }else{
+                                                    } else {
                                                         $error = "Option not found";
                                                     }
-                                                }else{
+                                                } else {
                                                     $discount = StockVariationDiscount::findOrFail($p['discount_id']);
-                                                    if($discount) {
+                                                    if ($discount) {
                                                         $this->price += $discount->price;
                                                         $itemPrice += $discount->price;
-                                                    }else{
+                                                    } else {
                                                         $error = "Option not found";
                                                     }
                                                 }
-                                            }elseif($option->price_type == 'dynamic'){
+                                            } elseif ($option->price_type == 'dynamic') {
                                                 $this->price += $p['qty'] * $option->item->default_price;
                                                 $itemPrice += $p['qty'] * $option->item->default_price;
-                                            }else{
+                                            } else {
                                                 $this->price += $p['qty'] * $option->price;
                                                 $itemPrice += $p['qty'] * $option->price;
                                             }
@@ -377,13 +377,13 @@ class CartService
 
 //                            dd($group->min_count_limit > $product_limit || $group->count_limit < $product_limit);
 //                            dd($product_limit,$data['options']);
-                            if($group->type == 'single'){
+                            if ($group->type == 'single') {
                                 if (!isset($data['options']) || count($data['options']) != 1) {
                                     $error = "Please select options according to limit";
                                 }
-                            }else{
-                                if (! isset($item['products']) || ($group->min_count_limit > count($item['products']) || $group->count_limit < count($item['products']))) {
-                                    dd($item,$group,$product_limit);
+                            } else {
+                                if (!isset($item['products']) || ($group->min_count_limit > count($item['products']) || $group->count_limit < count($item['products']))) {
+                                    dd($item, $group, $product_limit);
                                     $error = "Please select options according to limit";
                                 }
                             }
@@ -410,19 +410,19 @@ class CartService
     {
         $error = false;
         $offerData = [];
-        if(count($offers)){
-            foreach ($offers as $vdata){
+        if (count($offers)) {
+            foreach ($offers as $vdata) {
                 $data = [];
                 $offer = Stock::find($vdata['product_id']);
-                if($offer){
+                if ($offer) {
                     $data['offer'] = $offer;
                     $data['key'] = uniqid();
                     $data['variations'] = [];
                     $data['price'] = 0;
 
                     $variations = [];
-                    if(isset($vdata['variations']) && count($vdata['variations'])){
-                        foreach ($vdata['variations'] as $variation){
+                    if (isset($vdata['variations']) && count($vdata['variations'])) {
+                        foreach ($vdata['variations'] as $variation) {
                             $group = $offer->variations()->with('item')->where('variation_id', $variation['group_id'])->first();
                             if ($group) {
                                 $variations['group'] = $group;
@@ -451,29 +451,29 @@ class CartService
                                         foreach ($variation['products'] as $p) {
                                             $option = $offer->variations()->with('item')->where('variation_id', $variation['group_id'])->where('id', $p['id'])->first();
                                             if ($option) {
-                                                $p['qty'] = ($p['qty'])??1;
-                                                if($option->price_type == 'fixed' || $option->price_type == 'range'){
-                                                    if($p['discount_id'] == null){
-                                                        $discount = $option->discounts()->where('from','<=',$p['qty'])->where('to','>=',$p['qty'])->first();
-                                                        if($discount) {
+                                                $p['qty'] = ($p['qty']) ?? 1;
+                                                if ($option->price_type == 'fixed' || $option->price_type == 'range') {
+                                                    if ($p['discount_id'] == null) {
+                                                        $discount = $option->discounts()->where('from', '<=', $p['qty'])->where('to', '>=', $p['qty'])->first();
+                                                        if ($discount) {
                                                             $this->price += $p['qty'] * $discount->price;
                                                             $itemPrice += $p['qty'] * $discount->price;
-                                                        }else{
+                                                        } else {
                                                             $error = "Option not found";
                                                         }
-                                                    }else{
+                                                    } else {
                                                         $discount = StockVariationDiscount::findOrFail($p['discount_id']);
-                                                        if($discount) {
+                                                        if ($discount) {
                                                             $this->price += $discount->price;
                                                             $itemPrice += $discount->price;
-                                                        }else{
+                                                        } else {
                                                             $error = "Option not found";
                                                         }
                                                     }
-                                                }elseif($option->price_type == 'dynamic'){
+                                                } elseif ($option->price_type == 'dynamic') {
                                                     $this->price += $p['qty'] * $option->item->default_price;
                                                     $itemPrice += $p['qty'] * $option->item->default_price;
-                                                }else{
+                                                } else {
                                                     $this->price += $p['qty'] * $option->price;
                                                     $itemPrice += $p['qty'] * $option->price;
                                                 }
@@ -495,7 +495,7 @@ class CartService
                                     if ($group->min_count_limit > $product_limit || $group->count_limit < $product_limit) {
                                         $error = "Please select options according to limit";
                                     }
-                                }else{
+                                } else {
                                     $error = "Empty variation";
                                 }
                             }
@@ -503,12 +503,12 @@ class CartService
                     }
 
                     $data['variations'] = $variations;
-                }else{
+                } else {
                     $error = "Offer not found";
                 }
                 $offerData['data'][] = $data;
             }
-        }else {
+        } else {
             $error = "No offers";
         }
 
