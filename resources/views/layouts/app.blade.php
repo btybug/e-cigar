482c54e1c3456842b60dd9a20988a82f71509b39
sahakavatar <hakobyan.sahak88@gmail.com>
2020-04-19 21:08:06 +0400

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="google-site-verification" content="Z407Ah-a75yFLeExaK-cNsS1XEhzcbb-6qAPax2V0eI" />
+
 
 
     <!-- CSRF Token -->
