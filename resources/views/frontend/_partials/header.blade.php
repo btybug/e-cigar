a912a72b3a606715110eb78307c1fb5d430b99fe
sahakavatar <hakobyan.sahak88@gmail.com>
2020-08-03 13:43:33 +0400

    up

--- resources/views/frontend/_partials/header.blade.php
+++ resources/views/frontend/_partials/header.blade.php
@@ -324,7 +324,7 @@
     @include("frontend._partials.login_modal")
 
     <!--modal Register-->
-    @include("frontend._partials.create_account")
+{{--    @include("frontend._partials.create_account")--}}
 
 @endif
 
