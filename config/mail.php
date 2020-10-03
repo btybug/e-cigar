1ce4af1c91c2443e9d8521996bf1e9416589bda6
sahakavatar <hakobyan.sahak88@gmail.com>
2020-10-03 16:34:39 +0400

    up

--- config/mail.php
+++ config/mail.php
@@ -119,4 +119,10 @@ return [
             resource_path('views/vendor/mail'),
         ],
     ],
+    'stream' => [
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true,
+        ],
 ];
