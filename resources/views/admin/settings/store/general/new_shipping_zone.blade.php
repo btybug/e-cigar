b921b70d253ead626772af15c166d581876aa3ca
terakyan <edo.terakyan@gmail.com>
2020-10-07 15:03:52 +0400

    up

--- resources/views/admin/settings/store/general/new_shipping_zone.blade.php
+++ resources/views/admin/settings/store/general/new_shipping_zone.blade.php
@@ -188,6 +188,12 @@
                                                                         </div>
                                                                     </div>
                                                                 </div>
+                                                                <div class="col-xl-5">
+                                                                    <a href="javascript:void(0)" data-order-count="0"
+                                                                            class="btn btn-primary text-center table--store-settings_add-options add-new-order-filed pull-right">
+                                                                        <span><i class="fa fa-plus"></i></span> Add more option
+                                                                    </a>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </td>
@@ -199,7 +205,8 @@
                                                 <th>{!! ($delivery->delivery_cost_types_id==1)?'Order Amount':'Weight Amount' !!}</th>
                                                 <th>Courier</th>
                                                 <th>Cost</th>
-                                                <th colspan="3">Time</th>
+                                                <th colspan="2">Time</th>
+                                                <th><button type="button" class="btn btn-danger remove-deliver-option"><i class="fa fa-trash"></i></button></th>
                                             </tr>
                                             @foreach($delivery->options as $key=>$option)
                                                 <tr>
@@ -220,12 +227,17 @@
 
                                                     </td>
                                                     <td>
-                                                        {!! Form::text('delivery_cost['.$delivery->id.'][options]['.$option->id.'][time]',$option->time,['class'=>'form-control','placeholder'=>'3 day']) !!}
+                                                        {!! Form::text('delivery_cost['.$delivery->id.'][options]['.$option->id.'][time_text]',$option->time,['class'=>'form-control','placeholder'=>'3 day']) !!}
 
                                                     </td>
                                                     <td colspan="2" class="text-right">
                                                         <button type="button" class="btn btn-danger remove-ship-filed"><i
                                                                     class="fa fa-minus-circle"></i></button>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        {!! Form::textarea('delivery_cost['.$delivery->id.'][options]['.$option->id.'][order_amount_text]',$option->order_amount_text,['class'=>'form-control tinyMcArea','placeholder'=>'FREE on orders over £10']) !!}
                                                     </td>
                                                 </tr>
                                             @endforeach
@@ -239,19 +251,6 @@
                                                 </td>
                                             </tr>
                                             </tbody>
-
-                                            <tfoot>
-
-
-                                            <tr>
-                                                <td colspan="5" data-id="{!! $delivery->id !!}"
-                                                    class="text-center table--store-settings_add-options add-new-order-filed">
-                                                    <span><i class="fa fa-plus"></i></span> Add more option
-                                                </td>
-                                            </tr>
-
-
-                                            </tfoot>
                                         </table>
                                     @endforeach
                                 @else
@@ -278,7 +277,9 @@
                                             <th>Order Amount</th>
                                             <th>Courier</th>
                                             <th>Cost</th>
-                                            <th colspan="3">Time</th>
+                                            <th colspan="2">Time</th>
+                                            <th><button type="button" class="btn btn-danger remove-deliver-option"><i class="fa fa-trash"></i></button></th>
+
                                         </tr>
                                         <tr>
                                             <td class="table--store-settings_vert-top">
@@ -304,6 +305,11 @@
                                                             class="fa fa-minus-circle"></i></button>
                                             </td>
                                         </tr>
+                                        <tr>
+                                            <td>
+                                                {!! Form::textarea('delivery_cost[0][options][0][order_amount_text]',null,['class'=>'form-control tinyMcArea','placeholder'=>'FREE on orders over £10']) !!}
+                                            </td>
+                                        </tr>
                                         <tr class="add-new-ship-filed-container">
                                             <td colspan="6" class="text-right">
                                                 <button type="button" data-id="0" data-options-count="0"
@@ -313,19 +319,6 @@
                                             </td>
                                         </tr>
                                         </tbody>
-
-                                        <tfoot>
-
-
-                                        <tr>
-                                            <td colspan="5" data-order-count="0"
-                                                class="text-center table--store-settings_add-options add-new-order-filed">
-                                                <span><i class="fa fa-plus"></i></span> Add more option
-                                            </td>
-                                        </tr>
-
-
-                                        </tfoot>
                                     </table>
                                 @endif
                             </div>
@@ -350,9 +343,100 @@
     <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js')}}"></script>
     <script src="{{asset('public/js/custom/get_zones.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+    <script src="/public/plugins/tinymce/tinymce.js"></script>
 
     <script>
+        function initTinyMce(e) {
+            tinymce.init({
+                selector: e,
+                plugins: 'print preview fullpage   importcss  searchreplace autolink autosave save directionality  visualblocks visualchars fullscreen image link media  template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists  wordcount   imagetools textpattern noneditable help    charmap   quickbars  emoticons ',
+                //   imagetools_cors_hosts: ['picsum.photos'],
+                //   tinydrive_token_provider: function (success, failure) {
+                //     success({ token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.Ks_BdfH4CWilyzLNk8S2gDARFhuxIauLa8PwhdEQhEo' });
+                //   },
+                //   tinydrive_demo_files_url: '/docs/demo/tiny-drive-demo/demo_files.json',
+                //   tinydrive_dropbox_app_key: 'jee1s9eykoh752j',
+                //   tinydrive_google_drive_key: 'AIzaSyAsVRuCBc-BLQ1xNKtnLHB3AeoK-xmOrTc',
+                //   tinydrive_google_drive_client_id: '748627179519-p9vv3va1mppc66fikai92b3ru73mpukf.apps.googleusercontent.com',
+                // mobile: {
+                //     plugins: 'print preview fullpage   importcss  searchreplace autolink autosave save directionality  visualblocks visualchars fullscreen image link media  template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists  wordcount   textpattern noneditable help   charmap  quickbars  emoticons '
+                // },
+                menu: {
+                    tc: {
+                        title: 'TinyComments',
+                        items: 'addcomment showcomments deleteallconversations'
+                    }
+                },
+                menubar: '',
+                //   'file edit view insert format tools table tc help',
+                toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist  | forecolor backcolor    removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media  template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+                autosave_ask_before_unload: true,
+                //   autosave_interval: "30s",
+                //   autosave_prefix: "{path}{query}-{id}-",
+                //   autosave_restore_when_empty: false,
+                //   autosave_retention: "2m",
+                image_advtab: true,
+                content_css: [
+                    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                    '//www.tiny.cloud/css/codepen.min.css'
+                ],
+                link_list: [
+                    { title: 'My page 1', value: 'http://www.tinymce.com' },
+                    { title: 'My page 2', value: 'http://www.moxiecode.com' },
+                    { title: 'Kaliony', value: 'http://e-cigar.com' }
+                ],
+                image_list: [
+                    { title: 'My page 1', value: 'http://www.tinymce.com' },
+                    { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                ],
+                image_class_list: [
+                    { title: 'None', value: '' },
+                    { title: 'Some class', value: 'class-name' }
+                ],
+                importcss_append: true,
+                height: 200,
+                //   file_picker_callback: function (callback, value, meta) {
+                //     /* Provide file and text for the link dialog */
+                //     if (meta.filetype === 'file') {
+                //       callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+                //     }
 
+                //     /* Provide image and alt text for the image dialog */
+                //     if (meta.filetype === 'image') {
+                //       callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+                //     }
+
+                //     /* Provide alternative source and posted for the media dialog */
+                //     if (meta.filetype === 'media') {
+                //       callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+                //     }
+                //   },
+                templates: [
+                    { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+                    { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+                    { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+                ],
+                template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+                template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+                height: 200,
+                image_caption: true,
+                quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+                noneditable_noneditable_class: "mceNonEditable",
+                toolbar_drawer: 'sliding',
+                spellchecker_dialog: true,
+                spellchecker_whitelist: ['Ephox', 'Moxiecode'],
+                tinycomments_mode: 'embedded',
+                content_style: ".mymention{ color: gray; }",
+                contextmenu: "link image imagetools table configurepermanentpen",
+                mentions_selector: '.mymention',
+                //   mentions_fetch: mentions_fetch,
+                //   mentions_menu_hover: mentions_menu_hover,
+                //   mentions_menu_complete: mentions_menu_complete,
+                //   mentions_select: mentions_select,
+            });
+        }
+
+        initTinyMce(".tinyMcArea")
 
         $("body").on("click", ".remove-deliver-option", function () {
             $(this).closest("tbody").remove()
@@ -399,11 +483,17 @@
           <button type="button" data-options-count="1"  data-id="${count}" class="btn btn-primary add-new-ship-filed"><i class="fa fa-plus-circle"></i></button>
 </td>
 </tr>
+<tr>
+    <td>
+        {!! Form::textarea('delivery_cost[${count}][options][0][order_amount_text]',null,['class'=>'form-control tinyMcArea','placeholder'=>'FREE on orders over £10']) !!}
+    </td>
+</tr>
 </tbody>`;
             count++
             $(this)
                 .closest("table")
                 .append(html);
+            initTinyMce(".tinyMcArea")
         });
 
         let datax = "";
