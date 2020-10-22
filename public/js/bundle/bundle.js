ba500015e71b89bb55a2e46bc4e45de269fae30b
DeiMos13AK <galstyan.david1993@gmail.com>
2020-10-22 17:02:55 +0400

    up

--- public/js/bundle/bundle.js
+++ public/js/bundle/bundle.js
@@ -5890,9 +5890,9 @@ $(document).ready(function () {
             e.preventDefault();
             var id = $('#singleProductPageCnt #vpid').val();
             var price = countTotalPrice();
-            var variations = $("#special_offer_data").val() && JSON.parse($("#special_offer_data").val());
+            var variations = $("#special_offer_data").val() && JSON.parse($("#special_offer_data").val()).variations;
 
-            console.log("$(#special_offer_data).val()", $("#special_offer_data").val().length);
+            console.log("$(#special_offer_data).val()", variations);
             $.ajax({
                 type: "post",
                 url: "/get-special-offers",
