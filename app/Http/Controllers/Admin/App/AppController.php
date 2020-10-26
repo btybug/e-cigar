21eea47df85063d02d2ae38808f9e3790cd84c51
sahakavatar <hakobyan.sahak88@gmail.com>
2020-10-26 17:20:23 +0400

    up

--- app/Http/Controllers/Admin/App/AppController.php
+++ app/Http/Controllers/Admin/App/AppController.php
@@ -42,6 +42,13 @@ class AppController extends Controller
 
     public function products(Request $request, $q)
     {
+        $id=1;
+        $items = Items::leftJoin('app_items', 'items.id', 'app_items.item_id')
+            ->select('items.*', 'app_items.item_id','app_items.warehouse_id')
+            ->where(function ($query)use($id){
+                return  $query->whereNull('app_items.warehouse_id')->orWhere('app_items.warehouse_id','!=',$id);
+            })->with(['brand', 'categories', 'translations'])->get();
+        dd($items);
         $current = null;
         $warehouse = AppWarehouses::join('warehouses', 'app_warehouses.warehouse_id', '=', 'warehouses.id')
             ->leftJoin('warehouse_translations', 'warehouses.id', '=', 'warehouse_translations.warehouse_id')
@@ -79,6 +86,7 @@ class AppController extends Controller
            ->where(function ($query)use($id){
              return  $query->whereNull('app_items.warehouse_id')->orWhere('app_items.warehouse_id','!=',$id);
            })->with(['brand', 'categories', 'translations'])->get();
+
         $brands = Brands::all();
         $categories = Category::where('type', 'item')->get();
         return \Response::json(['error' => false, 'data' => $items, 'brands' => $brands, 'categories' => $categories]);
