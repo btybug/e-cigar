e355b2e7eef77014b1590dde0d1a041f9180f4e3
sahakavatar <hakobyan.sahak88@gmail.com>
2020-10-27 11:43:47 +0400

    update

--- app/Http/Controllers/Admin/App/AppController.php
+++ app/Http/Controllers/Admin/App/AppController.php
@@ -75,12 +75,13 @@ class AppController extends Controller
 
     public function notSelectedProducts($id)
     {
-//       $items = Items::leftJoin('app_items', 'items.id', 'app_items.item_id')
-//           ->select('items.*', 'app_items.item_id','app_items.warehouse_id')
-//           ->where(function ($query)use($id){
-//             return  $query->whereNull('app_items.warehouse_id')->orWhere('app_items.warehouse_id','!=',$id);
-//           })->with(['brand', 'categories', 'translations'])->get();
-        $items=[];
+       $items = Items::leftJoin('app_items', 'items.id', 'app_items.item_id')
+           ->select('items.*', 'app_items.item_id','app_items.warehouse_id')
+           ->where(function ($query)use($id){
+             return  $query->whereNull('app_items.warehouse_id')->orWhere('app_items.warehouse_id','!=',$id);
+           })
+//           ->with(['brand', 'categories', 'translations'])
+           ->get();
         $brands = Brands::all();
         $categories = Category::where('type', 'item')->get();
         return response()->json(['error' => false, 'data' => $items, 'brands' => $brands, 'categories' => $categories]);
