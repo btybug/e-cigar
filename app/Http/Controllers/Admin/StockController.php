c1f8fd0e763fb04f27d924c68e532110ad163f7e
terakyan <edo.terakyan@gmail.com>
2020-11-17 18:35:05 +0400

    up

--- app/Http/Controllers/Admin/StockController.php
+++ app/Http/Controllers/Admin/StockController.php
@@ -46,27 +46,24 @@ class StockController extends Controller
 
     public function stock()
     {
-//        $stocks = Items::all();
+        $stocks = Stock::all();
 
-//        $x = substr("123456789",10);
-//        dd($x);
+        foreach ($stocks as $stock){
+            $data = $stock->other_images;
+            $other_images = [];
+            if($data && count($data)){
+                foreach ($data as $k => $datum){
+                    $datum['image'] = substr($datum['image'],7);
+                    $other_images[$k] = $datum;
+                }
+            }
 
-//        foreach ($stocks as $stock){
-//            $data = $stock->other_images;
-//            $other_images = [];
-//            if($data && count($data)){
-//                foreach ($data as $k => $datum){
-//                    $datum['image'] = substr($datum['image'],7);
-//                    $other_images[$k] = $datum;
-//                }
-//            }
-//
-//            $stock->image = substr($stock->image,7);
-//            $stock->other_images = $other_images;
-//            $stock->save();
-//        }
-//
-//        dd("done !!!");
+            $stock->image = substr($stock->image,7);
+            $stock->other_images = $other_images;
+            $stock->save();
+        }
+
+        dd("done !!!");
 //
 //        $filters = StockVariation::where('type','filter')->get();
 //
